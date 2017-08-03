f5bedd4ab3cf996fbaa71374800dd1bb7d534105
fbraem <franky.braem@gmail.com>
2017-08-03 20:03:26 +0200

    Update routes

--- api/src/rest/Users/Router.php
+++ api/src/rest/Users/Router.php
@@ -6,13 +6,11 @@ class Router extends \Core\Router
 {
     public function createRoutes()
     {
-        $this->map->get('browse', '/users', \REST\Users\Actions\BrowseAction::class);
-            //->auth(['login' => true]);
-            //->accepts(['application/vnd.api+json'])
-            //->extras(['format' => 'json']);
+        $this->map->get('browse', '/users', \REST\Users\Actions\BrowseAction::class)
+            ->auth(['login' => true])
+            ->accepts(['application/vnd.api+json']);
         $this->map->get('read', '/users/{id}', \REST\Users\Actions\ReadAction::class)
-            ->auth(['login' => true]);
-            //->accepts(['application/vnd.api+json'])
-            //->extras(['format' => 'json']);
+            ->auth(['login' => true])
+            ->accepts(['application/vnd.api+json']);
     }
 }
