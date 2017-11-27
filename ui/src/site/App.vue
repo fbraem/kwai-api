57a04984f3a501615f64770c0b4b23e69c4d0d72
fbraem <franky.braem@gmail.com>
2017-11-27 19:59:31 +0100

    Renamed

---
+++ ui/src/site/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-layout>
+        <v-flex xs12 lg8 offset-lg2>
+            <v-card>
+                <v-card-media :src="require('./images/judokwaikemzeke.jpg')" height="500">
+                    <v-container fill-height fluid>
+                        <v-layout fill-height>
+                            <v-flex xs6 align-end flexbox>
+                                <div class="white--text" style="padding:5px;background-color:rgba(34,34,34,0.8)">
+                                    <h3 class="headline">Judokwai Kemzeke</h3>
+                                    <p>
+                                        Deze florerende judoclub bestaat al ongeveer 40 jaar, telt ongeveer 100-150 leden en heeft al veel op haar palmares.
+                                    </p>
+                                    <p>
+                                        Alle informatie betreffende trainingen, contactgegevens, geschiedenis van de club en het judo, foto’s van leden/kampen/tornooien.
+                                    </p>
+                                </div>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card-media>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        }
+    };
+</script>
