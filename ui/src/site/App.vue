af996db7c7d30676d1253e75a0941f070a373985
fbraem <franky.braem@gmail.com>
2017-11-27 22:08:39 +0100

    Bigger size for small screens

--- ui/src/site/App.vue
+++ ui/src/site/App.vue
@@ -5,14 +5,14 @@
                 <v-card-media :src="require('./images/judokwaikemzeke.jpg')" height="500">
                     <v-container fill-height fluid>
                         <v-layout fill-height>
-                            <v-flex xs6 align-end flexbox>
+                            <v-flex xs8 align-end flexbox>
                                 <div class="white--text" style="padding:5px;background-color:rgba(34,34,34,0.8)">
                                     <h3 class="headline">Judokwai Kemzeke</h3>
                                     <p>
                                         Deze florerende judoclub bestaat al ongeveer 40 jaar, telt ongeveer 100-150 leden en heeft al veel op haar palmares.
                                     </p>
                                     <p>
-                                        Alle informatie betreffende trainingen, contactgegevens, geschiedenis van de club en het judo, foto’s van leden/kampen/tornooien.
+                                        Alle informatie betreffende trainingen, contactgegevens, geschiedenis van de club en het judo, foto’s van leden / kampen / tornooien.
                                     </p>
                                 </div>
                             </v-flex>
