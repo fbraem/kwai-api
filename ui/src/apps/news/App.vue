4f9f3d5938e91ddfaad869dc7f0a66ffd17cec0b
fbraem <franky.braem@gmail.com>
2017-11-28 21:58:40 +0100

    Don't use the full screen on large screens

--- ui/src/apps/news/App.vue
+++ ui/src/apps/news/App.vue
@@ -1,58 +1,62 @@
 <template>
-    <v-card flat>
-        <v-card-media :src="backgroundImage" height="200">
-            <v-container fill-height fluid>
-                <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                        <span class="pa-2 white headline"><v-icon class="mr-2">subject</v-icon>News</span>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-card-media>
-        <v-card-text>
-            <v-layout row wrap>
-                <v-flex xs12 sm4 md3>
+    <v-layout>
+        <v-flex xs12 lg8 offset-lg2>
+            <v-card flat>
+                <v-card-media :src="backgroundImage" height="200">
+                    <v-container fill-height fluid>
+                        <v-layout fill-height>
+                            <v-flex xs12 align-end flexbox>
+                                <span class="pa-2 white headline"><v-icon class="mr-2">subject</v-icon>News</span>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card-media>
+                <v-card-text>
                     <v-layout row wrap>
+                        <v-flex xs12 sm4 md3>
+                            <v-layout row wrap>
+                                <v-flex xs12>
+                                    <v-card>
+                                        <v-card-title>
+                                            <div class="headline">Categories</div>
+                                        </v-card-title>
+                                    </v-card>
+                                </v-flex>
+                            </v-layout>
+                            <v-layout row wrap>
+                                <v-flex xs12>
+                                    <v-card>
+                                        <v-card-title>
+                                            <div class="headline">Archive</div>
+                                        </v-card-title>
+                                    </v-card>
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                        <v-flex xs12 sm8 md9>
+                            <v-layout v-if="noNews" row wrap>
+                                <v-flex xs12>
+                                    No news for today
+                                </v-flex>
+                            </v-layout>
+                            <v-layout row wrap>
+                                <v-flex v-for="story in stories" :key="story.id" xs12 sm6 md5 lg4>
+                                    <NewsCard :story="story" :complete="false" />
+                                </v-flex>
+                            </v-layout>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout>
                         <v-flex xs12>
-                            <v-card>
-                                <v-card-title>
-                                    <div class="headline">Categories</div>
-                                </v-card-title>
-                            </v-card>
+                            <v-btn v-if="$isAllowed('create')" icon :to="'create'">
+                                <v-icon>add</v-icon>
+                            </v-btn>
                         </v-flex>
                     </v-layout>
-                    <v-layout row wrap>
-                        <v-flex xs12>
-                            <v-card>
-                                <v-card-title>
-                                    <div class="headline">Archive</div>
-                                </v-card-title>
-                            </v-card>
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-                <v-flex xs12 sm8 md9>
-                    <v-layout v-if="noNews" row wrap>
-                        <v-flex xs12>
-                            No news for today
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row wrap>
-                        <v-flex v-for="story in stories" :key="story.id" xs12 sm6 md5 lg4>
-                            <NewsCard :story="story" :complete="false" />
-                        </v-flex>
-                    </v-layout>
-                </v-flex>
-            </v-layout>
-            <v-layout>
-                <v-flex xs12>
-                    <v-btn v-if="$isAllowed('create')" icon :to="'create'">
-                        <v-icon>add</v-icon>
-                    </v-btn>
-                </v-flex>
-            </v-layout>
-        </v-card-text>
-    </v-card>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
--- ui/src/apps/news/app/NewsCreate.vue
+++ ui/src/apps/news/app/NewsCreate.vue
@@ -1,15 +1,19 @@
 <template>
-    <v-layout row wrap>
-        <v-flex xs12>
-            <v-toolbar class="elevation-0">
-                <v-icon>add</v-icon>
-                <v-toolbar-title>Create News</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-btn icon dark color="primary" @click="$router.go(-1)"><v-icon>cancel</v-icon></v-btn>
-            </v-toolbar>
-        </v-flex>
-        <v-flex xs12>
-            <NewsForm :story="null" />
+    <v-layout>
+        <v-flex xs12 lg8 offset-lg2>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <v-toolbar class="elevation-0">
+                        <v-icon>add</v-icon>
+                        <v-toolbar-title>Create News</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-btn icon dark color="primary" @click="$router.go(-1)"><v-icon>cancel</v-icon></v-btn>
+                    </v-toolbar>
+                </v-flex>
+                <v-flex xs12>
+                    <NewsForm :story="null" />
+                </v-flex>
+            </v-layout>
         </v-flex>
     </v-layout>
 </template>
