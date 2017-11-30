2363985347ac86d415e626d10b074255a6eb9c02
fbraem <franky.braem@gmail.com>
2017-11-30 21:52:36 +0100

    Resize content

--- ui/src/apps/news/app/NewsRead.vue
+++ ui/src/apps/news/app/NewsRead.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-layout row wrap>
-        <v-flex xs12>
+    <v-layout>
+        <v-flex xs12 lg8 offset-lg2>
             <NewsCard v-if="story" :story="story" :complete="true" />
         </v-flex>
     </v-layout>
