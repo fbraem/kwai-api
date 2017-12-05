86c738c9ff10dbeeac293b212d7d2d5f71c29f9d
fbraem <franky.braem@gmail.com>
2017-12-05 21:26:15 +0100

    Set contents

--- api/src/domain/News/NewsStory.php
+++ api/src/domain/News/NewsStory.php
@@ -2,6 +2,7 @@
 namespace Domain\News;
 
 use Analogue\ORM\Entity;
+use Analogue\ORM\EntityCollection;
 
 /**
  * @inheritdoc
@@ -10,5 +11,6 @@ class NewsStory extends Entity
 {
     public function __construct()
     {
+        $this->contents = new EntityCollection();
     }
 }
