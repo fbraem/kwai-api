8d5bf4ca02ce92ca559dd179f86997f0e816804b
fbraem <franky.braem@gmail.com>
2017-12-23 21:11:54 +0100

    Change header

--- ui/src/apps/news/components/NewsCard.vue
+++ ui/src/apps/news/components/NewsCard.vue
@@ -1,26 +1,43 @@
 <template>
     <v-card :flat="complete">
-        <v-card-media v-if="complete && story.header_detail_crop" :src="story.header_detail_crop" height="200px">
-            <v-container fill-height fluid>
-                <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                        <span class="pa-2 white headline"><v-icon class="mr-2">subject</v-icon>{{ title }}</span>
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-card-media>
-        <v-card-media v-if="!complete && story.header_overview_crop" :src="story.header_overview_crop" height="200px" />
-        <v-card-title>
-            <div>
-                <h3 v-if="!complete"class="headline mb-0">{{ title }}</h3>
-                <div class="news-mini-meta">
-                    <span v-if="authorName.length > 0">{{ authorName }} | </span>
-                    <span v-if="story.publish_date">Published on {{ publishDate }} - {{ publishDateFromNow }}</span>
+        <div v-if="complete">
+            <v-card-media v-if="story.header_detail_crop" :src="story.header_detail_crop" height="400px">
+                <v-container fill-height fluid>
+                    <v-layout fill-height>
+                        <v-flex xs8 align-end flexbox>
+                            <div class="pa-2 white headline" style="background-color:rgba(34,34,34,0.8)">
+                                {{ title }}
+                            </div>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-card-media>
+            <v-card-title>
+                <div>
+                    <h3 v-if="!story.header_detail_crop" class="headline mb-0">{{ title }}</h3>
+                    <div class="news-mini-meta">
+                        <span v-if="authorName.length > 0">{{ authorName }} | </span>
+                        <span v-if="story.publish_date">{{ $t('published', { publishDate : publishDate, publishDateFromNow : publishDateFromNow }) }}</span>
+                    </div>
+                    <div v-html="summary" style="margin-top:20px" :class="{ 'news-meta' : complete }">
+                    </div>
                 </div>
-                <div v-html="summary" style="margin-top:20px" :class="{ 'news-meta' : complete }">
+            </v-card-title>
+        </div>
+        <div v-else>
+            <v-card-media v-if="story.header_overview_crop" :src="story.header_overview_crop" height="200px" />
+            <v-card-title>
+                <div>
+                    <h3 class="headline mb-0">{{ title }}</h3>
+                    <div class="news-mini-meta">
+                        <span v-if="authorName.length > 0">{{ authorName }} | </span>
+                        <span v-if="story.publish_date">{{ $t('published', { publishDate : publishDate, publishDateFromNow : publishDateFromNow }) }}</span>
+                    </div>
+                    <div v-html="summary" style="margin-top:20px" :class="{ 'news-meta' : complete }">
+                    </div>
                 </div>
-            </div>
-        </v-card-title>
+            </v-card-title>
+        </div>
         <v-divider v-if="complete"></v-divider>
         <v-card-text v-if="complete">
             <div class="news-content" v-html="content">
@@ -28,17 +45,17 @@
         </v-card-text>
         <v-card-actions>
             <v-btn v-if="!complete" icon :to="'/read/' + story.id" flat>
-                <v-icon>more_horiz</v-icon>
+                <v-icon>fa-ellipsis-h</v-icon>
             </v-btn>
             <v-btn v-if="complete" icon :to="'/'" flat>
                 <v-icon>view_list</v-icon>
             </v-btn>
             <v-spacer></v-spacer>
-            <v-btn v-if="$isAllowed('update', story)" icon :to="'/update/' + story.id" flat>
-                <v-icon>mode_edit</v-icon>
+            <v-btn v-if="$isAllowed('update', story)" color="secondary" icon :to="'/update/' + story.id" flat>
+                <v-icon>fa-edit</v-icon>
             </v-btn>
-            <v-btn v-if="$isAllowed('remove', story)" icon :to="'/delete/' + story.id" flat>
-                <v-icon>delete</v-icon>
+            <v-btn v-if="$isAllowed('remove', story)" color="secondary" icon :to="'/delete/' + story.id" flat>
+                <v-icon>fa-trash</v-icon>
             </v-btn>
         </v-card-actions>
     </v-card>
