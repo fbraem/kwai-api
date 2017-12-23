7a3bdbea53a898b8e6484acfb25003acb8bac12c
fbraem <franky.braem@gmail.com>
2017-12-23 21:10:08 +0100

    Add translations

---
+++ ui/src/apps/news/lang/lang.js
@@ -0,0 +1,20 @@
+export default {
+    nl : {
+        news : "Nieuws",
+        submit : "Verstuur",
+        categories : "Overzicht",
+        archive: "Archief",
+        clear : "Herbegin",
+        published : "Geschreven op {publishDate} - {publishDateFromNow}",
+        no_news : "Er zijn geen nieuwsberichten"
+    },
+    en : {
+        news : "News",
+        categories: "Categories",
+        submit : "Submit",
+        archive: "Archive",
+        clear : "Clear",
+        published : "Published on {publishDate} - {publishDateFromNow}",
+        no_news : "There are no newsstories"
+    }
+};
