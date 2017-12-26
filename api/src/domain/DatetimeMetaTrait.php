3b853f1eb0217f2668630d507260b5dab3a4e9d7
fbraem <franky.braem@gmail.com>
2017-12-26 20:24:42 +0100

    Remove conversion

--- api/src/domain/DatetimeMetaTrait.php
+++ api/src/domain/DatetimeMetaTrait.php
@@ -10,20 +10,14 @@ trait DatetimeMetaTrait
 
     public function createdAt() : ?string
     {
-        // Timestamps are always converted by MySQL to server time
-        // We need UTC, so convert it back ...
         $createdAt = new \Carbon\Carbon($this->created_at);
-        $createdAt->timezone('UTC');
         return $createdAt->toDateTimeString();
     }
 
     public function updatedAt() : ?string
     {
         if ($this->updated_at) {
-            // Timestamps are always converted by MySQL to server time
-            // We need UTC, so convert it back ...
             $updatedAt = new \Carbon\Carbon($this->updated_at);
-            $updatedAt->timezone('UTC');
             return $updatedAt->toDateTimeString();
         }
         return null;
