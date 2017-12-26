763b8db8545024109037cf8687517989281d4da4
fbraem <franky.braem@gmail.com>
2017-12-26 20:20:47 +0100

    Return dates as UTC

--- api/src/domain/DatetimeMetaTrait.php
+++ api/src/domain/DatetimeMetaTrait.php
@@ -10,11 +10,22 @@ trait DatetimeMetaTrait
 
     public function createdAt() : ?string
     {
-        return $this->created_at;
+        // Timestamps are always converted by MySQL to server time
+        // We need UTC, so convert it back ...
+        $createdAt = new \Carbon\Carbon($this->created_at);
+        $createdAt->timezone('UTC');
+        return $createdAt->toDateTimeString();
     }
 
     public function updatedAt() : ?string
     {
-        return $this->updated_at;
+        if ($this->updated_at) {
+            // Timestamps are always converted by MySQL to server time
+            // We need UTC, so convert it back ...
+            $updatedAt = new \Carbon\Carbon($this->updated_at);
+            $updatedAt->timezone('UTC');
+            return $updatedAt->toDateTimeString();
+        }
+        return null;
     }
 }
