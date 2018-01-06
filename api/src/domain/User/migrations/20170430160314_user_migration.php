3fefd51920ea0afbc871c0e5de288c697ab22813
fbraem <franky.braem@gmail.com>
2018-01-06 21:23:34 +0100

    Create a root user in the migration

--- api/src/domain/User/migrations/20170430160314_user_migration.php
+++ api/src/domain/User/migrations/20170430160314_user_migration.php
@@ -28,11 +28,41 @@ class UserMigration extends AbstractMigration
             ->addTimestamps()
             ->create()
         ;
+
+        $pwd = $this->randomPassword();
+
+        $application = \Core\Clubman::getApplication();
+        $db = $application->getContainer()['db'];
+        //TODO: Use User class
+        $table = new \Zend\Db\TableGateway\TableGateway('users', $db);
+        $table->insert([
+            'email' => $application->getConfig()->email,
+            'password' => password_hash($pwd, PASSWORD_DEFAULT),
+            'remark' => 'Clubman Root User',
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString()
+        ]);
+
+        echo 'Root User', PHP_EOL;
+        echo '---------', PHP_EOL;
+        echo 'Email: ', $application->getConfig()->email, PHP_EOL;
+        echo 'Password: ', $pwd, PHP_EOL;
     }
 
     public function down()
     {
         $this->table('users')->drop();
         $this->table('user_logs')->drop();
+    }
+
+    private function randomPassword()
+    {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
     }
 }
