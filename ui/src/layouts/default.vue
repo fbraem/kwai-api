53bcea27141321b057c77e9277f332c7c20f0b95
fbraem <franky.braem@gmail.com>
2018-01-14 17:36:59 +0100

    Remove fluid

--- ui/src/layouts/default.vue
+++ ui/src/layouts/default.vue
@@ -19,7 +19,7 @@
             </v-btn>
         </v-toolbar>
         <v-content>
-            <v-container fluid>
+            <v-container>
                 <router-view />
             </v-container>
         </v-content>
