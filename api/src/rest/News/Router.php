e82b8ee5b9bd37406eb60c7b5c48b9254e29010d
fbraem <franky.braem@gmail.com>
2018-01-15 19:23:56 +0100

    Use map to set some default values

--- api/src/rest/News/Router.php
+++ api/src/rest/News/Router.php
@@ -6,25 +6,22 @@ class Router extends \Core\Router
 {
     public function createRoutes()
     {
-        $this->map->get('browseStory', '/news/stories', \REST\News\Actions\BrowseStoryAction::class)
+        $this->map->extras(['rest' => 'news'])
             ->accepts(['application/vnd.api+json']);
-        $this->map->get('readStory', '/news/stories/{id}', \REST\News\Actions\ReadStoryAction::class)
-            ->accepts(['application/vnd.api+json']);
+
+        $this->map->get('browseStory', '/news/stories', \REST\News\Actions\BrowseStoryAction::class);
+        $this->map->get('readStory', '/news/stories/{id}', \REST\News\Actions\ReadStoryAction::class);
         $this->map->post('createStory', '/news/stories', \REST\News\Actions\CreateStoryAction::class)
-            ->auth(['login' => true])
-            ->accepts(['application/vnd.api+json']);
+            ->auth(['login' => true]);
         $this->map->patch('updateStory', '/news/stories/{id}', \REST\News\Actions\UpdateStoryAction::class)
-            ->auth(['login' => true])
-            ->accepts(['application/vnd.api+json']);
+            ->auth(['login' => true]);
         $this->map->delete('deleteStory', '/news/stories/{id}', \REST\News\Actions\DeleteStoryAction::class)
-            ->auth(['login' => true])
-            ->accepts(['application/vnd.api+json']);
+            ->auth(['login' => true]);
         $this->map->post('upload', '/news/image/{id}', \REST\News\Actions\UploadAction::class)
-            ->auth(['login' => true]);
-        $this->map->post('uploadEmbeddImage', '/news/embedded_image/{id}', \REST\News\Actions\UploadEmbeddedAction::class)
-            ->auth(['login' => true]);
-        $this->map->get('archive', '/news/archive', \REST\News\Actions\ArchiveAction::class)
-            ->accepts(['application/vnd.api+json'])
-            ->extras(['format' => 'json']);
+            ->auth(['login' => true])
+            ->accepts(['image/*']);
+        //        $this->map->post('uploadEmbeddImage', '/news/embedded_image/{id}', \REST\News\Actions\UploadEmbeddedAction::class)
+        //            ->auth(['login' => true]);
+        $this->map->get('archive', '/news/archive', \REST\News\Actions\ArchiveAction::class);
     }
 }
