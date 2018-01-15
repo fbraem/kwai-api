d728e1f70d440c216e35b20e36c0248137f73ba2
fbraem <franky.braem@gmail.com>
2018-01-15 22:19:10 +0100

    Use commonmark

--- api/src/domain/Content/ContentTransformer.php
+++ api/src/domain/Content/ContentTransformer.php
@@ -3,6 +3,11 @@
 namespace Domain\Content;
 
 use League\Fractal;
+use League\CommonMark\Converter;
+use League\CommonMark\DocParser;
+use League\CommonMark\Environment;
+use League\CommonMark\HtmlRenderer;
+use Webuni\CommonMark\TableExtension\TableExtension;
 
 class ContentTransformer extends Fractal\TransformerAbstract
 {
@@ -10,8 +15,11 @@ class ContentTransformer extends Fractal\TransformerAbstract
     {
         $data = $content->extract();
         if ($content->format() == 'md') {
-            $data['html_summary'] = \Parsedown::instance()->line($data['summary']);
-            $data['html_content'] = \Parsedown::instance()->text($data['content']);
+            $environment = Environment::createCommonMarkEnvironment();
+            $environment->addExtension(new TableExtension());
+            $converter = new Converter(new DocParser($environment), new HtmlRenderer($environment));
+            $data['html_summary'] = $converter->convertToHtml($data['summary']);
+            $data['html_content'] = $converter->convertToHtml($data['content']);
         } else {
             $data['html_summary'] = $data['summary'];
             $data['html_content'] = $data['content'];
