35ff68750481c572a23f3dd8a1f7ace6ebdbdbb3
fbraem <franky.braem@gmail.com>
2018-01-18 20:17:04 +0100

    Prefix route names with "auth."

--- api/src/rest/Auth/Router.php
+++ api/src/rest/Auth/Router.php
@@ -8,11 +8,9 @@ class Router extends \Core\Router
     {
         $this->map->extras(['rest' => 'auth']);
 
-        $this->map->post('token', '/auth/access_token', \REST\Auth\Actions\AccessTokenAction::class);
-        $this->map->post('logout', '/auth/logout', \REST\Auth\Actions\LogoutAction::class)
+        $this->map->post('auth.token', '/auth/access_token', \REST\Auth\Actions\AccessTokenAction::class);
+        $this->map->post('auth.logout', '/auth/logout', \REST\Auth\Actions\LogoutAction::class)
             ->auth(['login' => true]);
-        //->accepts(['application/vnd.api+json'])
-        //->extras(['format' => 'json']);
-        $this->map->get('authorize', '/auth/authorize', \REST\Auth\Actions\AuthorizeAction::class);
+        $this->map->get('auth.authorize', '/auth/authorize', \REST\Auth\Actions\AuthorizeAction::class);
     }
 }
