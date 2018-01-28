214c86f792e2c421aff98d5e32de509dd82aad12
fbraem <franky.braem@gmail.com>
2018-01-28 19:56:15 +0100

    Change text

--- ui/src/site/lang/nl.js
+++ ui/src/site/lang/nl.js
@@ -1,8 +1,8 @@
 const messages = {
     nl : {
         message : {
-            hello : "Deze florerende judoclub bestaat bijna 40 jaar, telt ongeveer 100-150 leden en heeft al een goed gevuld palmares.",
-            info : "Alle informatie betreffende trainingen, contactgegevens, geschiedenis van de club en het judo, foto’s van leden / kampen / tornooien zijn hier terug te vinden."
+            hello : "Deze florerende judoclub bestaat bijna 40 jaar, telt bijna 150 leden en heeft een goed gevuld palmares.",
+            info : "Alle informatie betreffende trainingen, contactgegevens, tornooien, activiteiten en het judo zijn hier terug te vinden."
         }
     }
 };
