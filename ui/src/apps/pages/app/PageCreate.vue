67d7daa38378a35ef67125b804183c8898372194
fbraem <franky.braem@gmail.com>
2018-01-29 20:03:50 +0100

    Wrap with v-container

--- ui/src/apps/pages/app/PageCreate.vue
+++ ui/src/apps/pages/app/PageCreate.vue
@@ -1,21 +1,23 @@
 <template>
-    <v-layout>
-        <v-flex xs12 lg10 offset-lg1>
-            <v-layout row wrap>
-                <v-flex xs12>
-                    <v-toolbar class="elevation-0">
-                        <v-icon>add</v-icon>
-                        <v-toolbar-title>{{ $t('create') }}</v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-btn icon dark color="primary" @click="$router.go(-1)"><v-icon>fa-times</v-icon></v-btn>
-                    </v-toolbar>
-                </v-flex>
-                <v-flex xs12>
-                    <PageForm :page="null" />
-                </v-flex>
-            </v-layout>
-        </v-flex>
-    </v-layout>
+    <v-container>
+        <v-layout>
+            <v-flex xs12 lg10 offset-lg1>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-toolbar class="elevation-0">
+                            <v-icon>add</v-icon>
+                            <v-toolbar-title>{{ $t('create') }}</v-toolbar-title>
+                            <v-spacer></v-spacer>
+                            <v-btn icon dark color="primary" @click="$router.go(-1)"><v-icon>fa-times</v-icon></v-btn>
+                        </v-toolbar>
+                    </v-flex>
+                    <v-flex xs12>
+                        <PageForm :page="null" />
+                    </v-flex>
+                </v-layout>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
