50f6b2c4a709a37a8f9255934b24552da07d4d23
fbraem <franky.braem@gmail.com>
2018-03-04 19:33:08 +0100

    Add login to browse persons

--- api/src/rest/Persons/Router.php
+++ api/src/rest/Persons/Router.php
@@ -7,6 +7,8 @@ class Router extends \Core\Router
     public function createRoutes()
     {
         $this->map->get('persons.browse', '/persons', \REST\Persons\Actions\BrowseAction::class)
-            ->accepts(['application/vnd.api+json']);
+            ->accepts(['application/vnd.api+json'])
+            ->auth(['login' => true]);
+        ;
     }
 }
