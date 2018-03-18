e1afa2b8a7bc5d166cb6538c4863da8b4ea0e2e7
fbraem <franky.braem@gmail.com>
2018-03-18 19:24:39 +0100

    Make use of the CakePHP classes

--- api/src/rest/Users/Actions/BrowseAction.php
+++ api/src/rest/Users/Actions/BrowseAction.php
@@ -9,19 +9,17 @@ use Aura\Payload\Payload;
 use Core\Responders\Responder;
 use Core\Responders\JSONResponder;
 
-use League\Fractal;
-
 class BrowseAction implements \Core\ActionInterface
 {
     public function __invoke(RequestInterface $request, Payload $payload) : ResponseInterface
     {
-        $db = $request->getAttribute('clubman.container')['db'];
-        $usersTable = new \Domain\User\UsersTable($db);
-
-        $users = $usersTable->find();
-
-        $payload->setOutput(new Fractal\Resource\Collection($users, new \Domain\User\UserTransformer, 'users'));
-
-        return (new JSONResponder(new Responder(), $payload))->respond();
+        $users = \Domain\User\UsersTable::getTableFromRegistry()->find()->all();
+        $payload->setOutput(\Domain\User\UserTransformer::createForCollection($users));
+        return (
+            new JSONResponder(
+                new Responder(),
+                $payload
+            )
+        )->respond();
     }
 }
--- api/src/rest/Users/Actions/ReadAction.php
+++ api/src/rest/Users/Actions/ReadAction.php
@@ -9,23 +9,28 @@ use Aura\Payload\Payload;
 use Core\Responders\Responder;
 use Core\Responders\JSONResponder;
 
-use League\Fractal;
-
 class ReadAction implements \Core\ActionInterface
 {
     public function __invoke(RequestInterface $request, Payload $payload) : ResponseInterface
     {
-        $db = $request->getAttribute('clubman.container')['db'];
-        $usersTable = new \Domain\User\UsersTable($db);
-
         $id = $request->getAttribute('route.id');
         try {
-            $user = $usersTable->findOne();
-        } catch (\Domain\NotFoundException $nfe) {
-            return (new NotFoundResponder(new Responder(), _("User doesn't exist.")))->respond();
+            $user = \Domain\User\UsersTable::getTableFromRegistry()->get($id);
+        } catch (\Cake\Datasource\Exception\RecordNotFoundException $rnfe) {
+            return (
+                new NotFoundResponder(
+                    new Responder(),
+                    _("User doesn't exist.")
+                )
+            )->respond();
         }
 
-        $payload->setOutput(new Fractal\Resource\Item($user, new \Domain\User\UserTransformer, 'users'));
-        return (new JSONResponder(new Responder(), $payload))->respond();
+        $payload->setOutput(\Domain\User\UserTransformer::createForItem($user));
+        return (
+            new JSONResponder(
+                new Responder(),
+                $payload
+            )
+        )->respond();
     }
 }
