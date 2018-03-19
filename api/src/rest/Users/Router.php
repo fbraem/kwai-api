6690c0bdc181d3261f67d75f34638f54427b58ea
fbraem <franky.braem@gmail.com>
2018-03-19 21:43:29 +0100

    styling

--- api/src/rest/Users/Router.php
+++ api/src/rest/Users/Router.php
@@ -7,16 +7,14 @@ class Router extends \Core\Router
     public function createRoutes()
     {
         $this->map->extras(['rest' => 'users'])
-            ->accepts(['application/vnd.api+json']);
+            ->accepts(['application/vnd.api+json'])
+        ;
 
         $this->map->get('users.browse', '/users', \REST\Users\Actions\BrowseAction::class)
             ->auth(['login' => true])
-            ->accepts(['application/vnd.api+json']);
+        ;
         $this->map->get('users.read', '/users/{id}', \REST\Users\Actions\ReadAction::class)
             ->auth(['login' => true])
-            ->accepts(['application/vnd.api+json']);
-        $this->map->get('users.news', '/users/news/{id}', \REST\Users\Actions\BrowseNewsAction::class)
-            //->auth(['login' => true])
-            ->accepts(['application/vnd.api+json']);
+        ;
     }
 }
