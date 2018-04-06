30d04e61c216865667741ec6041e22b469663f2c
fbraem <franky.braem@gmail.com>
2018-04-06 22:56:25 +0200

    Add virtual age property

--- api/src/domain/Person/Person.php
+++ api/src/domain/Person/Person.php
@@ -4,13 +4,25 @@ namespace Domain\Person;
 
 class Person extends \Cake\ORM\Entity
 {
+    protected $_virtual = [
+        'age'
+    ];
+
     use \Domain\DatetimeMetaTrait;
 
-    public function _getBirthdate($value)
+    protected function _getBirthdate($value)
     {
         if ($value) {
             return (new \Carbon\Carbon($value))->toDateString();
         }
         return null;
+    }
+
+    // Returns the age of the person at the end of the year
+    protected function _getAge()
+    {
+        $endOfYear = \Carbon\Carbon::now()->endOfYear();
+        $birthDate = new \Carbon\Carbon($this->_properties['birthdate']);
+        return $birthDate->diffInYears($endOfYear);
     }
 }
