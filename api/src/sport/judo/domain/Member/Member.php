607ae9f00ffd6fbb0ad93bfc3ebba9f5df3144da
fbraem <franky.braem@gmail.com>
2018-04-13 20:04:49 +0200

    Use \Cake\I18n\Date and hide person

--- api/src/sport/judo/domain/Member/Member.php
+++ api/src/sport/judo/domain/Member/Member.php
@@ -6,10 +6,12 @@ class Member extends \Cake\ORM\Entity
 {
     use \Domain\DatetimeMetaTrait;
 
+    protected $_hidden = ['person'];
+
     public function _getLicenseEndDate($value)
     {
         if ($value) {
-            return (new \Carbon\Carbon($value))->toDateString();
+            return $value->toDateString();
         }
         return null;
     }
