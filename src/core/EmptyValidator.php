2bfa7cc72fdf582d9df99878347f0a02f5f2602b
fbraem <franky.braem@gmail.com>
2018-08-23 22:40:21 +0200

    A specific validator for empty values

---
+++ src/core/EmptyValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Core;
+
+use Zend\Validator\NotEmpty;
+
+class EmptyValidator extends Validator
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function validate($data)
+    {
+        $this->errors = [];
+
+        foreach ($this->validators as $path => $validator) {
+            $value = \JmesPath\search($path, $data);
+            if (!$validator->isValid($value)) {
+                $messages = [];
+                foreach ($validator->getMessages() as $messageId => $message) {
+                    $pointer = '/' . str_replace('.', '/', $path);
+                    if (!isset($this->errors[$pointer])) {
+                        $this->errors[$pointer] = [];
+                    }
+                    $this->errors[$pointer][] = $message;
+                }
+            }
+        }
+
+        return count($this->errors) == 0;
+    }
+
+    public function addValidator($path, $option = null)
+    {
+        $notEmpty = new NotEmpty($option);
+        $notEmpty->setMessage(_("Value can't be empty"));
+        parent::addValidator($path, $notEmpty);
+    }
+}
