200665b0bac814d4395af42f7eb464c5fd188727
fbraem <franky.braem@gmail.com>
2018-08-24 18:08:08 +0200

    Move validator creation to constructor

--- src/rest/Seasons/SeasonEmptyValidator.php
+++ src/rest/Seasons/SeasonEmptyValidator.php
@@ -7,14 +7,9 @@ class SeasonEmptyValidator extends \Core\EmptyValidator
     public function __construct()
     {
         parent::__construct();
-    }
 
-    public function validate($data)
-    {
         $this->addValidator('data.attributes.name');
         $this->addValidator('data.attributes.start_date', \Zend\Validator\NotEmpty::INTEGER | \Zend\Validator\NotEmpty::NULL);
         $this->addValidator('data.attributes.end_date', \Zend\Validator\NotEmpty::INTEGER | \Zend\Validator\NotEmpty::NULL);
-
-        return parent::validate($data);
     }
 }
