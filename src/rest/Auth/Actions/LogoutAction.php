49eb1237729c2ee99034f16832240c114715fd81
fbraem <franky.braem@gmail.com>
2018-08-29 20:03:11 +0200

    Wrong key used for settings

--- src/rest/Auth/Actions/LogoutAction.php
+++ src/rest/Auth/Actions/LogoutAction.php
@@ -26,7 +26,7 @@ class LogoutAction
 
     public function __invoke(Request $request, Response $response, $args)
     {
-        $encKey = $this->container->get('settings')['encryption_key'];
+        $encKey = $this->container->get('settings')['oauth2']['encryption_key'];
         $this->setEncryptionKey($encKey);
         $server = $this->container->get('authorizationServer');
 
