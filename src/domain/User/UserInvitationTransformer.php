f4f5486829047712e96fae09d10720b0d6c20aa1
fbraem <franky.braem@gmail.com>
2018-08-29 22:30:23 +0200

    Add UserInvitationTransformer

---
+++ src/domain/User/UserInvitationTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Domain\User;
+
+use League\Fractal;
+
+class UserInvitationTransformer extends Fractal\TransformerAbstract
+{
+    private static $type = 'user_invitations';
+
+    public static function createForItem(UserInvitation $invitation)
+    {
+        return new Fractal\Resource\Item($invitation, new self(), self::$type);
+    }
+
+    public static function createForCollection(iterable $users)
+    {
+        return new Fractal\Resource\Collection($users, new self(), self::$type);
+    }
+
+    public function transform(UserInvitation $invitation)
+    {
+        return $invitation->toArray();
+    }
+}
