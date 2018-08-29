a1d1d036ac69fc3bc22fe2ef25def9103017f60b
fbraem <franky.braem@gmail.com>
2018-08-29 22:30:07 +0200

    Add _getExpiredAt

--- src/domain/User/UserInvitation.php
+++ src/domain/User/UserInvitation.php
@@ -4,4 +4,13 @@ namespace Domain\User;
 class UserInvitation extends \Cake\ORM\Entity
 {
     use \Domain\DatetimeMetaTrait;
+
+    public function _getExpiredAt($value)
+    {
+        if ($value) {
+            $date = new \Carbon\Carbon($value, 'UTC');
+            return $date->toDateTimeString();
+        }
+        return null;
+    }
 }
