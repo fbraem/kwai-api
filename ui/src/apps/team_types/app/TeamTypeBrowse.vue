58a82b4c7aee96a424d65da46572b2077f77e5f3
fbraem <franky.braem@gmail.com>
2018-09-23 22:42:59 +0200

    Remove commented code

--- ui/src/apps/team_types/app/TeamTypeBrowse.vue
+++ ui/src/apps/team_types/app/TeamTypeBrowse.vue
@@ -29,20 +29,6 @@
                 </table>
             </div>
         </div>
-<!--
-            <v-card-text v-if="this.types">
-                <v-list v-if="this.types.length > 0" two-line>
-                    <TeamTypeListItem v-for="type in types" :key="type.id" :type="type" />
-                </v-list>
-                <div v-else>
-                    {{ $t('no_types') }}.
-                </div>
-            </v-card-text>
-        </v-card>
-        <v-btn v-if="$isAllowed('create')" icon :to="{ name : 'teamtype.create' }" fab small>
-            <v-icon>fa-plus</v-icon>
-        </v-btn>
--->
     </Page>
 </template>
 
