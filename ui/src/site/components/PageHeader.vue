15a0aa00f4f24ef4cb3fcb6b9f293291dfbebbe7
fbraem <franky.braem@gmail.com>
2018-10-05 19:51:09 +0200

    Add new styles for xl image

--- ui/src/site/components/PageHeader.vue
+++ ui/src/site/components/PageHeader.vue
@@ -2,10 +2,12 @@
     <section class="uk-section uk-section-small uk-section-secondary">
         <div class="uk-container uk-container-expand">
             <div uk-grid>
-                <div v-if="picture" class="uk-width-1-1 uk-width-1-2@m uk-width-2-5@l">
-                    <img :src="picture" />
+                <div v-if="picture" class="uk-width-1-1 uk-width-1-2@m uk-width-2-3@l uk-width-3-5@xl uk-flex uk-flex-middle">
+                    <div>
+                        <img :src="picture" />
+                    </div>
                 </div>
-                <div class="uk-width-1-1" :class="{ 'uk-width-1-2@m' : picture != null, 'uk-width-3-5@l' : picture != null }">
+                <div class="uk-width-1-1" :class="{ 'uk-width-1-2@m' : picture != null, 'uk-width-1-3@l' : picture != null, 'uk-width-2-5@xl' : picture != null }">
                     <slot />
                 </div>
             </div>
