227aeae962b032d8f84cc8eb9613a913b9777e04
fbraem <franky.braem@gmail.com>
2018-11-19 20:23:11 +0100

    Add new migrations

--- src/phinx.php
+++ src/phinx.php
@@ -17,11 +17,12 @@ return [
             __DIR__ . '/domain/Auth/migrations',
             __DIR__ . '/domain/Content/migrations',
             __DIR__ . '/domain/Page/migrations',
-//            __DIR__ . '/domain/Person/migrations',
-//            __DIR__ . '/domain/Game/migrations',
-//            __DIR__ . '/domain/Team/migrations',
-//            __DIR__ . '/domain/Club/migrations',
-//            __DIR__ . '/sport/judo/domain/Member/migrations'
+            __DIR__ . '/domain/Person/migrations',
+            __DIR__ . '/domain/Game/migrations',
+            __DIR__ . '/domain/Team/migrations',
+            __DIR__ . '/domain/Club/migrations',
+            __DIR__ . '/sport/judo/domain/Member/migrations',
+            __DIR__ . '/domain/Training/migrations',
         ]
     ],
     'environments' => $environments
