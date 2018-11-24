82a82fc067690d80bd9e56002846449b9779b590
fbraem <franky.braem@gmail.com>
2018-11-24 19:14:18 +0100

    Hide entities, Add date/time getters

--- src/domain/Training/Event.php
+++ src/domain/Training/Event.php
@@ -5,4 +5,33 @@ namespace Domain\Training;
 class Event extends \Cake\ORM\Entity
 {
     use \Domain\DatetimeMetaTrait;
+
+    protected $_hidden = [ 'season', 'season_id', 'training_definition', 'training_definition_id', "user", "user_id" ];
+
+    public function _getStartDate($value)
+    {
+        if ($value) {
+            $date = new \Carbon\Carbon($value, 'UTC');
+            return $date->toDateString();
+        }
+        return null;
+    }
+
+    public function _getStartTime($value)
+    {
+        if ($value) {
+            $date = new \Carbon\Carbon($value, 'UTC');
+            return $date->toTimeString();
+        }
+        return null;
+    }
+
+    public function _getEndTime($value)
+    {
+        if ($value) {
+            $date = new \Carbon\Carbon($value, 'UTC');
+            return $date->toTimeString();
+        }
+        return null;
+    }
 }
