9db9058b2159f26244ff6254bbae211ad838ae14
fbraem <franky.braem@gmail.com>
2018-11-30 21:53:53 +0100

    Using webpack 4

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,104 +1,142 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const DelWebpackPlugin = require('del-webpack-plugin');
-//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 function resolve(dir) {
-    return path.join(__dirname, '..', dir);
+  return path.join(__dirname, '..', dir);
 }
 
-var environment = process.argv.indexOf('-p') !== -1 ? 'production' : 'development';
+module.exports = (env, argv) => {
 
-var config = {
-    entry : {
-        "vendor" : [
-            "lockr", "moment", "axios", "urijs", "babel-polyfill", "uikit"
-        ],
-        "vue" : [
-            "vuex", "vue", "vue-router", "vuelidate", "vue-kindergarten", "vue-extend-layout", "vue-i18n"
-        ],
-        "site" : "./src/site/main.js"
+  var config = {
+    watch: argv.mode === 'development',
+    mode: argv.mode,
+    entry: {
+      site: './src/site/main.js',
     },
-    output : {
-        path : path.join(__dirname, "build"),
-        filename : "[name].[chunkhash].js",
-        publicPath : "/ui/build/",
-        chunkFilename : 'chunk.[name].[chunkhash].js'
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[name].[chunkhash].js',
+      publicPath: '/ui/build/'
     },
-    module : {
-        loaders : [
-            { test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            { test: /\.vue$/,
-                loader: "vue-loader"
-            },
-            { test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback : "style-loader",
-                    use : "css-loader"
-                })
-            },
-            { test: /\.(woff(2)?|eot|svg|otf|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader',
-                options : {
-                    name : '[name]_[hash].[ext]',
-                    outputPath : 'fonts/'
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            priority: 1
+          },
+          styles: {
+            name: 'styles',
+            test: '\.css$/',
+            enforce: true
+          }
+        },
+        chunks: 'all'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader',
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+          ]
+        },
+        {
+          test: /\.(woff(2)?|eot|svg|otf|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'fonts/'
+          }
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          loaders: [
+            'file-loader?name=assets/[name]_[hash].[ext]&publicPath=ui/build/',
+            {
+              loader: 'image-webpack-loader',
+              query: {
+                bypassOnDebug: true,
+                gifsicle: {
+                  interlaced: false
+                },
+                optipng: {
+                  optimizationLevel: 7
                 }
+              }
             },
-            { test: /\.(png|jpe?g|gif)$/i,
-                loaders: [
-                    "file-loader?name=assets/[name]_[hash].[ext]&publicPath=ui/build/",
-                    {
-                        loader : 'image-webpack-loader',
-                        query : {
-                            bypassOnDebug : true,
-                            gifsicle: {
-                                interlaced : false
-                            },
-                            optipng : {
-                                optimizationLevel : 7
-                            }
-                        }
-                    }
-                ]
-            }
-        ]
+          ]
+        },
+      ]
     },
-    resolve : {
-        extensions : [ '*', '.js', '.vue', '.json' ],
-        alias : {
-            'vue$' : 'vue/dist/vue.common.js',
-            '@' : resolve('ui/src'),
-            'config' : path.join(__dirname, 'src', 'site', 'config', environment)
-        }
+    resolve: {
+      extensions: [ '*', '.js', '.vue', '.json' ],
+      alias: {
+        vue$: 'vue/dist/vue.common.js',
+        '@': resolve('ui/src'),
+        config: path.join(__dirname, 'src', 'site', 'config', argv.mode),
+      }
     },
-    plugins : [
-//        new BundleAnalyzerPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            names : ['vendor', 'vue'],
-            minChunks : Infinity
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name : 'manifest'
-        }),
-        new webpack.ContextReplacementPlugin(
-          /moment[\/\\]locale$/,
-          /nl|en/
-        ),
-        new ExtractTextPlugin("style.css"),
-        new HtmlWebpackPlugin({
-            filename: '../../index.html',
-            template: 'src/index.template.html'
-        }),
-        new DelWebpackPlugin({
-            keepGeneratedAssets: true,
-            exclude : [ "assets/**", "fonts/**" ]
-        })
+    plugins: [
+      new CleanWebpackPlugin([
+        path.join(__dirname, 'build'),
+      ],
+      {
+        verbose: true
+      }),
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+        chunkFilename: '[name].[hash].css',
+        publicPath: './build',
+      }),
+      //  new BundleAnalyzerPlugin(),
+      /*
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'vue'],
+        minChunks: Infinity
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest'
+      }),
+      */
+      new webpack.ContextReplacementPlugin(
+        /moment[\/\\]locale$/,
+        /nl|en/
+      ),
+      new HtmlWebpackPlugin({
+        filename: '../../index.html',
+        template: 'src/index.template.html'
+      }),
     ]
-};
+  };
 
-module.exports = config;
+  return config;
+};
