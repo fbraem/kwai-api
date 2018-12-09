186bf4ec57610f1272d5365fd5bb38b6c8050afe
fbraem <franky.braem@gmail.com>
2018-12-09 20:44:34 +0100

    Add getters for time attributes

--- src/domain/Training/Definition.php
+++ src/domain/Training/Definition.php
@@ -7,4 +7,22 @@ class Definition extends \Cake\ORM\Entity
     use \Domain\DatetimeMetaTrait;
 
     protected $_hidden = ['season_id', 'season', 'user_id', 'user', 'training_events'];
+
+    public function _getStartTime($value)
+    {
+        if ($value) {
+            $date = new \Carbon\Carbon($value, 'UTC');
+            return $date->toTimeString();
+        }
+        return null;
+    }
+
+    public function _getEndTime($value)
+    {
+        if ($value) {
+            $date = new \Carbon\Carbon($value, 'UTC');
+            return $date->toTimeString();
+        }
+        return null;
+    }
 }
