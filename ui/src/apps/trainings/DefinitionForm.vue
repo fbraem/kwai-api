fcca4df574c556a678fdef5fddc8f2fc6093fe6c
fbraem <franky.braem@gmail.com>
2018-12-22 22:51:57 +0100

    Correct translations

--- ui/src/apps/trainings/DefinitionForm.vue
+++ ui/src/apps/trainings/DefinitionForm.vue
@@ -3,8 +3,8 @@
   <div>
     <PageHeader>
       <h1>{{ $t('training.definitions.title') }}</h1>
-      <h3 v-if="creating" class="uk-h3 uk-margin-remove">{{ $t('create') }}</h3>
-      <h3 v-else class="uk-h3 uk-margin-remove">{{ $t('update') }}</h3>
+      <h3 v-if="creating" class="uk-h3 uk-margin-remove">{{ $t('training.definitions.create') }}</h3>
+      <h3 v-else class="uk-h3 uk-margin-remove">{{ $t('training.definitions.update') }}</h3>
     </PageHeader>
     <section class="uk-section uk-section-small uk-container uk-container-expand">
       <div uk-grid>
