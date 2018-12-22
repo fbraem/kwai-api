f793151311cf0791dcd4dc3e51828b095cb34645
fbraem <franky.braem@gmail.com>
2018-12-22 22:50:11 +0100

    Add docs and use arguments instead of objects in an array

--- ui/src/stores/mixin.js
+++ ui/src/stores/mixin.js
@@ -1,10 +1,32 @@
-export default function(modules) {
+/**
+ * Defines a function that can be used to create a Vue mixin for registering
+ * Vuex modules.
+ * @param { ...object} module - A object representing a module. Property order
+ * is important!
+ * @returns A mixin object for Vue.
+ *
+ * The following example will register training/coach module if it is not yet
+ * registered.
+ * @example
+ * import registerModule from 'mixin';
+ * registerModule({
+ *  training: trainingStore,
+ *  coach: coachStore
+ * });
+  */
+export default function(...modules) {
   return {
     beforeCreate() {
       modules.forEach((module) => {
-        if (!this.$store.state[module.namespace]) {
-          this.$store.registerModule(module.namespace, module.store);
-        }
+        var namespaces = [];
+        var state = this.$store.state;
+        Reflect.ownKeys(module).forEach((namespace) => {
+          namespaces.push(namespace);
+          if (!state[namespace]) {
+            this.$store.registerModule(namespaces, module[namespace]);
+          }
+          state = state[namespace];
+        });
       });
     }
   };
