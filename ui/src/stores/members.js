1c56a6a1da272fd35ec7ac29caf059f2f7f2dadc
fbraem <franky.braem@gmail.com>
2018-12-22 22:50:33 +0100

    Make it possible to search on name

--- ui/src/stores/members.js
+++ ui/src/stores/members.js
@@ -46,8 +46,15 @@ const mutations = {
 
 const actions = {
   async browse({ dispatch, commit }, payload) {
+    payload = payload || {};
+
     dispatch('wait/start', 'members.browse', { root: true });
+
     const member = new Member();
+    if (payload.name) {
+      member.where('name', payload.name);
+    }
+
     try {
       let members = await member.get();
       commit('members', members);
@@ -57,111 +64,6 @@ const actions = {
       dispatch('wait/end', 'members.browse', { root: true });
       throw error;
     }
-
-    /*
-        dispatch('wait/start', 'members.browse', { root : true });
-        var uri = new URI('api/sport/judo/members');
-        var offset = payload.offset || 0;
-        uri.addQuery('page[offset]', offset);
-/*
-        if (payload.category) {
-            uri.addQuery('filter[category]', payload.category);
-        }
-        if (payload.year) {
-            uri.addQuery('filter[year]', payload.year);
-        }
-        if (payload.month) {
-            uri.addQuery('filter[month]', payload.month);
-        }
-        if (payload.featured) {
-            uri.addQuery('filter[featured]', true);
-        }
-        if (payload.user) {
-            uri.addQuery('filter[user]', payload.user);
-        }
-        oauth.get(uri.href(), {
-            data : payload
-        }).then((res) => {
-            var api = new JSONAPI();
-            var members = api.parse(res.data);
-            context.commit('members', {
-                members : members.data
-            });
-            context.commit('success');
-        }).catch((error) => {
-            context.commit('error', error);
-        });
-*/
-  },
-  read(context, payload) {
-    context.commit('loading');
-    var member = context.getters['member'](payload.id);
-    if (member) { // already read
-      context.commit('success');
-      return;
-    }
-
-    oauth.get('api/members/' + payload.id, {
-      data: payload,
-    }).then((res) => {
-      var api = new JSONAPI();
-      var result = api.parse(res.data);
-      context.commit('setMember', {
-        member: result.data,
-      });
-      context.commit('success');
-    }).catch((error) => {
-      context.commit('error', error);
-    });
-  },
-  create(context, payload) {
-    context.commit('loading');
-    return oauth.post('api/members', {
-      data: payload,
-    }).then((res) => {
-      var api = new JSONAPI();
-      var result = api.parse(res.data);
-      context.commit('setMember', {
-        member: result.data,
-      });
-      context.commit('success');
-      return result.data;
-    }).catch((error) => {
-      context.commit('error', error);
-    });
-  },
-  update(context, payload) {
-    context.commit('loading');
-    return new Promise((resolve, reject) => {
-      oauth.patch('api/members/' + payload.data.id, {
-        data: payload,
-      }).then((res) => {
-        var api = new JSONAPI();
-        var result = api.parse(res.data);
-        context.commit('setMember', {
-          member: result.data,
-        });
-        context.commit('success');
-        resolve();
-      }).catch((error) => {
-        context.commit('error', error);
-        reject();
-      });
-    });
-  },
-  delete(context, payload) {
-    context.commit('loading');
-    return new Promise((resolve, reject) => {
-      oauth.delete('api/members/' + payload.id)
-        .then((res) => {
-          context.commit('deleteMember', { id: payload.id });
-          context.commit('success');
-          resolve();
-        }).catch((error) => {
-          context.commit('error', error);
-          reject();
-        });
-    });
   },
 };
 
