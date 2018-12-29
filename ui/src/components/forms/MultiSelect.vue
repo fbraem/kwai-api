1d4d5bebb488550981f3eab4f6ce67875bf8c11b
fbraem <franky.braem@gmail.com>
2018-12-29 19:17:06 +0100

    Add vue-multiselect

---
+++ ui/src/components/forms/MultiSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <multiselect v-model="field.value" :id="id"
+    placeholder="" v-bind="$attrs" :required="field.required">
+    <template v-if="field.placeholder" slot="placeholder">
+      {{ field.placeholder }}
+    </template>
+  </multiselect>
+</template>
+
+<style>
+  input:required {
+    border-left: 3px solid;
+  }
+</style>
+
+<script>
+import Multiselect from 'vue-multiselect';
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+
+export default {
+  components: {
+    Multiselect
+  },
+  inject: {
+    field: 'field',
+    id: 'id'
+  }
+};
+</script>
