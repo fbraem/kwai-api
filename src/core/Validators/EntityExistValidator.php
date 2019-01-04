0d3a90bd9d43e732fd2a6590b649c8ea6e9db2fe
fbraem <franky.braem@gmail.com>
2019-01-04 18:44:27 +0100

    Add isAssoc to distinguish between numeric or associative arrays

--- src/core/Validators/EntityExistValidator.php
+++ src/core/Validators/EntityExistValidator.php
@@ -26,10 +26,22 @@ class EntityExistValidator implements ValidatorInterface
 
         $relationshipData = \JmesPath\search($this->path . '.data', $data);
         if (isset($relationshipData)) {
-            try {
-                return $this->table->get($relationshipData['id']);
-            } catch (RecordNotFoundException $rnfe) {
-                $errors['/' . str_replace('.', '/', $this->path)] = end($paths) . " doesn't exist";
+            if ($this->isAssoc($relationshipData)) {
+                try {
+                    return $this->table->get($relationshipData['id']);
+                } catch (RecordNotFoundException $rnfe) {
+                    $errors['/' . str_replace('.', '/', $this->path)] = end($paths) . " doesn't exist";
+                }
+            } else {
+                $result = [];
+                foreach ($relationshipData as $relation) {
+                    try {
+                        $result[] = $this->table->get($relation['id']);
+                    } catch (RecordNotFoundException $rnfe) {
+                        $errors['/' . str_replace('.', '/', $this->path) . '/' . $relation['id']] = end($paths) . " doesn't exist";
+                    }
+                }
+                return $result;
             }
         } elseif ($this->required) {
             $errors['/' . str_replace('.', '/', $this->path)] = end($paths) . " is missing";
@@ -38,5 +50,15 @@ class EntityExistValidator implements ValidatorInterface
         if (count($errors) > 0) {
             throw new ValidationException($errors);
         }
+    }
+
+    private function isAssoc($array)
+    {
+        foreach ($array as $key => $value) {
+            if ($key !== (int) $key) {
+                return true;
+            }
+        }
+        return false;
     }
 }
