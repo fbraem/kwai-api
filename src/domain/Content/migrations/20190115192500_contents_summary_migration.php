228581beb7c99eef782b8a4b7a47e24c48c72eaf
fbraem <franky.braem@gmail.com>
2019-01-15 20:10:29 +0100

    Summary is required, Content is optional

---
+++ src/domain/Content/migrations/20190115192500_contents_summary_migration.php
@@ -0,0 +1,27 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Summary -> not null, content -> null
+ */
+class ContentsSummaryMigration extends AbstractMigration
+{
+    public function up()
+    {
+        $this->table('contents')
+            ->changeColumn('summary', 'text', ['null' => false])
+            ->changeColumn('content', 'text', ['null' => true])
+            ->save()
+        ;
+    }
+
+    public function down()
+    {
+        $this->table('contents')
+            ->changeColumn('summary', 'text', ['null' => true])
+            ->changeColumn('content', 'text', ['null' => false])
+            ->save()
+        ;
+    }
+}
