559d799a2e97dda046deb35203bbd50e27f21266
fbraem <franky.braem@gmail.com>
2019-02-04 20:20:13 +0100

    Use ArrayAttribute and ObjectAttribute

--- ui/src/models/trainings/Training.js
+++ ui/src/models/trainings/Training.js
@@ -1,49 +1,97 @@
 import Model from '../Model';
-import { DateAttribute } from '../Attribute';
+import {
+  Attribute, DateAttribute, ObjectAttribute, ArrayAttribute
+} from '../Attribute';
 
 import Season from '../Season';
 import User from '../User';
 import Team from '../Team';
 import Definition from './Definition';
 import Coach from './Coach';
-import Event from '@/models/Event';
 
 /**
- * Event model
+ * Training model
  */
 export default class Training extends Model {
+  /**
+   * The JSONAPI type
+   */
   static type() {
     return 'trainings';
   }
 
+  /**
+   * The JSONAPI attributes
+   */
   static fields() {
     return {
+      event: new ObjectAttribute({
+        start_date: new DateAttribute('YYYY-MM-DD HH:mm:ss'),
+        end_date: new DateAttribute('YYYY-MM-DD HH:mm:ss'),
+        contents: new ArrayAttribute(
+          new ObjectAttribute({
+            locale: new Attribute(),
+            title: new Attribute(),
+            summary: new Attribute(),
+            content: new Attribute(),
+            created_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true)
+          })
+        )
+      }),
       created_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true),
       updated_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true),
     };
   }
 
+  /**
+   * The JSONAPI relationships
+   */
   static relationships() {
     return {
       season: Season,
       teams: Team,
       coaches: Coach,
       definition: Definition,
-      event: Event,
       user: User,
     };
   }
 
+  /**
+   * Computed values
+   */
   static computed() {
     return {
-      formattedStartDate(event) {
-        return event.start_date.format('L');
+      /**
+       * Returns a formatted start date of the event
+       * @param {Training} training A training model
+       */
+      formattedStartDate(training) {
+        return training.event.start_date.format('L');
       },
-      formattedStartTime(event) {
-        return event.start_time.format('HH:mm');
+      /**
+       * Returns a formatted starttime of the event
+       * @param {Training} training A training model
+       */
+      formattedStartTime(training) {
+        return training.event.start_date.format('HH:mm');
       },
-      formattedEndTime(event) {
-        return event.end_time.format('HH:mm');
+      /**
+       * Returns a formatted endtime of the event
+       * @param {Training} training A training model
+       */
+      formattedEndTime(training) {
+        return training.event.end_date.format('HH:mm');
+      },
+      /**
+       * Returns the content of the event. For now the first content is
+       * returned.
+       * @param {Training} training A training model
+       * @todo Return the content belonging to the current locale
+       */
+      content(training) {
+        if (training.event.contents && training.event.contents.length > 0) {
+          return training.event.contents[0];
+        }
       }
     };
   }
