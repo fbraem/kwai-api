a5abfd82ab8acb02f9f297aab21536a97621e018
fbraem <franky.braem@gmail.com>
2019-02-08 23:19:54 +0100

    Hide some data

--- src/domain/Training/TrainingCoach.php
+++ src/domain/Training/TrainingCoach.php
@@ -5,4 +5,11 @@ namespace Domain\Training;
 class TrainingCoach extends \Cake\ORM\Entity
 {
     use \Domain\DatetimeMetaTrait;
+
+    protected $_hidden = [
+        'user',
+        'user_id',
+        'coach_id',
+        'training_id'
+    ];
 }
