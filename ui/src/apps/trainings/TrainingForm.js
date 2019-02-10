d1e7a835ccae3511e1afcace471cbdebef4ea6bb
fbraem <franky.braem@gmail.com>
2019-02-10 16:25:55 +0100

    Add training form

---
+++ ui/src/apps/trainings/TrainingForm.js
@@ -0,0 +1,164 @@
+import VueForm, { notEmpty, isDate, isTime } from '@/js/VueForm';
+
+import Season from '@/models/Season';
+
+import moment from 'moment';
+
+export default {
+  mixins: [ VueForm ],
+  form() {
+    return {
+      title: {
+        value: '',
+        required: true,
+        validators: [
+          {
+            v: notEmpty,
+            error: this.$t('training.events.form.title.required'),
+          },
+        ]
+      },
+      summary: {
+        value: '',
+        required: true,
+        validators: [
+          {
+            v: notEmpty,
+            error: this.$t('training.events.form.summary.required'),
+          },
+        ]
+      },
+      season: {
+        value: 0
+      },
+      coaches: {
+        value: []
+      },
+      team: {
+        value: 0
+      },
+      start_date: {
+        value: '',
+        required: true,
+        validators: [
+          {
+            v: notEmpty,
+            error: this.$t('training.events.form.start_date.required')
+          },
+          {
+            v: isDate,
+            error: this.$t('training.events.form.start_date.invalid')
+          },
+        ]
+      },
+      start_time: {
+        value: '',
+        required: true,
+        validators: [
+          {
+            v: notEmpty,
+            error: this.$t('training.events.form.start_time.required')
+          },
+          {
+            v: isTime,
+            error: this.$t('training.events.form.start_time.invalid')
+          },
+        ]
+      },
+      end_time: {
+        value: '',
+        required: true,
+        validators: [
+          {
+            v: notEmpty,
+            error: this.$t('training.events.form.end_time.required')
+          },
+          {
+            v: isTime,
+            error: this.$t('training.events.form.end_time.invalid')
+          },
+        ]
+      },
+      active: {
+        value: true
+      },
+      cancelled: {
+        value: false
+      },
+      location: {
+        value: null
+      },
+      remark: {
+        value: ''
+      }
+    };
+  },
+  validations() {
+    return [
+      () => {
+        var start = moment(this.form.start_time.value, 'HH:mm', true);
+        var end = moment(this.form.end_time.value, 'HH:mm', true);
+        if (end.isAfter(start)) {
+          return true;
+        }
+        this.form.end_time.errors = [
+          this.$t('training.events.form.end_time.after'),
+        ];
+        return false;
+      },
+    ];
+  },
+  methods: {
+    writeForm(training) {
+      this.form.title.value = training.content.title;
+      this.form.summary.value = training.content.summary;
+      this.form.active.value = training.event.active;
+      this.form.cancelled.value = training.event.cancelled;
+      this.form.location.value = training.event.location;
+      this.form.start_date.value = training.formattedStartDate;
+      this.form.start_time.value = training.formattedStartTime;
+      this.form.end_time.value = training.formattedEndTime;
+      this.form.coaches.value = training.coaches;
+      if (training.season) {
+        this.form.season.value = training.season.id;
+      }
+/* TODO
+      if (training.teams) {
+      }
+*/
+      this.form.remark.value = training.event.remark;
+    },
+    readForm(training) {
+      if (!training.event) {
+        training.event = Object.create(null);
+        training.event.contents = [ Object.create(null) ];
+      }
+      training.event.contents[0].title = this.form.title.value;
+      training.event.contents[0].summary = this.form.summary.value;
+      training.event.active = this.form.active.value;
+      training.event.cancelled = this.form.cancelled.value;
+      training.event.location = this.form.location.value;
+      training.event.time_zone = moment.tz.guess();
+      const date = moment(this.form.start_date.value, 'L', true);
+      const startTime = moment(this.form.start_time.value, 'HH:mm', true);
+      training.event.start_date = date.clone();
+      training.event.start_date.hours(startTime.hours());
+      training.event.start_date.minutes(startTime.minutes());
+      training.event.end_date = date.clone();
+      const endTime = moment(this.form.end_time.value, 'HH:mm', true);
+      training.event.end_date.hours(endTime.hours());
+      training.event.end_date.minutes(endTime.minutes());
+      training.event.remark = this.form.remark.value;
+      training.coaches = this.form.coaches.value;
+      if (this.form.season.value) {
+        if (this.form.season.value === 0) {
+          training.season = null;
+        } else {
+          training.season = new Season();
+          training.season.id = this.form.season.value;
+        }
+      }
+      return training;
+    }
+  }
+};
---
+++ ui/src/apps/trainings/TrainingForm.vue
@@ -0,0 +1,210 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div>
+    <PageHeader>
+      <h1>{{ $t('training.events.title') }}</h1>
+      <h3 v-if="creating" class="uk-h3 uk-margin-remove">{{ $t('training.events.create') }}</h3>
+      <h3 v-else class="uk-h3 uk-margin-remove">{{ $t('training.events.update') }}</h3>
+    </PageHeader>
+    <section class="uk-section uk-section-small uk-container uk-container-expand">
+      <div uk-grid>
+        <div class="uk-width-1-1">
+          <form class="uk-form-stacked">
+            <div uk-grid>
+              <div class="uk-width-expand">
+                <field name="title" :label="$t('training.events.form.title.label')">
+                  <uikit-input-text :placeholder="$t('training.events.form.title.placeholder')" />
+                </field>
+              </div>
+              <div>
+                <field name="active" :label="$t('training.events.form.active.label')">
+                  <uikit-switch />
+                </field>
+              </div>
+              <div>
+                <field name="cancelled" :label="$t('training.events.form.cancelled.label')">
+                  <uikit-switch />
+                </field>
+              </div>
+            </div>
+            <field name="summary" :label="$t('training.events.form.summary.label')">
+              <uikit-textarea :rows="5" :placeholder="$t('training.events.form.summary.placeholder')" />
+            </field>
+            <div class="uk-child-width-1-3" uk-grid>
+              <div>
+                <field name="start_date" :label="$t('training.events.form.start_date.label')">
+                  <uikit-input-text :placeholder="$t('training.events.form.start_date.placeholder')" />
+                </field>
+              </div>
+              <div>
+                <field name="start_time" :label="$t('training.events.form.start_time.label')">
+                  <uikit-input-text :placeholder="$t('training.events.form.start_time.placeholder')" />
+                </field>
+              </div>
+              <div>
+                <field name="end_time" :label="$t('training.events.form.end_time.label')">
+                  <uikit-input-text :placeholder="$t('training.events.form.end_time.placeholder')" />
+                </field>
+              </div>
+            </div>
+            <field name="season" :label="$t('training.events.form.season.label')">
+              <uikit-select :items="seasons" />
+            </field>
+            <field name="coaches" :label="$t('training.events.form.coaches.label')">
+              <multiselect :options="coaches" label="name"
+                track-by="id" :multiple="true" :close-on-select="false"
+                :selectLabel="$t('training.events.form.coaches.selectLabel')"
+                :deselectLabel="$t('training.events.form.coaches.deselectLabel')">
+              </multiselect>
+            </field>
+            <field name="remark" :label="$t('training.events.form.remark.label')">
+              <uikit-textarea :rows="5" :placeholder="$t('training.events.form.remark.placeholder')" />
+            </field>
+          </form>
+        </div>
+        <div uk-grid class="uk-width-1-1">
+          <div class="uk-width-expand">
+          </div>
+          <div class="uk-width-auto">
+            <button class="uk-button uk-button-primary" :disabled="!$valid" @click="submit">
+              <i class="fas fa-save"></i>&nbsp; {{ $t('save') }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import trainingStore from '@/stores/training';
+import coachStore from '@/stores/training/coaches';
+import seasonStore from '@/stores/seasons';
+import teamStore from '@/stores/teams';
+import registerModule from '@/stores/mixin';
+
+import Training from '@/models/trainings/Training';
+
+import PageHeader from '@/site/components/PageHeader.vue';
+import Field from '@/components/forms/Field.vue';
+import UikitInputText from '@/components/forms/InputText.vue';
+import UikitTextarea from '@/components/forms/Textarea.vue';
+import UikitSelect from '@/components/forms/Select.vue';
+import UikitSwitch from '@/components/forms/Switch.vue';
+import Multiselect from '@/components/forms/MultiSelect.vue';
+
+import messages from './lang';
+
+import TrainingForm from './TrainingForm';
+
+export default {
+  components: {
+    PageHeader,
+    Field,
+    UikitInputText,
+    UikitSwitch,
+    UikitSelect,
+    UikitTextarea,
+    Multiselect
+  },
+  i18n: messages,
+  mixins: [
+    TrainingForm,
+    registerModule(
+      {
+        training: trainingStore
+      },
+      {
+        training: trainingStore,
+        coach: coachStore,
+      },
+      {
+        season: seasonStore
+      },
+      {
+        team: teamStore
+      }
+    ),
+  ],
+  data() {
+    return {
+      training: new Training()
+    };
+  },
+  computed: {
+    creating() {
+      return this.training != null && this.training.id == null;
+    },
+    error() {
+      return this.$store.state.training.error;
+    },
+    seasons() {
+      var seasons = this.$store.getters['season/seasonsAsOptions'];
+      seasons.unshift({
+        value: 0,
+        text: this.$t('training.events.form.season.no_season')
+      });
+      return seasons;
+    },
+    coaches() {
+      var coaches = this.$store.state.training.coach.coaches;
+      return coaches || [];
+    }
+/*
+    teams() {
+      var teams = this.$store.getters['team/teamsAsOptions'];
+      teams.unshift({
+        value: 0,
+        text: this.$t('training.events.form.team.no_team')
+      });
+      return teams;
+    },
+*/
+  },
+  async created() {
+    this.$store.dispatch('season/browse');
+    this.$store.dispatch('training/coach/browse');
+//    await this.$store.dispatch('team/browse');
+  },
+  beforeRouteEnter(to, from, next) {
+    next(async(vm) => {
+      if (to.params.id) await vm.fetchData(to.params.id);
+      next();
+    });
+  },
+  watch: {
+    error(nv) {
+      if (nv) {
+        if (nv.response.status === 422) {
+          this.handleErrors(nv.response.data.errors);
+        } else if (nv.response.status === 404){
+          // this.error = err.response.statusText;
+        } else {
+          // TODO: check if we can get here ...
+          console.log(nv);
+        }
+      }
+    }
+  },
+  methods: {
+    async fetchData(id) {
+      this.training
+        = await this.$store.dispatch('training/read', {
+          id: id
+        });
+      this.writeForm(this.training);
+    },
+    submit() {
+      this.clearErrors();
+      this.readForm(this.training);
+      this.$store.dispatch('training/save', this.training)
+        .then((newTraining) => {
+          this.$router.push({
+            name: 'trainings.read',
+            params: { id: newTraining.id }
+          });
+        });
+    }
+  }
+};
+</script>
