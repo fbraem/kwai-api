f0f881b4f7777241afb4c01a0cc0292b7f1a9cab
fbraem <franky.braem@gmail.com>
2019-03-01 18:38:37 +0100

    Styling

--- ui/src/apps/categories/components/CategoryList.vue
+++ ui/src/apps/categories/components/CategoryList.vue
@@ -3,21 +3,36 @@
   <div uk-grid class="uk-margin-left uk-grid-small uk-child-width-1-1">
     <div>
       <h4 class="uk-heading-line uk-text-bold">
-        <span>{{ $t('overview') }}</span>
+        <span>
+          {{ $t('overview') }}
+        </span>
       </h4>
     </div>
     <div>
       <ul class="uk-list uk-list-divider">
-        <li v-for="(category, index) in categories" :key="index">
+        <li
+          v-for="(category, index) in categories"
+          :key="index"
+        >
           <div class="uk-flex uk-flex-middle uk-inline" uk-grid>
-            <div v-if="category.icon_picture" class="uk-width-auto">
-              <img :src="category.icon_picture"
+            <div
+              v-if="category.icon_picture"
+              class="uk-width-auto"
+            >
+              <img
+                :src="category.icon_picture"
                 style="color:#c61c18;height:32px;"
-                width="40" height="32" uk-svg />
+                width="40" height="32" uk-svg
+              />
             </div>
             <div class="uk-width-expand">
-              <router-link :to="{ name : 'categories.read', params : { id : category.id }}" class="uk-link-reset uk-position-cover uk-position-z-index uk-margin-remove-adjacent" />
-              <h4 class="uk-h4 uk-margin-remove">{{ category.name }}</h4>
+              <router-link
+                :to="{ name : 'categories.read', params : { id : category.id }}"
+                class="uk-link-reset uk-position-cover uk-position-z-index uk-margin-remove-adjacent"
+              />
+              <h4 class="uk-h4 uk-margin-remove">
+                {{ category.name }}
+              </h4>
               <div class="uk-text-meta">
                 {{ category.short_description }}
               </div>
