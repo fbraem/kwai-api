cf486ea9279ea0e65d4d5d59ee103ff75e8e1566
fbraem <franky.braem@gmail.com>
2019-03-02 17:10:50 +0100

    Style and documentation

--- ui/src/apps/categories/components/CategoryCard.vue
+++ ui/src/apps/categories/components/CategoryCard.vue
@@ -1,12 +1,25 @@
 <template>
   <!-- eslint-disable max-len -->
   <div class="uk-margin-left uk-margin-right">
-    <div class="uk-card uk-card-hover uk-card-body" uk-scrollspy="cls:uk-animation-slide-top-small">
-      <router-link :to="{ name : 'categories.read', params : { id : category.id } }"
-        class="uk-link-reset uk-position-cover uk-position-z-index uk-margin-remove-adjacent" />
-      <img v-if="category.icon_picture" :src="category.icon_picture"
-        style="color:#c61c18;" width="48" height="48" uk-svg />
-      <h4 class="uk-margin">{{ category.name }}</h4>
+    <div
+      class="uk-card uk-card-hover uk-card-body"
+      uk-scrollspy="cls:uk-animation-slide-top-small"
+    >
+      <router-link
+        :to="linkToCategory"
+        class="uk-link-reset uk-position-cover uk-position-z-index uk-margin-remove-adjacent"
+      />
+      <img
+        v-if="category.icon_picture"
+        :src="category.icon_picture"
+        style="color:#c61c18;"
+        width="48"
+        height="48"
+        uk-svg
+      />
+      <h4 class="uk-margin">
+        {{ category.name }}
+      </h4>
       <div class="uk-margin">
         {{ category.short_description }}
       </div>
@@ -20,9 +33,22 @@
  */
 export default {
   props: {
+    /**
+     * The category to show in the card.
+     */
     category: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    linkToCategory() {
+      return {
+        name: 'categories.read',
+        params: {
+          id: this.category.id
+        }
+      };
     }
   }
 };
