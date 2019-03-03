269ebedae167b8ef4db571b01990b67007b42f72
fbraem <franky.braem@gmail.com>
2019-03-03 22:22:31 +0100

    Styling

--- ui/src/apps/categories/CategoryForm.vue
+++ ui/src/apps/categories/CategoryForm.vue
@@ -3,16 +3,28 @@
   <div uk-grid>
     <div class="uk-width-1-1">
       <KwaiForm :form="form" :error="error">
-        <KwaiField name="name" :label="$t('form.name.label')">
+        <KwaiField
+          name="name"
+          :label="$t('form.name.label')"
+        >
           <KwaiInputText :placeholder="$t('form.name.placeholder')" />
         </KwaiField>
-        <KwaiField name="short_description" :label="$t('form.short_description.label')">
+        <KwaiField
+          name="short_description"
+          :label="$t('form.short_description.label')"
+        >
           <KwaiTextarea :placeholder="$t('form.short_description.placeholder')" />
         </KwaiField>
-        <KwaiField name="description" :label="$t('form.description.label')">
+        <KwaiField
+          name="description"
+          :label="$t('form.description.label')"
+        >
           <KwaiTextarea :placeholder="$t('form.description.placeholder')" />
         </KwaiField>
-        <KwaiField name="remark" :label="$t('form.remark.label')">
+        <KwaiField
+          name="remark"
+          :label="$t('form.remark.label')"
+        >
           <KwaiTextarea :placeholder="$t('form.remark.placeholder')" />
         </KwaiField>
       </KwaiForm>
