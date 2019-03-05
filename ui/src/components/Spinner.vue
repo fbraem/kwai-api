a0e397f0d891ad56316290e578065d0b416ccff3
fbraem <franky.braem@gmail.com>
2019-03-05 19:43:47 +0100

    Add a spinner component

---
+++ ui/src/components/Spinner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="uk-flex-center" uk-grid>
+    <div class="uk-text-center">
+      <i class="fas fa-spinner fa-2x fa-spin"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
