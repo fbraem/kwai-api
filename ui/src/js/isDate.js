c9e361b547c7d41ffdb2cbc1132641a0438ae5ec
fbraem <franky.braem@gmail.com>
2019-03-11 20:19:47 +0100

    Remove unused code

--- ui/src/js/isDate.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-export default date => {
-  return moment(date, 'L', true).isValid();
-};
--- ui/src/js/isTime.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-export default time => {
-  return moment(time, 'HH:mm', true).isValid();
-};
