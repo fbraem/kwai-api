0766b38c7bb4c998fa4420186d380684e3c391d6
fbraem <franky.braem@gmail.com>
2019-03-14 20:11:44 +0100

    Add KwaiCheckBox

---
+++ ui/src/components/forms/KwaiCheckbox.vue
@@ -0,0 +1,18 @@
+<template>
+    <input v-model="field.value"
+        :id="id"
+        class="uk-checkbox"
+        type="checkbox"
+        :class="{ 'uk-form-danger' : field.errors.length > 0 }"
+        v-bind="$attrs"
+    />
+</template>
+
+<script>
+export default {
+  inject: {
+    field: 'field',
+    id: 'id'
+  }
+};
+</script>
