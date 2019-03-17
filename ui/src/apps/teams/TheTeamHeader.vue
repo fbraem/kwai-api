043fba70667d8a424b47a1479e2ac6d348b7e40c
fbraem <franky.braem@gmail.com>
2019-03-17 13:47:06 +0100

    Created a Header component

--- ui/src/apps/teams/TheTeamHeader.vue
+++ ui/src/apps/teams/TheTeamHeader.vue
@@ -1,50 +1,50 @@
 <template>
-  <!-- eslint-disable max-len -->
-  <div class="uk-grid">
-    <div class="uk-width-1-1@s uk-width-5-6@m">
-      <h1 class="uk-h1">
-        {{ $t('teams') }}
-      </h1>
-      <h3
-        v-if="team"
-        class="uk-h3 uk-margin-remove"
-      >
-        {{ team.name }}
-      </h3>
-    </div>
-    <div class="uk-width-1-1@s uk-width-1-6@m">
-      <div class="uk-flex uk-flex-right">
-        <div>
-          <router-link
-            class="uk-icon-button uk-link-reset"
-            :to="{ 'name' : 'teams.browse' }"
-          >
-            <i class="fas fa-list"></i>
-          </router-link>
-        </div>
-        <div class="uk-margin-small-left">
-          <router-link
-            v-if="team && $can('update', team)"
-            class="uk-icon-button uk-link-reset"
-            :to="{ 'name' : 'teams.update', params : { id : team.id } }"
-          >
-            <i class="fas fa-edit"></i>
-          </router-link>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Header
+    :title="$t('teams')"
+    :subtitle="subtitle"
+    :toolbar="toolbar"
+  />
 </template>
 
 <script>
 import messages from './lang';
 
+import Header from '@/components/Header';
+
 export default {
   i18n: messages,
+  components: {
+    Header
+  },
   computed: {
     team() {
       return this.$store.getters['team/team'](this.$route.params.id);
     },
+    subtitle() {
+      return this.team ? this.team.name : null;
+    },
+    toolbar() {
+      const buttons = [
+        {
+          icon: 'fas fa-list',
+          route: {
+            name: 'teams.browse'
+          }
+        },
+      ];
+      if (this.team && this.$can('update', this.team)) {
+        buttons.push({
+          icon: 'fas fa-edit',
+          route: {
+            name: 'teams.update',
+            params: {
+              id: this.team.id
+            }
+          }
+        });
+      }
+      return buttons;
+    }
   }
 };
 </script>
---
+++ ui/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="uk-grid">
+    <div
+      class="uk-width-1-1@s"
+      :class="{ 'uk-width-5-6@m' : hasToolbar }"
+    >
+      <h1 class="uk-h1">
+        {{ title }}
+      </h1>
+      <h3
+        v-if="subtitle"
+        class="uk-h3 uk-margin-remove"
+      >
+        {{ subtitle }}
+      </h3>
+    </div>
+    <div
+      v-if="hasToolbar"
+      class="uk-width-1-1@s uk-width-1-6@m"
+    >
+      <div class="uk-flex uk-flex-right">
+        <template v-for="(button, index) in toolbar">
+          <div
+            :class="{ 'uk-margin-small-left': index > 0 }"
+            :key="index"
+          >
+            <router-link
+              class="uk-icon-button uk-link-reset"
+              :to="button.route"
+            >
+              <i :class="button.icon"></i>
+            </router-link>
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: false
+    },
+    toolbar: {
+      type: Array,
+      required: false
+    }
+  },
+  computed: {
+    hasToolbar() {
+      return this.toolbar && this.toolbar.length > 0;
+    }
+  }
+};
+</script>
