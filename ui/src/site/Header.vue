fa2bf1f75265084a5b6bb9759df7dceec9dd69fc
fbraem <franky.braem@gmail.com>
2019-04-08 21:49:42 +0200

    Add the section tag again

--- ui/src/site/Header.vue
+++ ui/src/site/Header.vue
@@ -1,31 +1,33 @@
 <template>
-  <div uk-grid>
-    <div class="uk-width-1-1 uk-width-1-2@m uk-width-2-3@l uk-width-3-5@xl uk-flex uk-flex-middle">
-      <div>
-        <img :src="require('./images/judokwaikemzeke.jpg')" />
-      </div>
-    </div>
-    <div class="uk-width-1-1 uk-width-1-2@m uk-width-1-3@l uk-width-2-5@xl">
-      <div uk-grid class="uk-flex uk-flex-center">
-        <div class="uk-width-1-1">
-          <img class="uk-align-center"
-            :src="require('./images/logo2.png')"
-            style="width:121px;height:121px;" />
-        </div>
-        <div class="uk-width-1-1">
-          <h4 class="uk-margin-remove-top uk-text-center">Judokwai Kemzeke</h4>
-          {{ $t("message.hello") }}
-          <br />
-          {{ $t("message.info") }}
-        </div>
-        <div class="uk-width-1-1">
-          <img class="uk-align-center"
-            :src="require('./images/40jaar.png')"
-            style="width:200px;height:200px;" />
+  <section class="uk-section uk-section-small uk-section-secondary uk-container uk-container-expand">
+    <div uk-grid>
+      <div class="uk-width-1-1 uk-width-1-2@m uk-width-2-3@l uk-width-3-5@xl uk-flex uk-flex-middle">
+        <div>
+          <img :src="require('./images/judokwaikemzeke.jpg')" />
         </div>
       </div>
+      <div class="uk-width-1-1 uk-width-1-2@m uk-width-1-3@l uk-width-2-5@xl">
+        <div uk-grid class="uk-flex uk-flex-center">
+          <div class="uk-width-1-1">
+            <img class="uk-align-center"
+              :src="require('./images/logo2.png')"
+              style="width:121px;height:121px;" />
+          </div>
+          <div class="uk-width-1-1">
+            <h4 class="uk-margin-remove-top uk-text-center">Judokwai Kemzeke</h4>
+            {{ $t("message.hello") }}
+            <br />
+            {{ $t("message.info") }}
+          </div>
+          <div class="uk-width-1-1">
+            <img class="uk-align-center"
+              :src="require('./images/40jaar.png')"
+              style="width:200px;height:200px;" />
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
