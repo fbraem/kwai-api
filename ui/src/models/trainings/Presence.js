24c723f15578a5f8ddcc976681ce7197949d9cd8
fbraem <franky.braem@gmail.com>
2019-04-26 15:58:34 +0200

    Add new models

---
+++ ui/src/models/trainings/Presence.js
@@ -0,0 +1,36 @@
+import Model from '../Model';
+import { Attribute, DateAttribute } from '../Attribute';
+
+import User from '../User';
+import Member from '../Member';
+import Training from './Training';
+
+/**
+ * Presences model
+ */
+export default class Presence extends Model {
+  static type() {
+    return 'members';
+  }
+
+  static namespace() {
+    return ['trainings'];
+  }
+
+  static fields() {
+    return {
+      ... Member.fields(),
+      presence_remark: new Attribute(),
+      created_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true),
+      updated_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true),
+    };
+  }
+
+  static relationships() {
+    return {
+      ... Member.relationships(),
+      user: User,
+      training: Training
+    };
+  }
+}
---
+++ ui/src/models/trainings/TrainingCoach.js
@@ -0,0 +1,51 @@
+import Model from '../Model';
+import { Attribute, DateAttribute } from '../Attribute';
+
+import User from '../User';
+import Coach from './Coach';
+import Training from './Training';
+
+/**
+ * Coaches model
+ */
+export default class TrainingCoach extends Model {
+  static type() {
+    return 'coaches';
+  }
+
+  static namespace() {
+    return ['trainings'];
+  }
+
+  static fields() {
+    return {
+      ... Coach.fields(),
+      present: new Attribute(),
+      coach_type: new Attribute(),
+      coach_remark: new Attribute(),
+      payed: new Attribute(),
+      remark: new Attribute(),
+      created_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true),
+      updated_at: new DateAttribute('YYYY-MM-DD HH:mm:ss', true),
+    };
+  }
+
+  static relationships() {
+    return {
+      ... Coach.relationships(),
+      training: Training,
+      user: User,
+    };
+  }
+
+  static computed() {
+    return {
+      name(coach) {
+        if (coach.member && coach.member.person) {
+          return coach.member.person.name;
+        }
+        return '';
+      },
+    };
+  }
+}
