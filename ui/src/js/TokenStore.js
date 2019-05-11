031a7ec661b19723c15fd4a5e8003b82d0c28b4b
fbraem <franky.braem@gmail.com>
2019-05-11 21:40:10 +0200

    Make it a simple (reactive) object

--- ui/src/js/TokenStore.js
+++ ui/src/js/TokenStore.js
@@ -1,43 +1,17 @@
 import Lockr from 'lockr';
 
-const USER_KEY = 'user';
 const ACCESSTOKEN_KEY = 'access_token';
 const REFRESHTOKEN_KEY = 'refresh_token';
 
-class TokenStore {
-  constructor() {
-    this.access_token = Lockr.get(ACCESSTOKEN_KEY, null);
-    this.refresh_token = Lockr.get(REFRESHTOKEN_KEY, null);
-    this.user = Lockr.get(USER_KEY, null);
-  }
-
+export default {
+  access_token: Lockr.get(ACCESSTOKEN_KEY, null),
+  refresh_token: Lockr.get(REFRESHTOKEN_KEY, null),
   clear() {
     this.access_token = null;
     Lockr.rm(ACCESSTOKEN_KEY);
     this.refresh_token = null;
     Lockr.rm(REFRESHTOKEN_KEY);
-  }
-
-  getAuthorizationHeader() {
-    return `Bearer ${this.access_token}`;
-  }
-
-  isGuest() {
-    return this.access_token == null;
-  }
-
-  isAuthenticated() {
-    return this.access_token != null;
-  }
-
-  getAccessToken() {
-    return this.access_token;
-  }
-
-  getRefreshToken() {
-    return this.refresh_token;
-  }
-
+  },
   setTokens(access, refresh) {
     this.access_token = access;
     if (access) {
@@ -51,7 +25,5 @@ class TokenStore {
     } else {
       Lockr.rm(REFRESHTOKEN_KEY);
     }
-  }
-}
-
-export default TokenStore;
+  },
+};
