8db52cc82de296fe5c6874d27e291d3bccdb1880
fbraem <franky.braem@gmail.com>
2019-08-09 17:43:07 +0200

    Add SimpleResponse

---
+++ src/core/Responses/SimpleResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Core\Responses;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+class SimpleResponse
+{
+    private $status;
+    private $message;
+
+    public function __construct($status, $message)
+    {
+        $this->status = $status;
+        $this->message = $message;
+    }
+
+    public function __invoke(Response $response) : Response
+    {
+        return $response
+            ->withStatus($this->status, $this->message)
+        ;
+    }
+}
