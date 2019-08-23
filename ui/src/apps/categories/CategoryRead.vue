f67bc1bfaf766bb1716095cd2524493ead1f95c4
fbraem <franky.braem@gmail.com>
2019-08-23 20:55:27 +0200

    Created NewsSlider component

--- ui/src/apps/categories/CategoryRead.vue
+++ ui/src/apps/categories/CategoryRead.vue
@@ -20,74 +20,8 @@
         >
           {{ $t('no_featured_news') }}
         </div>
-        <div
-          v-else-if="storyCount > 2"
-          uk-slider="velocity: 5; autoplay-interval: 5000;autoplay: true;"
-        >
-          <div class="uk-position-relative">
-            <div class="uk-slider-container">
-              <ul
-                class="uk-slider-items uk-child-width-1-2@m uk-grid-medium uk-grid"
-                uk-height-match="target: > li > div > .uk-card"
-              >
-                <li
-                  v-for="story in stories"
-                  :key="story.id"
-                >
-                  <NewsCard
-                    :story="story"
-                    :showCategory="false"
-                  />
-                </li>
-              </ul>
-            </div>
-            <div class="uk-hidden@m uk-light">
-              <a
-                class="uk-position-bottom-left uk-position-small"
-                href="#"
-                uk-slidenav-previous
-                uk-slider-item="previous"
-              >
-              </a>
-              <a
-                class="uk-position-bottom-right uk-position-small"
-                href="#"
-                uk-slidenav-next
-                uk-slider-item="next"
-              >
-              </a>
-            </div>
-            <div class="uk-visible@m">
-              <a
-                class="uk-position-center-left-out uk-position-small"
-                href="#"
-                uk-slidenav-previous
-                uk-slider-item="previous"
-              >
-              </a>
-              <a
-                class="uk-position-center-right-out uk-position-small"
-                href="#"
-                uk-slidenav-next
-                uk-slider-item="next"
-              >
-              </a>
-            </div>
-            <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin">
-            </ul>
-          </div>
-        </div>
-        <div
-          v-else
-          class="uk-child-width-1-1@s uk-child-width-1-2@m uk-flex uk-flex-center"
-          uk-grid
-        >
-          <NewsCard
-            v-for="story in stories"
-            :story="story"
-            :key="story.id"
-            :showCategory="false"
-          />
+        <div v-if="stories">
+          <NewsSlider :stories="stories" />
         </div>
         <div
           v-if="category"
@@ -143,6 +77,7 @@
 import messages from './lang';
 
 import NewsCard from '@/apps/news/components/NewsCard.vue';
+import NewsSlider from '@/apps/news/components/NewsSlider.vue';
 import PageSummary from '@/apps/pages/components/PageSummary.vue';
 
 /**
@@ -152,6 +87,7 @@ export default {
   i18n: messages,
   components: {
     NewsCard,
+    NewsSlider,
     PageSummary
   },
   computed: {
