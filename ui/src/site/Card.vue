655a1ffbcf6260d5104a0d043a8db49884f3385f
fbraem <franky.braem@gmail.com>
2019-08-24 22:10:19 +0200

    Remove comment

--- ui/src/site/Card.vue
+++ ui/src/site/Card.vue
@@ -22,32 +22,6 @@
       <slot name="description">Please specify a description ...</slot>
     </div>
   </div>
-  <!-- eslint-disable max-len -->
-<!--
-  <div class="uk-margin-left">
-    <div
-      class="uk-card uk-card-hover uk-card-body"
-      uk-scrollspy="cls:uk-animation-slide-top-small"
-    >
-      <a
-        v-if="isLink"
-        :href="to"
-        class="uk-link-reset uk-position-cover uk-position-z-index uk-margin-remove-adjacent">
-      </a>
-      <router-link
-        v-else :to="to"
-        class="uk-link-reset uk-position-cover uk-position-z-index uk-margin-remove-adjacent">
-      </router-link>
-      <slot name="icon"></slot>
-      <h4 class="uk-margin">
-        {{ title }}
-      </h4>
-      <div class="uk-margin">
-          <slot name="description">Please specify a description ...</slot>
-      </div>
-    </div>
-  </div>
--->
 </template>
 
 <script>
