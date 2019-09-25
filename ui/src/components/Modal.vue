8b02a7f829e5a12377d47fa03e5c032c72712de0
fbraem <franky.braem@gmail.com>
2019-09-25 22:26:17 +0200

    Move modal styles to Modal component

--- ui/src/components/Modal.vue
+++ ui/src/components/Modal.vue
@@ -1,20 +1,20 @@
 <template>
-  <transition name="kwai-modal-fade">
+  <transition name="modal-fade">
     <div
-      class="kwai-modal"
+      class="modal"
       @click="click"
     >
-      <div class="kwai-modal-container" role="dialog">
+      <div class="modal-container" role="dialog">
         <header>
           <slot name="header"></slot>
         </header>
         <span
-          class="kwai-modal-close"
+          class="modal-close"
           @click.prevent.stop="close"
         >
           &times;
         </span>
-        <div class="kwai-modal-content">
+        <div class="modal-content">
           <slot></slot>
         </div>
         <footer>
@@ -25,6 +25,84 @@
   </transition>
 </template>
 
+<style scoped>
+.modal {
+    //display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+.modal-container {
+    margin: 10% auto; /* 15% from the top and centered */
+    border: 1px solid #888;
+    width: 70%; /* Fallback for fit-content */
+    width: fit-content;
+    display: grid;
+    grid-template-columns: auto 30px;
+    grid-template-rows: 30px auto 1fr;
+    grid-gap: 10px;
+    grid-template-areas:
+        "modal-header modal-close"
+        "modal-content modal-content"
+        "modal-footer modal-footer"
+    ;
+    background-color: var(--kwai-color-default-bg);
+    color: var(--kwai-color-default-fg);
+    filter: drop-shadow(0 2px 0 rgba(0,0,0,.15) );
+    border-radius: 15px;
+    padding: 10px;
+}
+
+.modal-content {
+    grid-area: modal-content;
+    background-color: var(--kwai-color-default-bg);
+    margin: 1em;
+}
+
+.modal-container header {
+    font-size: 28px;
+    padding-left: 20px;
+    grid-area: modal-header;
+}
+
+.modal-container footer {
+    grid-area: modal-footer;
+    margin-bottom: 20px;
+}
+
+.modal-close {
+  color: var(--kwai-color-default-fg);
+  font-size: 28px;
+  font-weight: bold;
+  grid-area: modal-close;
+  text-align: center;
+}
+
+.modal-close:hover,
+.modal.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-active {
+    opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+    transition: opacity .5s ease
+}
+</style>
+
 <script>
 export default {
   methods: {
@@ -32,7 +110,7 @@ export default {
       this.$emit('close');
     },
     click(e) {
-      if (e.target.className === 'kwai-modal') this.$emit('close');
+      if (e.target.className === 'modal') this.$emit('close');
     }
   }
 };
