a7804a07ee3f0adebb0f6f06adc617915bc0b098
fbraem <franky.braem@gmail.com>
2019-10-03 19:46:53 +0200

    Add purgecss plugin

--- ui/postcss.config.js
+++ ui/postcss.config.js
@@ -1,7 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ]
+});
+
 module.exports = {
   plugins:
     [
       require('tailwindcss'),
       require('autoprefixer'),
+      ...process.env.NODE_ENV === 'production' ? [ purgecss ] : [],
     ]
 };
