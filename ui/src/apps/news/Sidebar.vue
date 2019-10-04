ffd6c7ced88ee08b0b12be69b20c037693ac7a9f
fbraem <franky.braem@gmail.com>
2019-10-04 20:16:45 +0200

    Create new components for Page/Sidebar

---
+++ ui/src/apps/news/Sidebar.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div>
+    <CategoryList
+      v-if="categories"
+      :categories="categories"
+    />
+    <h4>
+      <span>{{ $t('archive') }}</span>
+    </h4>
+    <template v-for="(year) in archiveYears">
+      <div :key="year">
+        <h5>{{ year }}</h5>
+        <ul class="kwai-list">
+          <li v-for="(month) in archive[year]" :key="month.month">
+            <router-link
+              :to="{ name : 'news.archive', params : { year : year, month : month.month }}"
+            >
+              {{ month.monthName }} {{ year }}
+              <span
+                class="primary:kwai-badge kwai-badge-rounded"
+                style="float:right"
+              >
+                {{ month.count }}
+              </span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import messages from './lang';
+
+import CategoryList from '@/apps/categories/components/CategoryList.vue';
+
+export default {
+  i18n: messages,
+  components: {
+    CategoryList
+  },
+  computed: {
+    categories() {
+      return this.$store.state.category.categories;
+    },
+    archiveYears() {
+      var archive = this.$store.state.news.archive;
+      var sorted = Object.keys(archive).reverse();
+      return sorted;
+    },
+    archive() {
+      return this.$store.state.news.archive;
+    }
+  },
+  created() {
+    this.$store.dispatch('category/browse');
+    this.$store.dispatch('news/loadArchive');
+  }
+};
+</script>
---
+++ ui/src/components/Page.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page-container">
+    <div style="grid-area: page-content;">
+      <slot></slot>
+    </div>
+    <div style="grid-area: page-sidebar">
+      <slot name="sidebar"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.page-container {
+    display: grid;
+    grid-gap: 20px;
+    @apply p-2 mt-3;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+        "page-content"
+        "page-sidebar"
+    ;
+}
+@screen lg {
+    .page-container {
+        grid-template-columns: 3fr 1fr;
+        grid-template-rows: auto;
+        grid-template-areas:
+            "page-content page-sidebar"
+        ;
+    }
+}
+</style>
+
+<script>
+export default {
+};
+</script>
