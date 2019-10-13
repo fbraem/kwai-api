00a725276bc7eebfc4d8ee7e2b5b8c8219910fa1
fbraem <franky.braem@gmail.com>
2019-10-13 19:14:41 +0200

    Remove kwai-checkbox class

--- ui/src/components/forms/KwaiCheckbox.vue
+++ ui/src/components/forms/KwaiCheckbox.vue
@@ -1,7 +1,6 @@
 <template>
     <input v-model="field.value"
         :id="id"
-        class="kwai-checkbox"
         type="checkbox"
         :class="{ 'kwai-form-danger' : field.errors.length > 0 }"
         v-bind="$attrs"
