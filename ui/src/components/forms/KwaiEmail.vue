f3cb5828c47ccb2d67de9a4d3201dec32c71390b
fbraem <franky.braem@gmail.com>
2019-10-18 15:43:35 +0200

    Tailwind for icons

--- ui/src/components/forms/KwaiEmail.vue
+++ ui/src/components/forms/KwaiEmail.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- eslint-disable max-len -->
-  <div class="flex mb-6 border rounded">
-    <div class="bg-gray-500">
+  <div class="flex mb-4">
+    <span class="flex items-center py-2 px-4 rounded rounded-r-none border border-r-0 border-gray-200 bg-gray-500">
       <i class="far fa-envelope"></i>
-    </div>
+    </span>
     <input v-model="field.value"
       :id="id"
-      class="flex h-auto appearance-once block w-full bg-gray-200 border py-3 px-4 mb-3 leading-thight focus:outline-none focus:bg-white"
+      class="h-auto appearance-once block w-full bg-gray-200 border py-3 px-4 leading-thight focus:outline-none focus:bg-white rounded rounded-l-none"
       :class="{ 'border-red-600' : field.errors.length > 0 }"
       type="email"
       :required="field.required"
--- ui/src/components/forms/KwaiPassword.vue
+++ ui/src/components/forms/KwaiPassword.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- eslint-disable max-len -->
-  <div class="flex mb-6 border rounded">
-    <div class="bg-gray-500">
+  <div class="flex mb-4">
+    <span class="flex items-center py-2 px-4 rounded rounded-r-none border border-r-0 bg-gray-500">
       <i class="fas fa-lock"></i>
-    </div>
+    </span>
     <input v-model="field.value"
       :id="id"
-      class="flex h-auto appearance-once block w-full bg-gray-200 border py-3 px-4 mb-3 leading-thight focus:outline-none focus:bg-white"
+      class="flex-grow flex-shrink h-auto appearance-once block w-full bg-gray-200 border py-3 px-4 leading-thight focus:outline-none focus:bg-white rounded rounded-l-none"
       :class="{ 'border-red-600' : field.errors.length > 0 }"
       type="password"
       :required="field.required"
