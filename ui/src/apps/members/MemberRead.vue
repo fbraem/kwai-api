8ab009a5de82569a3bac14d4f1d474fbbc3b3a3a
fbraem <franky.braem@gmail.com>
2019-11-02 19:08:18 +0100

    Work on member page

--- ui/src/apps/members/MemberRead.vue
+++ ui/src/apps/members/MemberRead.vue
@@ -1,48 +1,102 @@
 <template>
   <!-- eslint-disable max-len -->
-  <div class="bg-gray-300 p-10">
-    <div
-      v-if="member"
-      class="container mx-auto py-3"
-    >
-      <ProfileCard
-        class="lg:w-2/3 mx-auto bg-white"
-        :image="image"
-      >
-        <template slot="header">
-          <h1 class="m-0">
-            {{ member.person.name }}
-            <sup
-              :class="flagClass"
-              class="flag-icon text-sm"
-            >
-          </sup>
-          </h1>
-          <div class="flex flex-wrap">
-            <div class="p-2 flex items-center">
-              <div>
-                <i class="fas fa-id-card text-gray-500"></i>
+  <div v-if="member">
+    <div class="bg-gray-300 p-10">
+      <div class="container mx-auto py-3">
+        <ProfileCard
+          class="lg:w-2/3 mx-auto bg-white"
+          :image="image"
+        >
+          <template slot="header">
+            <h1 class="m-0">
+              <i
+                v-if="member.person.isMale"
+                class="fas fa-male"
+              >
+              </i>
+              <i
+                v-if="member.person.isFemale"
+                class="fas fa-female"
+              >
+              </i>
+              {{ member.person.name }}
+              <sup
+                :class="flagClass"
+                class="flag-icon text-sm"
+              >
+              </sup>
+            </h1>
+            <div v-if="!member.active" class="ribbon">
+              <span>Niet actief</span>
+            </div>
+            <div class="flex flex-wrap">
+              <div class="p-2 flex items-center">
+                <div>
+                  <i class="fas fa-id-card text-gray-500"></i>
+                </div>
+                <div class="text-sm ml-2">
+                  {{ member.license }}
+                  (vervalt
+                  <span :class="licenseDateClass">{{ member.formatted_license_end_date }}</span>)
+                </div>
               </div>
-              <div class="text-sm ml-2">
-                {{ member.license }}
-                (vervalt
-                <span :class="licenseDateClass">{{ member.formatted_license_end_date }}</span>)
+              <div class="p-2 flex items-center">
+                <div>
+                  <i class="fas fa-birthday-cake text-gray-500"></i>
+                </div>
+                <div class="text-sm ml-2">
+                  {{ member.person.formatted_birthdate }} ({{ member.person.age }} jr)
+                </div>
               </div>
             </div>
-            <div class="p-2 flex items-center">
-              <div>
-                <i class="fas fa-birthday-cake text-gray-500"></i>
-              </div>
-              <div class="text-sm ml-2">
-                {{ member.person.formatted_birthdate }} ({{ member.person.age }} jr)
-              </div>
-            </div>
-          </div>
-        </template>
-      </ProfileCard>
+          </template>
+        </ProfileCard>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.ribbon {
+  position: absolute;
+  right: -5px; top: -5px;
+  z-index: 1;
+  overflow: hidden;
+  width: 75px; height: 75px;
+  text-align: right;
+}
+.ribbon span {
+  font-size: 10px;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+  line-height: 20px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+  width: 100px;
+  @apply block absolute bg-red-700 text-white;
+  box-shadow: 0 3px 10px -5px rgba(0, 0, 0, 1);
+  top: 19px; right: -21px;
+}
+.ribbon span::before {
+  content: "";
+  position: absolute; left: 0px; top: 100%;
+  z-index: -1;
+  border-left: 3px solid #8F0808;
+  border-right: 3px solid transparent;
+  border-bottom: 3px solid transparent;
+  border-top: 3px solid #8F0808;
+}
+.ribbon span::after {
+  content: "";
+  position: absolute; right: 0px; top: 100%;
+  z-index: -1;
+  border-left: 3px solid transparent;
+  border-right: 3px solid #8F0808;
+  border-bottom: 3px solid transparent;
+  border-top: 3px solid #8F0808;
+}
+</style>
 
 <script>
 import ProfileCard from '@/components/ProfileCard';
@@ -66,6 +120,12 @@ export default {
         'text-red-700': this.member.license_ended,
         'font-bold': this.member.license_ended
       };
+    },
+    logo() {
+      if (this.member.person.isMale) {
+        return 'fas fa-male';
+      }
+      return 'fas fa-female';
     }
   },
   beforeRouteEnter(to, from, next) {
--- ui/src/apps/members/TheMemberHeader.vue
+++ ui/src/apps/members/TheMemberHeader.vue
@@ -1,30 +1,163 @@
 <template>
-  <Header
-    v-if="member"
-    :title="$t('members')"
-    :subtitle="member.person.name"
-    :toolbar="toolbar"
-  >
-  </Header>
+  <div class="bg-gray-800 text-white border-b-2 border-gray-300">
+    <div
+      v-if="member"
+      class="container mx-auto flex flex-col p-4 lg:p-6 "
+    >
+      <div class="text-xl font-bold flex flex-wrap justify-between">
+        <div>
+          <h1>Ledeninformatie</h1>
+          <h2>
+            {{ member.person.name }}
+            <sup
+              :class="flagClass"
+              class="flag-icon text-sm"
+            >
+            </sup>
+          </h2>
+        </div>
+        <div>
+          <i
+            class="fas fa-bars fa-2x hover:cursor-pointer"
+            @click.stop.prevent="showMenu = !showMenu"
+          >
+          </i>
+        </div>
+      </div>
+      <div v-if="showMenu">
+        <MegaMenu
+          class="mega-menu"
+          role="toggle"
+        >
+  <!--
+          <template slot="subtitle">
+            <h2>
+              {{ member.person.name }}
+              <sup
+                :class="flagClass"
+                class="flag-icon text-sm"
+              >
+              </sup>
+            </h2>
+          </template>
+  -->
+          <MegaMenuBlock
+            class="w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0"
+            :to="{ name: 'members.read', params: { id: member.id }}"
+            linkTitle='Toon het profiel ...'
+          >
+            <div class="flex justify-center align-center mb-2">
+              <img
+                class="h-32 w-32 rounded-full"
+                :src="image"
+              >
+            </div>
+          </MegaMenuBlock>
+          <MegaMenuBlock
+            class="w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r-0 lg:border-r lg:border-b-0"
+            title="Teams"
+            logo="fas fa-users"
+            :to="{ name: 'members.read', params: { id: member.id }}"
+          >
+            <p class="text-gray-100 text-sm">
+              Bekijk tot welke teams <strong>{{ member.person.name }}</strong> behoort.
+            </p>
+          </MegaMenuBlock>
+          <MegaMenuBlock
+            class="w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-b-0 sm:border-r md:border-b-0"
+            title="Trainingen"
+            logo="fas fa-university"
+            :to="{ name: 'members.read', params: { id: member.id }}"
+          >
+            <p class="text-gray-100 text-sm">
+              Aan welke trainingen heeft <strong>{{ member.person.name }}</strong> deel genomen?
+            </p>
+          </MegaMenuBlock>
+          <MegaMenuBlock
+            class="w-full sm:w-1/2 lg:w-1/4"
+            title="Tornooien"
+            logo="fas fa-trophy"
+            :to="{ name: 'members.read', params: { id: member.id }}"
+          >
+            <p class="text-gray-100 text-sm">
+              Voor welke tornooien is <strong>{{ member.person.name }}</strong> ingeschreven?
+              Wat waren de resultaten?
+            </p>
+          </MegaMenuBlock>
+        </MegaMenu>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+/*
+.toggleable > label:after {
+    content: "\25BC";
+    font-size: 10px;
+    padding-left: 6px;
+    position: relative;
+    top: -1px;
+  }
+*/
+  .toggle-input {
+    display: none;
+  }
+  .toggle-input:not(checked) ~ .mega-menu {
+    display: none;
+  }
+
+  .toggle-input:checked ~ .mega-menu {
+    display: block;
+  }
+
+/*
+  .toggle-input:checked + label {
+    color: white;
+  }
+  .toggle-input:checked ~ label:after {
+    content: "\25B2";
+    font-size: 10px;
+    padding-left: 6px;
+    position: relative;
+    top: -1px;
+  }
+  */
+
+</style>
 
 <script>
 import messages from './lang';
 
-import Header from '@/components/Header';
+import MegaMenu from '@/components/MegaMenu';
+import MegaMenuBlock from '@/components/MegaMenuBlock';
 
 export default {
   components: {
-    Header
+    MegaMenu,
+    MegaMenuBlock
   },
   i18n: messages,
+  data() {
+    return {
+      showMenu: false
+    };
+  },
   computed: {
     member() {
       return this.$store.getters['member/member'](this.$route.params.id);
     },
-    toolbar() {
-      const buttons = [];
-      return buttons;
+    logo() {
+      if (this.member.person.isMale) {
+        return 'fas fa-male';
+      }
+      return 'fas fa-female';
+    },
+    image() {
+      return this.member?.image ?? require('./images/no_avatar.png');
+    },
+    flagClass() {
+      return 'flag-icon-' + this.member.person.nationality.iso_2.toLowerCase();
     }
   },
 };
