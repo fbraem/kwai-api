8f90d34c519458187112b663e799d0d0c9e5131d
fbraem <franky.braem@gmail.com>
2019-11-02 19:06:45 +0100

    Add MegaMenu component

---
+++ ui/src/components/MegaMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mx-auto w-full flex flex-wrap justify-between mx-2">
+    <div class="w-full text-white mb-8">
+      <h1
+        v-if="title"
+        class="font-bold text-2xl"
+      >
+        {{ title }}
+      </h1>
+      <slot name="subtitle">
+        <p v-if="subtitle">
+          {{ subtitle }}
+        </p>
+      </slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String
+    }
+  }
+};
+</script>
---
+++ ui/src/components/MegaMenuBlock.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="px-4 pb-6 pt-6 lg:pt-3">
+    <div class="flex mb-2">
+      <i
+        v-if="logo"
+        :class="logo"
+        class="fa-2x mr-3 fill-current"
+      >
+      </i>
+      <h3 class="font-bold text-xl text-bold mb-0">
+        <slot name="title">
+          {{ title }}
+        </slot>
+      </h3>
+    </div>
+    <slot></slot>
+    <div
+      v-if="to"
+      class="relative flex align-baseline items-center py-3 hover:text-blue-300"
+    >
+      <a
+        v-if="isLink"
+        :href="to"
+        class="text-white bold hover:text-blue-300 cover"
+      >
+      </a>
+      <router-link
+        v-else
+        class="text-white bold hover:text-blue-300 cover"
+        :to="to"
+      >
+      </router-link>
+      <i class="far fa-arrow-alt-circle-right fa-lg mr-3 fill-current">
+      </i>
+      <slot name="link">
+        <span class="border-b-2 border-blue-300">
+          {{ linkTitle }}
+        </span>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    logo: {
+      type: String
+    },
+    to: {
+      type: [String, Object]
+    },
+    linkTitle: {
+      type: String,
+      default: 'Toon de informatie ...'
+    }
+  },
+  computed: {
+    isLink() {
+      return typeof this.to === 'string';
+    }
+  }
+};
+</script>
