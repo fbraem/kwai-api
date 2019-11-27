c012b0f8b52c98433a29e8d22e0cfdd80aecf600
fbraem <franky.braem@gmail.com>
2019-11-27 20:22:40 +0100

    category store already registered

--- ui/src/apps/categories/App.vue
+++ ui/src/apps/categories/App.vue
@@ -6,20 +6,11 @@
 </template>
 
 <script>
-import store from './store';
-
 export default {
-  beforeCreate() {
-    this.$store.registerModule('category', store);
-  },
-  beforeDestroy() {
-    this.$store.dispatch('category/reset');
-    this.$store.unregisterModule('category');
-  },
   beforeRouteEnter(to, from, next) {
     next(async vm => {
-      await store.dispatch('category/read', { id: to.params.id });
-      const category = store.getters['category/category'](to.params.id);
+      await vm.$store.dispatch('category/read', { id: to.params.id });
+      const category = vm.$store.getters['category/category'](to.params.id);
       if (category.app) {
         return {
           path: '/' + category.app
