7838aa701f31b7cdfaa52e3a74111a7f47cd63f8
fbraem <franky.braem@gmail.com>
2019-11-28 19:23:36 +0100

    register store

--- ui/src/apps/events/App.vue
+++ ui/src/apps/events/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
+  <div class="news">
     <router-view name="hero"></router-view>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import store from './store';
+
 export default {
+  beforeCreate() {
+    this.$store.registerModule('events', store);
+  },
+  beforeDestroy() {
+    this.$store.dispatch('events/reset');
+    this.$store.unregisterModule('events');
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      if (to.meta.active) {
+        vm.$store.dispatch('events/set', to.meta.active);
+      }
+    });
+  }
 };
 </script>
