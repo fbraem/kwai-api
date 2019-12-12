a703a7b40f8ca84a477fe476d349b75d5e29713d
fbraem <franky.braem@gmail.com>
2019-12-12 20:33:14 +0100

    Add (kind of) fieldset component

---
+++ ui/src/components/forms/KwaiFieldset.vue
@@ -0,0 +1,24 @@
+<template functional>
+  <div class="flex flex-wrap border-t py-4">
+    <div class="w-full sm:w-1/3 mb-3">
+      {{ props.title }}
+      <small class="block text-grey text-xs italic mt-1">
+        <slot name="description"></slot>
+      </small>
+    </div>
+    <div class="w-full sm:w-2/3 mb-3 sm:pl-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
