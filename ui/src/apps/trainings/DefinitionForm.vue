420bdaa0ff38d34826b3a7f25a6b00c638669ec7
fbraem <franky.braem@gmail.com>
2019-12-12 21:48:53 +0100

    Use KwaiFieldset

--- ui/src/apps/trainings/DefinitionForm.vue
+++ ui/src/apps/trainings/DefinitionForm.vue
@@ -1,14 +1,17 @@
 <template>
   <!-- eslint-disable max-len -->
-  <div class="m-6">
-    <KwaiForm
-      :form="form"
-      :save="$t('save')"
-      :error="error"
-      @submit="submit"
-    >
-      <div class="flex">
-        <div class="flex-grow">
+  <KwaiForm
+    :form="form"
+    :save="$t('save')"
+    :error="error"
+    @submit="submit"
+  >
+    <KwaiFieldset :title="$t('training.definitions.title')">
+      <template slot="description">
+        Geef een trainingsmoment een naam en een omschrijving.
+      </template>
+      <div class="flex flex-wrap mb-3">
+        <div class="w-full sm:w-auto sm:flex-grow sm:pl-2">
           <KwaiField
             name="name"
             :label="$t('training.definitions.form.name.label')"
@@ -16,7 +19,7 @@
             <KwaiInputText :placeholder="$t('training.definitions.form.name.placeholder')" />
           </KwaiField>
         </div>
-        <div class="ml-4 self-begin flex-none">
+        <div class="w-full sm:w-auto sm:pl-4 self-begin flex-none">
           <KwaiField
             name="active"
             :label="$t('training.definitions.form.active.label')"
@@ -25,26 +28,32 @@
           </KwaiField>
         </div>
       </div>
-      <KwaiField
-        name="description"
-        :label="$t('training.definitions.form.description.label')"
-      >
-        <KwaiTextarea
-          :rows="5"
-          :placeholder="$t('training.definitions.form.description.placeholder')"
-        />
-      </KwaiField>
-      <KwaiField
-        name="weekday"
-        :label="$t('training.definitions.form.weekday.label')"
-      >
-        <KwaiSelect :items="weekdays" />
-      </KwaiField>
-      <div
-        class="uk-child-width-1-2"
-        uk-grid
-      >
-        <div>
+      <div class="sm:pl-2">
+        <KwaiField
+          name="description"
+          :label="$t('training.definitions.form.description.label')"
+        >
+          <KwaiTextarea
+            :rows="5"
+            :placeholder="$t('training.definitions.form.description.placeholder')"
+          />
+        </KwaiField>
+      </div>
+    </KwaiFieldset>
+    <KwaiFieldset title="Tijdstip">
+      <template slot="description">
+        Wanneer gaat dit traingsmoment door?
+      </template>
+      <div class="w-full sm:pl-2 mb-3">
+        <KwaiField
+          name="weekday"
+          :label="$t('training.definitions.form.weekday.label')"
+        >
+          <KwaiSelect :items="weekdays" />
+        </KwaiField>
+      </div>
+      <div class="w-full flex flex-wrap">
+        <div class="w-full sm:w-1/2 sm:pl-2">
           <KwaiField
             name="start_time"
             :label="$t('training.definitions.form.start_time.label')"
@@ -52,7 +61,7 @@
             <KwaiInputText :placeholder="$t('training.definitions.form.start_time.placeholder')" />
           </KwaiField>
         </div>
-        <div>
+        <div class="w-full sm:w-1/2 sm:pl-2">
           <KwaiField
             name="end_time"
             :label="$t('training.definitions.form.end_time.label')"
@@ -61,24 +70,49 @@
           </KwaiField>
         </div>
       </div>
+    </KwaiFieldset>
+    <KwaiFieldset :title="$t('training.definitions.form.season.label')">
+      <template slot="description">
+        Is dit trainingsmoment specifiek voor één seizoen?<br />
+        Selecteer dan hier het seizoen.
+      </template>
       <KwaiField
         name="season"
         :label="$t('training.definitions.form.season.label')"
       >
         <KwaiSelect :items="seasons" />
       </KwaiField>
+    </KwaiFieldset>
+    <KwaiFieldset :title="$t('training.definitions.form.team.label')">
+      <template slot="description">
+        Is dit trainingsmoment voor een specifiek team?<br />
+        Selecteer dan hier het team.
+      </template>
       <KwaiField
         name="team"
         :label="$t('training.definitions.form.team.label')"
       >
         <KwaiSelect :items="teams" />
       </KwaiField>
+    </KwaiFieldset>
+    <KwaiFieldset :title="$t('training.definitions.form.location.label')">
+      <template slot="description">
+        Gaat dit trainingsmoment altijd op dezelfde plaats door?<br />
+        Zo ja, geef dan hier de locatie in.
+      </template>
       <KwaiField
         name="location"
         :label="$t('training.definitions.form.location.label')"
-      >
+        >
         <KwaiInputText :placeholder="$t('training.definitions.form.location.placeholder')" />
       </KwaiField>
+    </KwaiFieldset>
+    <KwaiFieldset :title="$t('training.definitions.form.remark.label')">
+      <template slot="description">
+        Geef eventueel nog een opmerking in over dit trainingsmoment.<br />
+        Deze opmerking zal niet zichtbaar zijn voor gewone bezoekers van de
+        website.
+      </template>
       <KwaiField
         name="remark"
         :label="$t('training.definitions.form.remark.label')"
@@ -88,23 +122,23 @@
           :placeholder="$t('training.definitions.form.remark.placeholder')"
         />
       </KwaiField>
-    </KwaiForm>
-  </div>
+    </KwaiFieldset>
+  </KwaiForm>
 </template>
 
 <script>
 import moment from 'moment';
 
-import TrainingDefinition from '@/models/trainings/Definition';
 import Season from '@/models/Season';
 import Team from '@/models/Team';
 
 import KwaiForm from '@/components/forms/KwaiForm';
-import KwaiField from '@/components/forms/KwaiField.vue';
-import KwaiInputText from '@/components/forms/KwaiInputText.vue';
-import KwaiTextarea from '@/components/forms/KwaiTextarea.vue';
-import KwaiSelect from '@/components/forms/KwaiSelect.vue';
-import KwaiSwitch from '@/components/forms/KwaiSwitch.vue';
+import KwaiFieldset from '@/components/forms/KwaiFieldset';
+import KwaiField from '@/components/forms/KwaiField';
+import KwaiInputText from '@/components/forms/KwaiInputText';
+import KwaiTextarea from '@/components/forms/KwaiTextarea';
+import KwaiSelect from '@/components/forms/KwaiSelect';
+import KwaiSwitch from '@/components/forms/KwaiSwitch';
 
 import makeForm, { makeField, notEmpty, isTime } from '@/js/Form';
 const makeDefinitionForm = (fields, validations) => {
@@ -167,13 +201,12 @@ import messages from './lang';
 
 export default {
   components: {
-    KwaiForm, KwaiField, KwaiInputText, KwaiTextarea,
-    KwaiSelect, KwaiSwitch
+    KwaiForm, KwaiFieldset, KwaiField, KwaiInputText,
+    KwaiTextarea, KwaiSelect, KwaiSwitch
   },
   i18n: messages,
   data() {
     return {
-      definition: new TrainingDefinition(),
       weekdays: moment.weekdays(true).map((d, i) => {
         return {
           value: i + 1,
@@ -250,14 +283,14 @@ export default {
     };
   },
   computed: {
-    creating() {
-      return this.definition != null && this.definition.id == null;
+    definition() {
+      return this.$store.state.training.definition.active;
     },
     error() {
       return this.$store.state.training.definition.error;
     },
     seasons() {
-      var seasons = this.$store.getters['season/seasonsAsOptions'];
+      var seasons = this.$store.getters['training/season/seasonsAsOptions'];
       seasons.unshift({
         value: 0,
         text: this.$t('training.definitions.form.season.no_season')
@@ -265,7 +298,7 @@ export default {
       return seasons;
     },
     teams() {
-      var teams = this.$store.getters['team/teamsAsOptions'];
+      var teams = this.$store.getters['training/team/teamsAsOptions'];
       teams.unshift({
         value: 0,
         text: this.$t('training.definitions.form.team.no_team')
@@ -274,26 +307,25 @@ export default {
     },
   },
   async created() {
-    await this.$store.dispatch('season/browse');
-    await this.$store.dispatch('team/browse');
+    await this.$store.dispatch('training/season/browse');
+    await this.$store.dispatch('training/team/browse');
   },
   beforeRouteEnter(to, from, next) {
     next(async(vm) => {
-      if (to.params.id) await vm.fetchData(to.params.id);
+      vm.setupForm(to.params);
       next();
     });
   },
   methods: {
-    clear() {
-      // this.$v.$reset();
-      // this.form.reset();
-    },
-    async fetchData(id) {
-      this.definition
-        = await this.$store.dispatch('training/definition/read', {
-          id: id
+    async setupForm(params) {
+      if (params.id) {
+        await this.$store.dispatch('training/definition/read', {
+          id: params.id
         });
-      this.form.writeForm(this.definition);
+        this.form.writeForm(this.definition);
+      } else {
+        this.$store.dispatch('training/definition/create');
+      }
     },
     submit() {
       this.form.clearErrors();
