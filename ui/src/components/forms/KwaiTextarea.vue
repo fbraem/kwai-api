b3076df2900eb2258a67f4f1dbe70bc57a603311
fbraem <franky.braem@gmail.com>
2019-12-12 20:16:34 +0100

    Remove mb-3

--- ui/src/components/forms/KwaiTextarea.vue
+++ ui/src/components/forms/KwaiTextarea.vue
@@ -2,7 +2,7 @@
   <!-- eslint-disable max-len -->
   <textarea v-model="field.value"
     :id="id"
-    class="appearance-once block w-full bg-gray-200 border rounded py-3 px-4 mb-3 leading-thight focus:outline-none focus:shadow-outline"
+    class="appearance-once block w-full bg-gray-200 border rounded py-3 px-4 leading-thight focus:outline-none focus:shadow-outline"
     :class="{ 'border-red-600' : field.errors.length > 0 }"
     :required="field.required"
     v-bind="$attrs">
