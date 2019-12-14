89a2ab7b62df807658a960a224168d34f702a9f5
fbraem <franky.braem@gmail.com>
2019-12-14 18:19:56 +0100

    Use computed

--- ui/src/components/Paginator.vue
+++ ui/src/components/Paginator.vue
@@ -7,7 +7,7 @@
     <li v-if="currentPage > 1">
       <a
         class="block hover:text-red-300 hover:bg-red-700 hover:cursor-pointer text-gray-700 border-r border-gray-300 px-3 py-2"
-        @click="currentPage -= 1"
+        @click="triggerPage(currentPage - 1)"
       >
         <i class="fas fa-chevron-left"></i>
       </a>
@@ -27,7 +27,7 @@
         <a
           :class="{ 'text-gray-700': page !== currentPage, 'text-red-300' : page === currentPage, 'bg-red-700' : page === currentPage }"
           class="block hover:text-red-300 hover:bg-red-700 hover:cursor-pointer border-r border-gray-300 px-3 py-2"
-          @click="currentPage = page"
+          @click="triggerPage(page)"
         >
           {{ page }}
         </a>
@@ -38,7 +38,7 @@
     >
       <a
         class="block hover:text-red-300 hover:bg-red-700 hover:cursor-pointer text-gray-700 border-r border-gray-300 px-3 py-2"
-        @click="currentPage += 1"
+        @click="triggerPage(currentPage + 1)"
       >
         <i class="fas fa-chevron-right"></i>
       </a>
@@ -57,51 +57,46 @@ export default {
     offset: Number,
     count: Number
   },
-  data() {
-    var currentPage = 1;
-    for (var offset = 0; offset < this.offset; currentPage++) {
-      offset += this.limit;
-    }
-    var pageCount = Math.ceil(this.count / this.limit);
-    return {
-      currentPage: currentPage,
-      pageCount: pageCount,
-      pages: this.pagination(currentPage, pageCount)
-    };
-  },
-  watch: {
-    currentPage(nv) {
-      if (nv) {
-        var offset = 0;
-        for (var page = 1; page < nv; page++) {
-          offset += this.limit;
-        }
-        this.pages = this.pagination(nv, this.pageCount);
-        this.$emit('page', offset);
+  computed: {
+    pageCount() {
+      return Math.ceil(this.count / this.limit);
+    },
+    currentPage() {
+      let current = 1;
+      for (var offset = 0; offset < this.offset; current++) {
+        offset += this.limit;
       }
-    }
-  },
-  methods: {
-    pagination(currentPage, pageCount) {
+      return current;
+    },
+    pages() {
       const delta = 2;
 
       let range = [];
-      for (let i = Math.max(2, currentPage - delta);
-        i <= Math.min(pageCount - 1,
-          currentPage + delta); i++) {
+      for (let i = Math.max(2, this.currentPage - delta);
+        i <= Math.min(this.pageCount - 1,
+          this.currentPage + delta); i++) {
         range.push(i);
       }
-      if (currentPage - delta > 2) {
+      if (this.currentPage - delta > 2) {
         range.unshift('...');
       }
-      if (currentPage + delta < pageCount - 1) {
+      if (this.currentPage + delta < this.pageCount - 1) {
         range.push('...');
       }
 
       range.unshift(1);
-      range.push(pageCount);
+      range.push(this.pageCount);
 
       return range;
+    }
+  },
+  methods: {
+    triggerPage(newPage) {
+      var offset = 0;
+      for (var page = 1; page < newPage; page++) {
+        offset += this.limit;
+      }
+      this.$emit('page', offset);
     }
   }
 };
