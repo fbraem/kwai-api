4d6d5827063b9c064813b8346ea9a74b127d7630
fbraem <franky.braem@gmail.com>
2019-12-17 20:40:09 +0100

    Add new component

---
+++ ui/src/components/forms/KwaiInputGroupAddon.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div class="flex flex-wrap items-stretch w-full mb-4 relative">
+    <div
+      v-if="left"
+      class="flex -mr-px"
+    >
+      <span class="flex items-center leading-normal bg-grey-lighter rounded rounded-r-none border border-r-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">
+        <i class="left"></i>
+      </span>
+    </div>
+    <slot></slot>
+    <div
+      v-if="right"
+      class="flex -mr-px">
+      <span class="flex items-center leading-normal bg-grey-lighter rounded rounded-l-none border border-l-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">
+        <i class="right"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    left: {
+      type: String
+    },
+    right: {
+      type: String
+    }
+  },
+  inject: {
+    field: 'field',
+    id: 'id'
+  },
+  provide() {
+    return {
+      field: this.field,
+      id: this.name
+    };
+  }
+};
+</script>
