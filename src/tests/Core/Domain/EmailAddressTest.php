98ecba1e0c699917f14e2b3ba850b53ff27ee683
fbraem <franky.braem@gmail.com>
2019-12-31 15:54:54 +0100

    Unit tests for value objects

---
+++ src/tests/Core/Domain/EmailAddressTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Testcase for EmailAddress
+ */
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Kwai\Core\Domain\EmailAddress;
+
+final class EmailAddressTest extends TestCase
+{
+    public function testCreateValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            EmailAddress::class,
+            new EmailAddress('user@example.com')
+        );
+    }
+
+    public function testInvalidEmailAddress(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new EmailAddress('invalid');
+    }
+
+    public function testEmailAddressToString(): void
+    {
+        $this->assertEquals(
+            strval(new EmailAddress('user@example.com')),
+            'user@example.com'
+        );
+    }
+}
---
+++ src/tests/Core/Domain/UniqueIdTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Testcase for UniqueId
+ */
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Kwai\Core\Domain\UniqueId;
+
+final class UniqueIdTest extends TestCase
+{
+    public function testCreateNewUniqueId(): void
+    {
+        $this->assertInstanceOf(
+            UniqueId::class,
+            new UniqueId()
+        );
+    }
+
+    public function testInvalidUniqueId(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new UniqueId('invalid');
+    }
+
+    public function testInvalidVersion(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        // A UUID1 is passed
+        new UniqueId('e4eaaaf2-d142-11e1-b3e4-080027620cdd');
+    }
+}
