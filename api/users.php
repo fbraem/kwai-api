66631e6b2de324649f0d00cb249dc8b1ed61ac17
fbraem <franky.braem@gmail.com>
2020-01-05 19:20:26 +0100

    $this must be $group

--- api/users.php
+++ api/users.php
@@ -6,63 +6,63 @@ use Slim\Routing\RouteCollectorProxy;
 $app = \Core\Clubman::getApplication();
 
 $app->group('/users', function (RouteCollectorProxy $group) {
-    $this->get('', \REST\Users\Actions\BrowseAction::class)
+    $group->get('', \REST\Users\Actions\BrowseAction::class)
         ->setName('users.browse')
         ->setArgument('auth', true)
     ;
-    $this->get('/{id:[0-9]+}', \REST\Users\Actions\ReadAction::class)
+    $group->get('/{id:[0-9]+}', \REST\Users\Actions\ReadAction::class)
         ->setName('users.read')
         ->setArgument('auth', true)
     ;
     // Rules
-    $this->get('/{id:[0-9]+}/abilities', \REST\Users\Actions\UserAbilityBrowseAction::class)
+    $group->get('/{id:[0-9]+}/abilities', \REST\Users\Actions\UserAbilityBrowseAction::class)
         ->setName('user.abilities.browse')
         ->setArgument('auth', true)
     ;
-    $this->patch('/{id:[0-9]+}/abilities/{ability:[0-9]+}', \REST\Users\Actions\UserAttachAbilityAction::class)
+    $group->patch('/{id:[0-9]+}/abilities/{ability:[0-9]+}', \REST\Users\Actions\UserAttachAbilityAction::class)
         ->setName('user.abilities.attach')
         ->setArgument('auth', true)
     ;
-    $this->delete('/{id:[0-9]+}/abilities/{ability:[0-9]+}', \REST\Users\Actions\UserDetachAbilityAction::class)
+    $group->delete('/{id:[0-9]+}/abilities/{ability:[0-9]+}', \REST\Users\Actions\UserDetachAbilityAction::class)
         ->setName('user.abilities.detach')
         ->setArgument('auth', true)
     ;
 
-    $this->get('/abilities', \REST\Users\Actions\AbilityBrowseAction::class)
+    $group->get('/abilities', \REST\Users\Actions\AbilityBrowseAction::class)
         ->setName('users.abilities.browse')
         ->setArgument('auth', true)
     ;
-    $this->post('/abilities', \REST\Users\Actions\AbilityCreateAction::class)
+    $group->post('/abilities', \REST\Users\Actions\AbilityCreateAction::class)
         ->setName('users.abilities.create')
         ->setArgument('auth', true)
     ;
-    $this->get('/abilities/{id:[0-9]+}', \REST\Users\Actions\AbilityReadAction::class)
+    $group->get('/abilities/{id:[0-9]+}', \REST\Users\Actions\AbilityReadAction::class)
         ->setName('users.abilities.read')
         ->setArgument('auth', true)
     ;
-    $this->patch('/abilities/{id:[0-9]+}', \REST\Users\Actions\AbilityUpdateAction::class)
+    $group->patch('/abilities/{id:[0-9]+}', \REST\Users\Actions\AbilityUpdateAction::class)
         ->setName('users.abilities.update')
         ->setArgument('auth', true)
     ;
-    $this->get('/rules', \REST\Users\Actions\RuleBrowseAction::class)
+    $group->get('/rules', \REST\Users\Actions\RuleBrowseAction::class)
         ->setName('users.rules.browse')
         ->setArgument('auth', true)
     ;
 
     // Invitations
-    $this->post('/invitations', \REST\Users\Actions\CreateInvitationAction::class)
+    $group->post('/invitations', \REST\Users\Actions\CreateInvitationAction::class)
         ->setName('users.invitations.create')
         ->setArgument('auth', true)
     ;
-    $this->get('/invitations', \REST\Users\Actions\BrowseInvitationAction::class)
+    $group->get('/invitations', \REST\Users\Actions\BrowseInvitationAction::class)
         ->setName('users.invitations.browse')
         ->setArgument('auth', true)
     ;
-    $this->get('/invitations/{token:[0-9a-zA-Z]+}', \REST\Users\Actions\ReadInvitationByTokenAction::class)
+    $group->get('/invitations/{token:[0-9a-zA-Z]+}', \REST\Users\Actions\ReadInvitationByTokenAction::class)
         ->setName('users.invitations.token')
     ;
 
-    $this->post('/{token:[0-9a-zA-Z]+}', \REST\Users\Actions\CreateWithTokenAction::class)
+    $group->post('/{token:[0-9a-zA-Z]+}', \REST\Users\Actions\CreateWithTokenAction::class)
         ->setName('users.create.token')
     ;
 });
