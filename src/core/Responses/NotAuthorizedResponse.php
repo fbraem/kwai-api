98672bee42787826330605742a13a142f1a4d89f
fbraem <franky.braem@gmail.com>
2020-01-18 19:57:51 +0100

    Add NotAuthorizedResponse

--- src/core/Responses/NotAuthorizedResponse.php
+++ src/core/Responses/NotAuthorizedResponse.php
@@ -4,15 +4,31 @@ namespace Core\Responses;
 
 use Psr\Http\Message\ResponseInterface as Response;
 
-class NotFoundResponse
+/**
+ * NotAuthorizedResponse is a response with status 401 Unauthorized.
+ */
+class NotAuthorizedResponse
 {
+    /**
+     * Status message
+     * @var string
+     */
     private $message;
 
+    /**
+     * Constructor
+     * @param string $message
+     */
     public function __construct($message = 'Not Authorized')
     {
         $this->message = $message;
     }
 
+    /**
+     * Returns a response with status code 401 and a status message.
+     * @param  Response $response
+     * @return Response
+     */
     public function __invoke(Response $response) : Response
     {
         return $response
