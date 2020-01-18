8ab42896f8643a92d19a5b9d7662f389f2758afb
fbraem <franky.braem@gmail.com>
2020-01-18 22:00:24 +0100

    Extend from SimpleResponse

--- src/core/Responses/ForbiddenResponse.php
+++ src/core/Responses/ForbiddenResponse.php
@@ -8,32 +8,14 @@ use Psr\Http\Message\ResponseInterface as Response;
  * ForbiddenResponse is returned when an action is not allowed.
  * It means: we know who you are, but you are not allowed to.
  */
-class ForbiddenResponse
+class ForbiddenResponse extends SimpleResponse
 {
-    /**
-     * A status message
-     * @var string
-     */
-    private $message;
-
     /**
      * Constructor
      * @param string $message
      */
     public function __construct($message = 'Action is forbidden')
     {
-        $this->message = $message;
-    }
-
-    /**
-     * Returns a response with status 403 and a status message.
-     * @param  Response $response
-     * @return Response
-     */
-    public function __invoke(Response $response) : Response
-    {
-        return $response
-            ->withStatus(403, $this->message);
-        ;
+        parent::construct(403, $message);
     }
 }
--- src/core/Responses/NotAuthorizedResponse.php
+++ src/core/Responses/NotAuthorizedResponse.php
@@ -7,32 +7,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 /**
  * NotAuthorizedResponse is a response with status 401 Unauthorized.
  */
-class NotAuthorizedResponse
+class NotAuthorizedResponse extends SimpleResponse
 {
-    /**
-     * Status message
-     * @var string
-     */
-    private $message;
-
     /**
      * Constructor
      * @param string $message
      */
     public function __construct($message = 'Not Authorized')
     {
-        $this->message = $message;
-    }
-
-    /**
-     * Returns a response with status code 401 and a status message.
-     * @param  Response $response
-     * @return Response
-     */
-    public function __invoke(Response $response) : Response
-    {
-        return $response
-            ->withStatus(401, $this->message);
-        ;
+        parent::__construct(401, $message);
     }
 }
