199ccc2527c4e7f88afd745d2e5e096cdf5f9433
fbraem <franky.braem@gmail.com>
2020-01-22 19:27:22 +0100

    Add create

--- src/kwai/Modules/Users/Repositories/AccessTokenRepository.php
+++ src/kwai/Modules/Users/Repositories/AccessTokenRepository.php
@@ -10,6 +10,7 @@ namespace Kwai\Modules\Users\Repositories;
 
 use Kwai\Core\Domain\Entity;
 use Kwai\Modules\Users\Domain\ValueObjects\TokenIdentifier;
+use Kwai\Modules\Users\Domain\AccessToken;
 
 /**
  * AccessToken repository interface
@@ -30,4 +31,11 @@ interface AccessTokenRepository
      * @return Entity[]   An array with accesstokens
      */
     public function getTokensForUser(Entity $user): array;
+
+    /**
+     * Save a new AccessToken
+     * @param  AccessToken $token
+     * @return Entity
+     */
+    public function create(AccessToken $token): Entity;
 }
