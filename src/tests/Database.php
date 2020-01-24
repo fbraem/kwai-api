84105dd0fe949139ec0c63013a3b18ed8fd641ec
fbraem <franky.braem@gmail.com>
2020-01-24 17:45:45 +0100

    Test will now use a sqlite database

--- src/tests/Database.php
+++ src/tests/Database.php
@@ -1,21 +1,35 @@
 <?php
 declare(strict_types = 1);
 
+use Phinx\Config\Config;
+use Phinx\Migration\Manager;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\NullOutput;
+
 class Database
 {
+    const CONNECTION_STR = 'sqlite:/var/tmp/kwai.db';
+
     private static $db;
 
     public static function getDatabase()
     {
         if (! self::$db) {
-            $config = include __DIR__ . '/../../api/config.php';
-            $dbConfig = $config['database'][$config['default_database']];
-            $connection = new \Opis\Database\Connection(
-                $dbConfig['adapter'] . ':host=' . $dbConfig['host'] . ';dbname=' . $dbConfig['name'],
-                $dbConfig['user'],
-                $dbConfig['pass']
-            );
-            $connection->initCommand('SET NAMES UTF8');
+            $connection = new \Opis\Database\Connection(self::CONNECTION_STR);
+            $connection->option(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+
+            $configArray = require('../phinx.php');
+            $configArray['environments']['test'] = [
+                'adapter'    => 'sqlite',
+                'connection' => $connection->getPDO()
+            ];
+            $config = new Config($configArray);
+            $manager = new Manager($config, new StringInput(' '), new NullOutput());
+            $manager->migrate('test');
+
+            $connection = new \Opis\Database\Connection(self::CONNECTION_STR);
+            $connection->option(\PDO::ATTR_STRINGIFY_FETCHES, false);
             self::$db = new \Opis\Database\Database($connection);
         }
         return self::$db;
