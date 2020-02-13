8fd9060781dc869474dc5f5398c21d5bd4a634ed
fbraem <franky.braem@gmail.com>
2020-02-13 21:31:52 +0100

    Add generic User in docs of includeAbilities

--- src/kwai/Modules/Users/Presentation/Transformers/UserTransformer.php
+++ src/kwai/Modules/Users/Presentation/Transformers/UserTransformer.php
@@ -51,7 +51,7 @@ class UserTransformer extends Fractal\TransformerAbstract
 
     /**
      * Get the included abilities
-     * @param  Entity $user The user containing the abilities.
+     * @param  Entity<User> $user The user containing the abilities.
      * @return Fractal\Resource\Collection
      */
     public function includeAbilities(Entity $user): Fractal\Resource\Collection
