9199fd4c3093bf92f674af026a85d0c97d64317a
fbraem <franky.braem@gmail.com>
2020-02-16 17:09:09 +0100

    Use ::class

--- api/auth.php
+++ api/auth.php
@@ -11,17 +11,17 @@ $app->group('/auth', function (RouteCollectorProxy $group) {
             ->setName('auth.authorize')
         ;
     */
-    $group->post('/login', '\Kwai\Modules\Users\Presentation\Rest\LoginAction')
+    $group->post('/login', Kwai\Modules\Users\Presentation\Rest\LoginAction::class)
         ->setName('auth.login')
     ;
-    $group->post('/access_token', '\Kwai\Modules\Users\Presentation\Rest\AccessTokenAction')
+    $group->post('/access_token', Kwai\Modules\Users\Presentation\Rest\AccessTokenAction::class)
         ->setName('auth.access_token')
     ;
-    $group->post('/logout', '\Kwai\Modules\Users\Presentation\Rest\LogoutAction')
+    $group->post('/logout', Kwai\Modules\Users\Presentation\Rest\LogoutAction::class)
         ->setName('auth.logout')
         ->setArgument('auth', true)
     ;
-    $group->get('/users', '\Kwai\Modules\Users\Presentation\Rest\UserAction')
+    $group->get('/users', Kwai\Modules\Users\Presentation\Rest\UserAction::class)
         ->setArgument('auth', true)
         ->setName('auth.users')
     ;
