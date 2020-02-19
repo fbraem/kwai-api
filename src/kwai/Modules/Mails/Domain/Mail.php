1841196ef67ca474e092c106a2b34344a70f0ace
fbraem <franky.braem@gmail.com>
2020-02-19 18:39:37 +0100

    WIP: mail

---
+++ src/kwai/Modules/Mails/Domain/Mail.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Kwai/Modules
+ * @subpackage Mails
+ */
+declare(strict_types = 1);
+
+namespace Kwai\Modules\Mails\Domain;
+
+use Kwai\Core\Domain\EmailAddress;
+use Kwai\Core\Domain\UniqueId;
+use Kwai\Core\Domain\TraceableTime;
+use Kwai\Core\Domain\Timestamp;
+use Kwai\Core\Domain\DomainEntity;
+use Kwai\Core\Domain\Entity;
+
+/**
+ * Mail Entity
+ */
+class Mail implements DomainEntity
+{
+}
