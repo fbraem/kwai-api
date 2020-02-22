265e44d3edda85dedeed423cab62055bc9dd7c7a
fbraem <franky.braem@gmail.com>
2020-02-22 20:59:18 +0100

    Add mail_recipients

--- src/kwai/Modules/Mails/Infrastructure/Migrations/20200217184000_mail_migration.php
+++ src/kwai/Modules/Mails/Infrastructure/Migrations/20200217184000_mail_migration.php
@@ -12,14 +12,19 @@ class MailMigration extends AbstractMigration
         $this->table('mails', ['signed' => false])
             ->addColumn('uuid', 'string')
             ->addColumn('from', 'string')
-            ->addColumn('to', 'string')
-            ->addColumn('cc', 'string')
-            ->addColumn('bcc', 'string')
-            ->addColumn('body', 'text')
+            ->addColumn('html_body', 'text', ['null' => true])
+            ->addColumn('text_body', 'text')
             ->addColumn('sent_time', 'datetime', ['null' => true])
             ->addColumn('remark', 'text', ['null' => true])
             ->addColumn('user_id', 'integer')
             ->addTimestamps()
+            ->create()
+        ;
+        $this->table('mail_recipients', ['signed' => false])
+            ->addColumn('mail_id', 'integer')
+            ->addColumn('type', 'integer')
+            ->addColumn('email', 'string')
+            ->addColumn('name', 'string', ['null' => true])
             ->create()
         ;
     }
@@ -27,5 +32,6 @@ class MailMigration extends AbstractMigration
     public function down()
     {
         $this->table('mails')->drop()->save();
+        $this->table('mail_recipients')->drop()->save();
     }
 }
