84967ccbe934e7f4a5bb0ce579cfdc0ebdc7b6d3
fbraem <franky.braem@gmail.com>
2020-02-25 22:06:21 +0100

    Add property types and some phpdocs

--- src/kwai/Modules/Mails/Domain/ValueObjects/Address.php
+++ src/kwai/Modules/Mails/Domain/ValueObjects/Address.php
@@ -16,29 +16,38 @@ final class Address
 {
     /**
      * The emailaddress
-     * @var EmailAddress
      */
-    private $email;
+    private EmailAddress $email;
 
     /**
      * The name of for the address
-     * @var string
      */
-    private $name;
+    private string $name;
 
+    /**
+     * Address constructor.
+     * @param EmailAddress $email
+     * @param string $name
+     */
     public function __construct(
         EmailAddress $email,
-        string $name
+        string $name = ''
     ) {
         $this->email = $email;
         $this->name = $name;
     }
 
+    /**
+     * Get email address
+     */
     public function getEmail(): EmailAddress
     {
         return $this->email;
     }
 
+    /**
+     * Get name
+     */
     public function getName(): string
     {
         return $this->name;
