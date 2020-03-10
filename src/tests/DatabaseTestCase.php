f00ccfb849bdaeb15c1707c26af880c364400454
fbraem <franky.braem@gmail.com>
2020-03-10 21:21:49 +0100

    Commit on success, rollback when at least one test failed

--- src/tests/DatabaseTestCase.php
+++ src/tests/DatabaseTestCase.php
@@ -7,10 +7,16 @@ use Core\Clubman;
 use Kwai\Core\Infrastructure\Database\Connection;
 use Kwai\Core\Infrastructure\Database\DatabaseException;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
+/**
+ * @SuppressWarnings(PHPMD.ShortVariable)
+ */
 class DatabaseTestCase extends TestCase
 {
     protected static ?Connection $db;
+
+    private static bool $success = true;
 
     /**
      * @throws DatabaseException
@@ -26,5 +32,30 @@ class DatabaseTestCase extends TestCase
             $config['database']['test']['user'],
             $config['database']['test']['pass']
         );
+        self::$db->begin();
+    }
+
+    protected function onNotSuccessfulTest(Throwable $t): void
+    {
+        // When a transaction is active, rollback it.
+        self::$success = false;
+        if (self::$db != null) {
+            if (self::$db->inTransaction()) {
+                self::$db->rollback();
+            }
+        }
+        parent::onNotSuccessfulTest($t);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        if (self::$success) {
+            if (self::$db != null) {
+                if (self::$db->inTransaction()) {
+                    self::$db->commit();
+                }
+            }
+        }
+        parent::tearDownAfterClass();
     }
 }
