0c241822ede63d6ef8c6dbff15239d011d38e20c
fbraem <franky.braem@gmail.com>
2020-03-10 22:02:46 +0100

    Use types for properties and add use InvalidArgumentException

--- src/kwai/Core/Domain/EmailAddress.php
+++ src/kwai/Core/Domain/EmailAddress.php
@@ -6,33 +6,35 @@ declare(strict_types = 1);
 
 namespace Kwai\Core\Domain;
 
+use InvalidArgumentException;
+
 /**
  * Value object for an email address.
  */
 final class EmailAddress
 {
     /**
-     * @var string $userName is the username part of the emailaddress.
+     * The username part of the email address.
      */
-    private $userName;
+    private string $userName;
 
     /**
-    * @var string $domain is the domain part of the emailaddress.
+    * The domain part of the email address.
      */
-    private $domain;
+    private string $domain;
 
     /**
      * Constructs a new EmailAddress.
      *
      * @param string $emailAddress
-     * @throws \InvalidArgumentException Thrown when the emailaddress is invalid
+     * @throws InvalidArgumentException Thrown when the emailaddress is invalid
      */
     public function __construct(string $emailAddress)
     {
         if (filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) {
             list($this->userName, $this->domain) = explode('@', $emailAddress);
         } else {
-            throw new \InvalidArgumentException(
+            throw new InvalidArgumentException(
                 "'$emailAddress' is not a valid emailaddress."
             );
         }
