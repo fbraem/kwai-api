ccc283beb7f3e698cc3ff3b0a39943d2ab0806f1
fbraem <franky.braem@gmail.com>
2020-03-11 19:35:43 +0100

    Use a longer variable name

--- src/kwai/Modules/Mails/Infrastructure/Mailer/MailerException.php
+++ src/kwai/Modules/Mails/Infrastructure/Mailer/MailerException.php
@@ -8,23 +8,24 @@ declare(strict_types = 1);
 
 namespace Kwai\Modules\Mails\Infrastructure\Mailer;
 
+use Exception;
+
 /**
  * This exception is thrown when an exception occurs with SmtpMailerService.
  */
-class MailerException extends \Exception
+class MailerException extends Exception
 {
     /**
      * Original exception
-     * @var \Exception
      */
-    private $exception;
+    private Exception $exception;
 
     /**
      * Constructor
      * @param string $message
-     * @param \Exception $exception Original exception
+     * @param Exception $exception Original exception
      */
-    public function __construct(string $message, \Exception $exception = null)
+    public function __construct(string $message, Exception $exception = null)
     {
         parent::__construct($message);
         $this->exception = $exception;
@@ -32,10 +33,10 @@ class MailerException extends \Exception
 
     public function __toString()
     {
-        $s = __CLASS__ . ': ' . $this->getMessage();
+        $str = __CLASS__ . ': ' . $this->getMessage();
         if ($this->exception) {
-            $s .= '(' . $this->exception->getMessage() . ')';
+            $str .= '(' . $this->exception->getMessage() . ')';
         }
-        return $s;
+        return $str;
     }
 }
