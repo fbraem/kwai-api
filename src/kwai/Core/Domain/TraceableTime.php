babb3ee1ce51d3d98b95de5acebc34ef9b31c66b
fbraem <franky.braem@gmail.com>
2020-03-11 18:47:42 +0100

    Use type for property

--- src/kwai/Core/Domain/TraceableTime.php
+++ src/kwai/Core/Domain/TraceableTime.php
@@ -9,66 +9,64 @@
 /**
   * Value object that handles the created_at and updated_at timestamps.
   */
- final class TraceableTime
- {
-     /**
-      * Time of creation
-      * @var Timestamp
-      */
-     private $created_at;
+final class TraceableTime
+{
+    /**
+     * Time of creation
+     */
+    private Timestamp $created_at;
 
-     /**
-      * Time of last update
-      * @var Timestamp
-      */
-     private $updated_at;
+    /**
+     * Time of last update
+     */
+    private Timestamp $updated_at;
 
-     /**
-      * Constructor
-      *
-      * @param Timestamp $created_at The timestamp of creation
-      * @param Timestamp $updated_at The timestamp of the last modification
-      */
-     public function __construct(
-         Timestamp $created_at = null,
-         Timestamp $updated_at = null
-     ) {
-         $this->created_at = $created_at ?? Timestamp::createNow();
-         $this->updated_at = $updated_at;
-     }
+    /**
+     * Constructor
+     *
+     * @param Timestamp $created_at The timestamp of creation
+     * @param Timestamp $updated_at The timestamp of the last modification
+     */
+    public function __construct(
+        Timestamp $created_at = null,
+        Timestamp $updated_at = null
+    ) {
+        $this->created_at = $created_at ?? Timestamp::createNow();
+        $this->updated_at = $updated_at;
+    }
 
-     /**
-      * Returns the timestamp of creation.
-      * @return Timestamp
-      */
-     public function getCreatedAt(): Timestamp
-     {
-         return $this->created_at;
-     }
+    /**
+     * Returns the timestamp of creation.
+     * @return Timestamp
+     */
+    public function getCreatedAt(): Timestamp
+    {
+        return $this->created_at;
+    }
 
-     /**
-      * Returns the timestamp of the last modification
-      * @return Timestamp|null
-      */
-     public function getUpdatedAt(): ?Timestamp
-     {
-         return $this->updated_at;
-     }
+    /**
+     * Returns the timestamp of the last modification
+     * @return Timestamp|null
+     */
+    public function getUpdatedAt(): ?Timestamp
+    {
+        return $this->updated_at;
+    }
 
-     /**
-      * Check if there was an update.
-      * @return bool
-      */
-     public function isUpdated(): bool
-     {
-         return $this->updated_at != null;
-     }
+    /**
+     * Check if there was an update.
+     * @return bool
+     */
+    public function isUpdated(): bool
+    {
+        return $this->updated_at != null;
+    }
 
-     /**
-      * Change the updateAt to the current timestamp
-      */
-     public function markUpdated(): void
-     {
-         $this->updated_at = Timestamp::createNow();
-     }
- }
+    /**
+     * Change the updateAt to the current timestamp
+     */
+    public function markUpdated(): void
+    {
+        $this->updated_at = Timestamp::createNow();
+    }
+}
