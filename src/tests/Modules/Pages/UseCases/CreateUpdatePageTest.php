e564ad0f1d50e20bf7776cd4bc899fb34c32bd79
fbraem <franky.braem@gmail.com>
2020-07-25 18:34:06 +0200

    Add update test

--- src/tests/Modules/Pages/UseCases/CreateUpdatePageTest.php
+++ src/tests/Modules/Pages/UseCases/CreateUpdatePageTest.php
@@ -10,11 +10,14 @@ use Kwai\Modules\Pages\Infrastructure\Repositories\AuthorDatabaseRepository;
 use Kwai\Modules\Pages\Infrastructure\Repositories\PageDatabaseRepository;
 use Kwai\Modules\Pages\UseCases\CreatePage;
 use Kwai\Modules\Pages\UseCases\CreatePageCommand;
+use Kwai\Modules\Pages\UseCases\UpdatePage;
+use Kwai\Modules\Pages\UseCases\UpdatePageCommand;
 use Tests\Context;
 
 /**
  * Context for all tests in this file
  * + db: Database connection
+ * + id: Id of the created page
  */
 $context = Context::createContext();
 
@@ -44,4 +47,37 @@ it('can create a page', function () use ($context) {
         }
     )($command);
     assertInstanceOf(Entity::class, $page);
+    $context->id = $page->id();
+});
+
+it('can update a page', function () use ($context) {
+    $command = new UpdatePageCommand();
+    $command->id = $context->id;
+    $command->application = 1;
+    $content = new Content();
+    $content->title = 'Test title';
+    $content->summary = 'This is an updated summary';
+    $content->author = 1;
+    $content->content = 'This is the content of the page';
+    $command->contents[] = $content;
+
+    $page = UpdatePage::create(
+        new PageDatabaseRepository($context->db),
+        new ApplicationDatabaseRepository($context->db),
+        new AuthorDatabaseRepository($context->db),
+        new class implements ImageRepository {
+            public function getImages(int $id): array
+            {
+                return [];
+            }
+
+            public function removeImages(int $id): void
+            {
+            }
+        }
+    )($command);
+    assertInstanceOf(Entity::class, $page);
+    /** @noinspection PhpUndefinedMethodInspection */
+    assertEquals('This is an updated summary', $page->getContents()[0]->getSummary());
+    $context->id = $page->id();
 });
