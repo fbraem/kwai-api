464d59bfc73a503c2eef24bd85b822099c43e77a
fbraem <franky.braem@gmail.com>
2020-10-04 19:50:42 +0200

    Get logger configuration from logger/kwai

--- src/kwai/Core/Infrastructure/Dependencies/LoggerDependency.php
+++ src/kwai/Core/Infrastructure/Dependencies/LoggerDependency.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Kwai\Core\Infrastructure\Dependencies;
 
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 /**
@@ -18,6 +19,19 @@ class LoggerDependency implements Dependency
 {
     public function __invoke(array $settings)
     {
-        return new Logger('kwai');
+        $logger = new Logger('kwai');
+
+        if (isset($settings['logger']['kwai'])) {
+            if (isset($settings['logger']['kwai']['file'])) {
+                $logger->pushHandler(
+                    new StreamHandler(
+                        $settings['logger']['kwai']['file'],
+                        $settings['logger']['kwai']['level'] ?? Logger::DEBUG
+                    )
+                );
+            }
+        }
+
+        return $logger;
     }
 }
