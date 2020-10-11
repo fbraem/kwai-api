ffd129e8bc73fbbcc3970ec6a85d42ed556f6ceb
fbraem <franky.braem@gmail.com>
2020-10-11 11:28:53 +0200

    Add getByUniqueId

--- src/kwai/Modules/Pages/Infrastructure/Repositories/AuthorDatabaseRepository.php
+++ src/kwai/Modules/Pages/Infrastructure/Repositories/AuthorDatabaseRepository.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Kwai\Modules\Pages\Infrastructure\Repositories;
 
 use Kwai\Core\Domain\Entity;
+use Kwai\Core\Domain\ValueObjects\UniqueId;
 use Kwai\Core\Infrastructure\Database\DatabaseRepository;
 use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
@@ -22,21 +23,26 @@ use function Latitude\QueryBuilder\field;
  */
 class AuthorDatabaseRepository extends DatabaseRepository implements AuthorRepository
 {
-    /**
-     * @inheritDoc
-     */
-    public function getById(int $id): Entity
+    private function createQuery()
     {
         $aliasFn = Tables::AUTHORS()->getAliasFn();
-
-        /** @noinspection PhpUndefinedFieldInspection */
-        $query = $this->db->createQueryFactory()
+        return $this->db->createQueryFactory()
             ->select(
                 $aliasFn('id'),
                 $aliasFn('first_name'),
                 $aliasFn('last_name')
             )
             ->from((string) Tables::AUTHORS())
+        ;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getById(int $id): Entity
+    {
+        /** @noinspection PhpUndefinedFieldInspection */
+        $query = $this->createQuery()
             ->where(field(Tables::AUTHORS()->id)->eq($id))
         ;
 
@@ -55,5 +61,28 @@ class AuthorDatabaseRepository extends DatabaseRepository implements AuthorRepos
         return AuthorMapper::toDomain(
             Tables::AUTHORS()->createColumnFilter()->filter($row)
         );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getByUniqueId(UniqueId $uuid): Entity
+    {
+        /** @noinspection PhpUndefinedFieldInspection */
+        $query = $this->createQuery()
+            ->where(field(Tables::AUTHORS()->uuid)->eq($uuid))
+        ;
+
+        try {
+            $user = $this->db->execute($query)->fetch();
+        } catch (QueryException $e) {
+            throw new RepositoryException(__METHOD__, $e);
+        }
+        if ($user) {
+            return AuthorMapper::toDomain(
+                Tables::AUTHORS()->createColumnFilter()->filter($user)
+            );
+        }
+        throw new AuthorNotFoundException($uuid);
     }
 }
--- src/kwai/Modules/Pages/Repositories/AuthorRepository.php
+++ src/kwai/Modules/Pages/Repositories/AuthorRepository.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Kwai\Modules\Pages\Repositories;
 
 use Kwai\Core\Domain\Entity;
+use Kwai\Core\Domain\ValueObjects\UniqueId;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Modules\Pages\Domain\Author;
 use Kwai\Modules\Pages\Domain\Exceptions\AuthorNotFoundException;
@@ -26,4 +27,14 @@ interface AuthorRepository
      * @throws RepositoryException
      */
     public function getById(int $id): Entity;
+
+    /**
+     * Get the author with the given unique id
+     *
+     * @param UniqueId $uuid
+     * @return Entity
+     * @throws AuthorNotFoundException
+     * @throws RepositoryException
+     */
+    public function getByUniqueId(UniqueId $uuid): Entity;
 }
