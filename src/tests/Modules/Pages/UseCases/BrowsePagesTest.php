263fb7cdbc6639b7b34f5b55abbba43fa7471444
fbraem <franky.braem@gmail.com>
2020-10-31 23:17:00 +0100

    Update tests

--- src/tests/Modules/Pages/UseCases/BrowsePagesTest.php
+++ src/tests/Modules/Pages/UseCases/BrowsePagesTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Core\Infrastructure\Repositories\ImageRepository;
+use Kwai\Core\Infrastructure\Repositories\RepositoryException;
+use Kwai\Modules\Pages\Domain\Exceptions\AuthorNotFoundException;
 use Kwai\Modules\Pages\Infrastructure\Repositories\AuthorDatabaseRepository;
 use Kwai\Modules\Pages\Infrastructure\Repositories\PageDatabaseRepository;
 use Kwai\Modules\Pages\UseCases\BrowsePages;
@@ -33,8 +35,12 @@ it('can browse pages', function () use ($context) {
             }
         )($command);
         test()->expectNotToPerformAssertions();
-    } catch (QueryException $qe) {
-        assertTrue(false, (string) $qe);
+    } catch (QueryException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (RepositoryException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (AuthorNotFoundException $e) {
+        $this->assertTrue(false, (string) $e);
     }
 })
     ->skip(!Context::hasDatabase(), 'No database available')
--- src/tests/Modules/Pages/UseCases/CreateUpdatePageTest.php
+++ src/tests/Modules/Pages/UseCases/CreateUpdatePageTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 use Kwai\Core\Domain\Entity;
 use Kwai\Core\Infrastructure\Repositories\ImageRepository;
+use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Core\UseCases\Content;
+use Kwai\Modules\Pages\Domain\Exceptions\ApplicationNotFoundException;
+use Kwai\Modules\Pages\Domain\Exceptions\AuthorNotFoundException;
+use Kwai\Modules\Pages\Domain\Exceptions\PageNotFoundException;
+use Kwai\Modules\Pages\Domain\Page;
 use Kwai\Modules\Pages\Infrastructure\Repositories\ApplicationDatabaseRepository;
 use Kwai\Modules\Pages\Infrastructure\Repositories\AuthorDatabaseRepository;
 use Kwai\Modules\Pages\Infrastructure\Repositories\PageDatabaseRepository;
@@ -31,24 +36,38 @@ it('can create a page', function () use ($context) {
     $content->content = 'This is the content of the page';
     $command->contents[] = $content;
 
-    $page = CreatePage::create(
-        new PageDatabaseRepository($context->db),
-        new ApplicationDatabaseRepository($context->db),
-        new AuthorDatabaseRepository($context->db),
-        new class implements ImageRepository {
-            public function getImages(int $id): array
-            {
-                return [];
-            }
+    try {
+        $page = CreatePage::create(
+            new PageDatabaseRepository($context->db),
+            new ApplicationDatabaseRepository($context->db),
+            new AuthorDatabaseRepository($context->db),
+            new class implements ImageRepository {
+                public function getImages(int $id): array
+                {
+                    return [];
+                }
 
-            public function removeImages(int $id): void
-            {
+                public function removeImages(int $id): void
+                {
+                }
             }
-        }
-    )($command);
-    assertInstanceOf(Entity::class, $page);
-    $context->id = $page->id();
-});
+        )($command);
+        expect($page)
+            ->toBeInstanceOf(Entity::class)
+            ->and($page->domain())
+            ->toBeInstanceOf(Page::class)
+        ;
+        $context->id = $page->id();
+    } catch (RepositoryException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (ApplicationNotFoundException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (AuthorNotFoundException $e) {
+        $this->assertTrue(false, (string) $e);
+    }
+})
+    ->skip(!Context::hasDatabase(), 'No database available')
+;
 
 it('can update a page', function () use ($context) {
     $command = new UpdatePageCommand();
@@ -61,23 +80,39 @@ it('can update a page', function () use ($context) {
     $content->content = 'This is the content of the page';
     $command->contents[] = $content;
 
-    $page = UpdatePage::create(
-        new PageDatabaseRepository($context->db),
-        new ApplicationDatabaseRepository($context->db),
-        new AuthorDatabaseRepository($context->db),
-        new class implements ImageRepository {
-            public function getImages(int $id): array
-            {
-                return [];
-            }
+    try {
+        $page = UpdatePage::create(
+            new PageDatabaseRepository($context->db),
+            new ApplicationDatabaseRepository($context->db),
+            new AuthorDatabaseRepository($context->db),
+            new class implements ImageRepository {
+                public function getImages(int $id): array
+                {
+                    return [];
+                }
 
-            public function removeImages(int $id): void
-            {
+                public function removeImages(int $id): void
+                {
+                }
             }
-        }
-    )($command);
-    assertInstanceOf(Entity::class, $page);
-    /** @noinspection PhpUndefinedMethodInspection */
-    assertEquals('This is an updated summary', $page->getContents()[0]->getSummary());
-    $context->id = $page->id();
-});
+        )($command);
+        expect($page)
+            ->toBeInstanceOf(Entity::class)
+        ;
+        /** @noinspection PhpUndefinedMethodInspection */
+        expect($page->getContents()[0]->getSummary())
+            ->toBe('This is an updated summary')
+        ;
+    } catch (RepositoryException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (ApplicationNotFoundException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (AuthorNotFoundException $e) {
+        $this->assertTrue(false, (string) $e);
+    } catch (PageNotFoundException $e) {
+        $this->assertTrue(false, (string) $e);
+    }
+})
+    ->depends('it it can create a page')
+    ->skip(!Context::hasDatabase(), 'No database available')
+;
