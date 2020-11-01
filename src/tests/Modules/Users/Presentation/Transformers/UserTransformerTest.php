111b11cb699fb82446f76a4362fc3862eff1412b
fbraem <franky.braem@gmail.com>
2020-11-01 20:05:32 +0100

    Use Pest

--- src/tests/Modules/Users/Presentation/Transformers/UserTransformerTest.php
+++ src/tests/Modules/Users/Presentation/Transformers/UserTransformerTest.php
@@ -14,42 +14,36 @@ use Kwai\Modules\Users\Domain\User;
 use Kwai\Modules\Users\Presentation\Transformers\UserTransformer;
 use League\Fractal\Manager;
 use League\Fractal\Serializer\DataArraySerializer;
-use PHPUnit\Framework\TestCase;
 
-final class UserTransformerTest extends TestCase
-{
-    public function testTransform()
-    {
-        $uuid = new UniqueId();
-        $email = new EmailAddress('test@kwai.com');
-        $remark = 'test';
-        $traceableTime = new TraceableTime();
-        $user = new User((object)[
-            'uuid' => $uuid,
-            'emailAddress' => $email,
-            'remark' => $remark,
-            'traceableTime' => $traceableTime
-        ]);
-        $entity = new Entity(1, $user);
+it('can transform a user', function () {
+    $uuid = new UniqueId();
+    $email = new EmailAddress('test@kwai.com');
+    $remark = 'test';
+    $traceableTime = new TraceableTime();
+    $user = new User((object)[
+        'uuid' => $uuid,
+        'emailAddress' => $email,
+        'remark' => $remark,
+        'traceableTime' => $traceableTime
+    ]);
+    $entity = new Entity(1, $user);
 
-        $fractal = new Manager();
-        $fractal->setSerializer(new DataArraySerializer());
-        $data = $fractal
-            ->createData(UserTransformer::createForItem($entity))
-            ->toArray();
+    $fractal = new Manager();
+    $fractal->setSerializer(new DataArraySerializer());
+    $data = $fractal
+        ->createData(UserTransformer::createForItem($entity))
+        ->toArray();
 
-        $this->assertEquals(
-            $data,
-            [
-                'data' => [
-                    'id' => strval($uuid),
-                    'email' => strval($email),
-                    'remark' => 'test',
-                    'created_at' => strval($traceableTime->getCreatedAt()),
-                    'updated_at' => null,
-                    'username' => null
-                ]
+    expect($data)
+        ->toBe([
+            'data' => [
+                'id' => strval($uuid),
+                'email' => strval($email),
+                'username' => '',
+                'remark' => 'test',
+                'created_at' => strval($traceableTime->getCreatedAt()),
+                'updated_at' => null
             ]
-        );
-    }
-}
+        ])
+    ;
+});
