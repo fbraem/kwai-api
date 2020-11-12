7ac7ae69aae2e8607b3fce1e5ef7b3eacc3b5404
fbraem <franky.braem@gmail.com>
2020-11-12 20:30:46 +0100

    Install composer

--- Vagrant.provision.sh
+++ Vagrant.provision.sh
@@ -57,10 +57,12 @@ a2enmod php7.4
 
 phpenmod intl
 
-if ! [ -L /var/www/kwai_api ]; then
-  rm -rf /var/www/kwai_api
-  ln -fs /vagrant/ /var/www/kwai_api
-fi
+# Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
 
 # PHPMyAdmin
 add-apt-repository -y ppa:phpmyadmin/ppa
@@ -69,6 +71,12 @@ apt-get upgrade
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install no"
 apt-get -yq install phpmyadmin
+
+# Document root is a symlink to /vagrant
+if ! [ -L /var/www/kwai_api ]; then
+  rm -rf /var/www/kwai_api
+  ln -fs /vagrant/ /var/www/kwai_api
+fi
 
 # Everything installed, restart apache
 service apache2 restart
