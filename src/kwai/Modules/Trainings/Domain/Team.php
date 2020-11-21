41c79ec057aaafab083c4e1cc9c6f8069718592d
fbraem <franky.braem@gmail.com>
2020-11-21 21:15:22 +0100

    Add Team

---
+++ src/kwai/Modules/Trainings/Domain/Team.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package Modules
+ * @subpackage Trainings
+ */
+declare(strict_types=1);
+
+namespace Kwai\Modules\Trainings\Domain;
+
+use Kwai\Core\Domain\DomainEntity;
+
+/**
+ * Class Team
+ *
+ * Represents a team.
+ */
+class Team implements DomainEntity
+{
+    /**
+     * The name of the team.
+     */
+    private string $name;
+
+    /**
+     * Team constructor.
+     *
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
