20949a1b3c11ee1cebd82809ae85d621d17bf8ca
fbraem <franky.braem@gmail.com>
2020-11-24 19:26:53 +0100

    Install unzip for composer

--- Vagrant.provision.sh
+++ Vagrant.provision.sh
@@ -57,6 +57,9 @@ a2enmod php7.4
 
 phpenmod intl
 
+# Install unzip for Composer
+apt-get install -y unzip
+
 # Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
