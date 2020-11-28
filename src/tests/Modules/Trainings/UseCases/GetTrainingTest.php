6dfeb17ba9f606b5538485f92b07f4b9cf192476
fbraem <franky.braem@gmail.com>
2020-11-28 23:17:45 +0100

    Add expect

--- src/tests/Modules/Trainings/UseCases/GetTrainingTest.php
+++ src/tests/Modules/Trainings/UseCases/GetTrainingTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Kwai\Core\Domain\Entity;
 use Kwai\Modules\Trainings\Infrastructure\Repositories\TrainingDatabaseRepository;
 use Kwai\Modules\Trainings\UseCases\GetTraining;
 use Kwai\Modules\Trainings\UseCases\GetTrainingCommand;
@@ -14,9 +15,12 @@ it('can get a training', function () use ($context) {
     $command->id = 1;
 
     try {
-        GetTraining::create(new TrainingDatabaseRepository(
+        $training = GetTraining::create(new TrainingDatabaseRepository(
             $context->db
         ))($command);
+        expect($training)
+            ->toBeInstanceOf(Entity::class)
+        ;
     } catch (Exception $e) {
         $this->fail((string) $e);
     }
