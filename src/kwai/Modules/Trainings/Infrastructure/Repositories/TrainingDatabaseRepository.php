8befd8de565fc6bd627862829b7a16d0858f7186
fbraem <franky.braem@gmail.com>
2020-12-04 22:41:56 +0100

    Implement execute

--- src/kwai/Modules/Trainings/Infrastructure/Repositories/TrainingDatabaseRepository.php
+++ src/kwai/Modules/Trainings/Infrastructure/Repositories/TrainingDatabaseRepository.php
@@ -7,11 +7,13 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Trainings\Infrastructure\Repositories;
 
+use Illuminate\Support\Collection;
 use Kwai\Core\Domain\Entity;
 use Kwai\Core\Infrastructure\Database\DatabaseRepository;
 use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Modules\Trainings\Domain\Exceptions\TrainingNotFoundException as TrainingNotFoundExceptionAlias;
+use Kwai\Modules\Trainings\Infrastructure\Mappers\TrainingMapper;
 use Kwai\Modules\Trainings\Repositories\TrainingQuery;
 use Kwai\Modules\Trainings\Repositories\TrainingRepository;
 
@@ -31,7 +33,7 @@ class TrainingDatabaseRepository extends DatabaseRepository implements TrainingR
         $query->filterId($id);
 
         try {
-            $entities = $query->execute();
+            $entities = $this->execute($query);
         } catch (QueryException $e) {
             throw new RepositoryException(__METHOD__, $e);
         }
@@ -49,5 +51,20 @@ class TrainingDatabaseRepository extends DatabaseRepository implements TrainingR
     public function createQuery(): TrainingQuery
     {
         return new TrainingDatabaseQuery($this->db);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(
+        TrainingQuery $query,
+        ?int $limit = null,
+        ?int $offset = null
+    ): Collection {
+        /* @var Collection $trainings */
+        $trainings = $query->execute($limit, $offset);
+        return $trainings->mapWithKeys(
+            fn($item, $key) => [ $key => TrainingMapper::toDomain($item) ]
+        );
     }
 }
