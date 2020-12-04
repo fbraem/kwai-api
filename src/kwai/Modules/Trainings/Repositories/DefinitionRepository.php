48b6d600cc96236ff5714383f6074db618e43fda
fbraem <franky.braem@gmail.com>
2020-12-04 22:42:05 +0100

    Add execute

--- src/kwai/Modules/Trainings/Repositories/DefinitionRepository.php
+++ src/kwai/Modules/Trainings/Repositories/DefinitionRepository.php
@@ -7,7 +7,9 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Trainings\Repositories;
 
+use Illuminate\Support\Collection;
 use Kwai\Core\Domain\Entity;
+use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Modules\Trainings\Domain\Exceptions\TrainingDefinitionNotFoundException;
 use Kwai\Modules\Trainings\Domain\Definition;
@@ -33,4 +35,16 @@ interface DefinitionRepository
      * @return DefinitionQuery
      */
     public function createQuery(): DefinitionQuery;
+
+    /**
+     * Executes the query and returns a Collection with entities.
+     * For each record, the mapper will be called to create an entity.
+     *
+     * @param DefinitionQuery $query
+     * @param int|null        $limit
+     * @param int|null        $offset
+     * @return Collection
+     * @throws QueryException
+     */
+    public function execute(DefinitionQuery $query, ?int $limit, ?int $offset): Collection;
 }
