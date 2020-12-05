5e066d8afcdaeebfb98a7ead7e73748c58cc3040
fbraem <franky.braem@gmail.com>
2020-12-05 16:04:15 +0100

    Use Enum

--- src/kwai/Core/Domain/ValueObjects/Weekday.php
+++ src/kwai/Core/Domain/ValueObjects/Weekday.php
@@ -7,30 +7,28 @@ declare(strict_types=1);
 
 namespace Kwai\Core\Domain\ValueObjects;
 
-use InvalidArgumentException;
+use MyCLabs\Enum\Enum;
 
-class Weekday
+/**
+ * Class Weekday
+ *
+ * A class representing a day in the week.
+ *
+ * @method static Weekday MONDAY()
+ * @method static Weekday TUESDAY()
+ * @method static Weekday WEDNESDAY()
+ * @method static Weekday THURSDAY()
+ * @method static Weekday FRIDAY()
+ * @method static Weekday SATURDAY()
+ * @method static Weekday SUNDAY()
+ */
+class Weekday extends Enum
 {
-    private int $day;
-
-    /**
-     * Weekday constructor.
-     *
-     * @param int $day
-     */
-    public function __construct(int $day)
-    {
-        if ($day < 1 || $day > 7) {
-            throw new InvalidArgumentException("$day must be between 1 and 7");
-        }
-        $this->day = $day;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDay(): int
-    {
-        return $this->day;
-    }
+    private const MONDAY = 1;
+    private const TUESDAY = 2;
+    private const WEDNESDAY = 3;
+    private const THURSDAY = 4;
+    private const FRIDAY = 5;
+    private const SATURDAY = 6;
+    private const SUNDAY = 7;
 }
