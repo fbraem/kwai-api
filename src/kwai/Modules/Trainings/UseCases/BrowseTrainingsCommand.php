6745e7ee2edcec6fdfa070d65b845efe534e38ea
fbraem <franky.braem@gmail.com>
2020-12-09 20:34:04 +0100

    Add docs

--- src/kwai/Modules/Trainings/UseCases/BrowseTrainingsCommand.php
+++ src/kwai/Modules/Trainings/UseCases/BrowseTrainingsCommand.php
@@ -14,18 +14,46 @@ namespace Kwai\Modules\Trainings\UseCases;
  */
 class BrowseTrainingsCommand
 {
+    /**
+     * Limit the returned number of trainings
+     *
+     * @var int|null
+     */
     public ?int $limit = null;
+
+    /**
+     * Set the offset
+     *
+     * @var int|null
+     */
     public ?int $offset = null;
 
+    /**
+     * Get only trainings for this year
+     *
+     * @var int|null
+     */
     public ?int $year = null;
+
+    /**
+     * Get only trainings for this month
+     *
+     * @var int|null
+     */
     public ?int $month = null;
 
     /**
      * Get only trainings for this coach
+     *
      * @var int|null
      */
     public ?int $coach = null;
 
-    // By default, only the active trainings will be returned.
+    /**
+     * Get only the active trainings
+     * By default, only the active trainings will be returned.
+     *
+     * @var bool
+     */
     public bool $active = true;
 }
