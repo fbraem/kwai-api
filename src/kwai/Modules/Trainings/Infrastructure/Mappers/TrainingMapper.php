ec0e898aafc7ec21f1b8e738ff9fee1456b0ea4e
fbraem <franky.braem@gmail.com>
2020-12-15 22:09:28 +0100

    Use new mappers and return domain in toDomain

--- src/kwai/Modules/Trainings/Infrastructure/Mappers/TrainingMapper.php
+++ src/kwai/Modules/Trainings/Infrastructure/Mappers/TrainingMapper.php
@@ -30,126 +30,53 @@ class TrainingMapper
      * Maps a table record to the Training domain entity.
      *
      * @param Collection $data
-     * @return Entity<Training>
+     * @return Training
      */
-    public static function toDomain(Collection $data): Entity
+    public static function toDomain(Collection $data): Training
     {
-        $props = $data->transformWithKeys(fn($item, $key) => match ($key) {
-            'remark' => true,
-            'teams' => [ 'teams' => $item->map(fn ($team, $key) => TeamMapper::toDomain($team)) ],
-            'coaches' => [ 'coaches' => $item->map(fn ($coach, $key) =>
-                new TrainingCoach(
-                    coach: CoachMapper::toDomain($coach),
-                    head: $coach->get('coach_type', 0) === 1,
-                    present: $coach->get('present', false),
-                    payed: $coach->get('payed', false),
-                    remark: $coach->get('remark'),
-                    creator: new Creator(
-                    (int) $coach->get('creator')['id'],
-                        new Name(
-                        $coach->get('creator')['first_name'],
-                        $coach->get('creator')['last_name'])
-                    ),
-                    traceableTime: new TraceableTime(
-                        Timestamp::createFromString($coach->get('created_at')),
-                        $coach->has('updated_at')
-                            ? Timestamp::createFromString($coach->get('updated_at'))
-                            : null
-                    )
-                ))
-            ],
-            'definition' => [ 'definition' => DefinitionMapper::toDomain($item) ],
-            'start_date' => [
-                'event' => new Event(
-                    startDate: Timestamp::createFromString($data['start_date'], $data['time_zone']),
-                    endDate: Timestamp::createFromString($data['end_date'], $data['time_zone']),
-                    location: $data->has('location') ? new Location($data->get('location')) : null,
-                    text: $data['contents']->map(fn(Collection $t) => new Text(
-                        locale: new Locale($t['locale']),
-                        format: new DocumentFormat($t['format']),
-                        title: $t['title'],
-                        summary: $t['summary'],
-                        content: $t->get('content'),
-                        author: new Creator(
-                            (int) $t['creator']['id'],
-                            new Name(
-                                first_name: $t['creator']->get('first_name'),
-                                last_name: $t['creator']->get('last_name')
-                            )
-                        )
-                    ))
-                )
-            ],
-            'created_at' => ['traceableTime' => new TraceableTime(
-                Timestamp::createFromString($item),
+        return new Training(
+            event: EventMapper::toDomain($data),
+            text: $data->get('contents')->map(fn ($text) => TextMapper::toDomain($text)),
+            definition: $data->has('definition')
+                ? new Entity((int) $data->get('definition')->get('id'), DefinitionMapper::toDomain($data)) : null,
+            teams: $data->get('teams')->map(fn ($team) => TeamMapper::toDomain($team)),
+            coaches: $data->get('coaches')->map(fn ($coach) => TrainingCoachMapper::toDomain($coach)),
+            remark: $data->get('remark'),
+            presences: new Collection(),
+            traceableTime: new TraceableTime(
+                Timestamp::createFromString($data->get('created_at')),
                 $data->has('updated_at')
                     ? Timestamp::createFromString($data->get('updated_at'))
                     : null
-            )],
-            default => false
-        });
-        return new Entity(
-            (int) $data['id'],
-            new Training(... $props)
+            )
         );
     }
 
     /**
      * Maps a training to a table record
      *
-     * This method will return an array with Collection objects for:
-     * 1. All training properties
-     * 2. All training event contents
-     * 3. All training coaches
-     * 4. All teams for this training
-     *
      * @param Training $training
-     * @return array<Collection>
+     * @return Collection
      */
-    public static function toPersistence(Training $training): array
+    public static function toPersistence(Training $training): Collection
     {
-        return [
-            collect([ // Training data
-                'definition_id' => $training->getDefinition()?->id(),
-                'start_date' => (string) $training->getEvent()->getStartDate(),
-                'end_date' => (string) $training->getEvent()->getEndDate(),
-                'time_zone' => $training->getEvent()->getStartDate()->getTimezone(),
-                'active' => $training->getEvent()->isActive(),
-                'cancelled' => $training->getEvent()->isCancelled(),
-                'location' => $training->getEvent()->getLocation()?->__toString(),
-                'created_at' => (string) $training->getTraceableTime()->getCreatedAt(),
-                'updated_at' => $training->getTraceableTime()->getUpdatedAt()?->__toString()
-            ]),
-            // Event content data
-            $training->getEvent()->getText()->map(fn (Text $text) =>
-                collect([
-                    'locale' => (string) $text->getLocale(),
-                    'format' => (string) $text->getFormat(),
-                    'title' => $text->getTitle(),
-                    'summary' => $text->getSummary(),
-                    'content' => $text->getContent(),
-                    'user_id' => $text->getAuthor()->getId()
-                ])
+        return collect([
+            'definition_id' => $training->getDefinition()?->id(),
+            ... EventMapper::toPersistence($training->getEvent()),
+            'created_at' => (string) $training->getTraceableTime()->getCreatedAt(),
+            'updated_at' => $training->getTraceableTime()->getUpdatedAt()?->__toString(),
+            'remark' => $training->getRemark(),
+            'coaches' => $training->getCoaches()->map(
+                fn (TrainingCoach $coach) => TrainingCoachMapper::toPersistence($coach)
             ),
-            // Coaches data
-            $training->getCoaches()->map(fn (TrainingCoach $coach) =>
-                collect([
-                    'coach_id' => $coach->getCoach()->id(),
-                    'coach_type' => $coach->isHead() ? 1 : 0,
-                    'present' => $coach->isPresent(),
-                    'payed' => $coach->isPayed(),
-                    'remark' => $coach->getRemark(),
-                    'user_id' => $coach->getCreator()->getId(),
-                    'created_at' => $coach->getTraceableTime()->getCreatedAt(),
-                    'updated_at' => $coach->getTraceableTime()->getUpdatedAt(),
-                ])
+            'contents' => $training->getText()->map(
+                fn (Text $text) => TextMapper::toPersistence($text)
             ),
-            // Teams data
-            $training->getTeams()->map(fn (Entity $team) =>
+            'teams' =>  $training->getTeams()->map(fn (Entity $team) =>
                 collect([
                     'team_id' => $team->id()
                 ])
             )
-        ];
+        ]);
     }
 }
