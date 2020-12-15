495c53aeedf29d73b8067e02b85abf6c03d7e9cb
fbraem <franky.braem@gmail.com>
2020-12-15 22:07:07 +0100

    Remove text and remark

--- src/kwai/Core/Domain/ValueObjects/Event.php
+++ src/kwai/Core/Domain/ValueObjects/Event.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 
 namespace Kwai\Core\Domain\ValueObjects;
 
-use Illuminate\Support\Collection;
-
 /**
  * Class Event
  *
@@ -17,107 +15,74 @@ use Illuminate\Support\Collection;
 class Event
 {
     /**
-     * The start of the event.
-     */
-    private Timestamp $startDate;
-
-    /**
-     * The end of the event.
-     */
-    private Timestamp $endDate;
-
-    /**
-     * Is this event active?
-     */
-    private bool $active;
-
-    /**
-     * Is this event cancelled?
-     */
-    private bool $cancelled;
-
-    /**
-     * The location of the event.
-     */
-    private ?Location $location;
-
-    /**
-     * A remark
-     */
-    private ?string $remark;
-
-    /**
-     * Text information for the event.
-     *
-     * @var Collection
-     */
-    private Collection $text;
-
-    /**
      * Event constructor.
      *
      * @param Timestamp       $startDate
      * @param Timestamp       $endDate
      * @param Location|null   $location
-     * @param Collection|null $text
      * @param bool            $active
      * @param bool            $cancelled
-     * @param string|null     $remark
      */
     public function __construct(
-        Timestamp $startDate,
-        Timestamp $endDate,
-        ?Location $location = null,
-        ?Collection $text = null,
-        bool $active = true,
-        bool $cancelled = false,
-        ?string $remark = null
+        private Timestamp $startDate,
+        private Timestamp $endDate,
+        private ?Location $location = null,
+        private bool $active = true,
+        private bool $cancelled = false,
     ) {
         $this->startDate = $startDate;
         $this->endDate = $endDate;
         $this->location = $location;
-        $this->text = $text ?? new Collection();
         $this->active = $active;
         $this->cancelled = $cancelled;
-        $this->remark = $remark;
     }
 
+    /**
+     * Get the start datetime of the event
+     * @return Timestamp
+     */
     public function getStartDate(): Timestamp
     {
         return $this->startDate;
     }
 
+    /**
+     * Get the end datetime of the event
+     * @return Timestamp
+     */
     public function getEndDate(): Timestamp
     {
         return $this->endDate;
     }
 
+    /**
+     * Is this event active?
+     *
+     * @return bool
+     */
     public function isActive(): bool
     {
         return $this->active;
     }
 
+    /**
+     * Is this event cancelled?
+     *
+     * @return bool
+     */
     public function isCancelled(): bool
     {
         return $this->cancelled;
     }
 
+    /**
+     * Get the location of the event
+     *
+     * @return Location|null
+     */
     public function getLocation(): ?Location
     {
         return $this->location;
-    }
-
-    public function getRemark(): ?string
-    {
-        return $this->remark;
-    }
-
-    /**
-     * @return Collection
-     */
-    public function getText(): Collection
-    {
-        return $this->text->collect();
     }
 
     /**
@@ -126,15 +91,5 @@ class Event
     public function cancel(): void
     {
         $this->cancelled = true;
-    }
-
-    /**
-     * Add text content to the event.
-     *
-     * @param Text $text
-     */
-    public function addText(Text $text)
-    {
-        $this->text->push($text);
     }
 }
