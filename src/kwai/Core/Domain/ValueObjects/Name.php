4f87d05592222b67af0d3345717e56f0300a5e31
fbraem <franky.braem@gmail.com>
2020-12-18 21:48:42 +0100

    Use PHP8 constructor, rename properties

--- src/kwai/Core/Domain/ValueObjects/Name.php
+++ src/kwai/Core/Domain/ValueObjects/Name.php
@@ -14,27 +14,23 @@ namespace Kwai\Core\Domain\ValueObjects;
  */
 class Name
 {
-    private ?string $first_name;
-
-    private ?string $last_name;
-
     /**
      * Name constructor.
      *
-     * @param string|null $first_name
-     * @param string|null $last_name
+     * @param string|null $firstname
+     * @param string|null $lastname
      */
-    public function __construct(?string $first_name = null, ?string $last_name = null)
+    public function __construct(
+        private ?string $firstname = null,
+        private ?string $lastname = null)
     {
-        $this->first_name = $first_name;
-        $this->last_name = $last_name;
     }
 
     public function __toString() : string
     {
         return implode(
             ' ',
-            array_filter([$this->first_name, $this->last_name])
+            array_filter([$this->firstname, $this->lastname])
         );
     }
 
@@ -45,7 +41,7 @@ class Name
      */
     public function getFirstName(): ?string
     {
-        return $this->first_name;
+        return $this->firstname;
     }
 
     /**
@@ -55,6 +51,6 @@ class Name
      */
     public function getLastName(): ?string
     {
-        return $this->last_name;
+        return $this->lastname;
     }
 }
