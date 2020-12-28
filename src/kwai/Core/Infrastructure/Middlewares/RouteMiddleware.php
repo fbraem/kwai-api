e5dbd1827aace238a65f93645867e5e65c7e16b8
fbraem <franky.braem@gmail.com>
2020-12-28 19:27:23 +0100

    Middleware for handling a Route

---
+++ src/kwai/Core/Infrastructure/Middlewares/RouteMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package Kwai
+ * @subpackage Core
+ */
+declare(strict_types=1);
+
+namespace Kwai\Core\Infrastructure\Middlewares;
+
+use Kwai\Core\Infrastructure\Presentation\Router;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Class RouteMiddleware
+ *
+ * Middleware for handling routes with FastRoute
+ */
+class RouteMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private Router $router,
+        private ResponseFactoryInterface $responseFactory
+    ) {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        [$route, $action, $parameters, $extra] = $this->router->matchRequest($request);
+        if ( isset($action) && is_callable($action)) {
+            $request = $request
+                ->withAttribute('kwai.route', $route)
+                ->withAttribute('kwai.action', $action)
+                ->withAttribute('kwai.action.args', $parameters)
+                ->withAttribute('kwai.extra', $extra)
+            ;
+        }
+        return $handler->handle($request);
+    }
+}
