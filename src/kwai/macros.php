4581b9182eda4e1e7e13e2eed7be3fe3aae0351d
fbraem <franky.braem@gmail.com>
2020-12-30 21:30:25 +0100

    Add filterColumns and recursive

--- src/kwai/macros.php
+++ src/kwai/macros.php
@@ -45,3 +45,42 @@ Collection::macro('transformWithKeys', function(callable $callback) {
     });
     return $newCollection;
 });
+
+Collection::macro('filterColumns', function(Collection $prefixes) {
+    return $this->keys()->reduce(
+        function ($result, $column) use ($prefixes) {
+            $prefixes->each(function ($prefix, $pos) use (&$result, $column) {
+                if (str_starts_with($column, $prefix)) {
+                    $originalColumn = substr($column, strlen($prefix));
+
+                    // When there is no collection yet for the given position,
+                    // push a new one.
+                    $result->when(!$result->has($pos), function ($collection) {
+                        return $collection->push(new Collection());
+                    });
+
+                    // Put the value in the collection with the original
+                    // column name when it is not null.
+                    $value = $this->get($column, null);
+                    if ($value !== null) {
+                        $result->get($pos)->put($originalColumn, $value);
+                    }
+
+                    // We found the original column, skip the other filters
+                    return false;
+                }
+                return true;
+            });
+            return $result;
+        },
+        new Collection()
+    );
+});
+
+Collection::macro('recursive', function () {
+    return $this->map(function ($value) {
+        return is_array($value)
+            ? (new static($value))->recursive()
+            : $value;
+    });
+});
--- src/tests/Core/MacrosTest.php
+++ src/tests/Core/MacrosTest.php
@@ -23,3 +23,38 @@ it('can transform a collection', function () {
         ])
     ;
  });
+
+it('can create a collection recursively', function () {
+   $arr = [
+       'a' => 1,
+       'b' => [
+           'c' => 3
+       ]
+   ];
+
+   $collect = collect($arr)->recursive();
+   expect($collect)
+       ->toBeInstanceOf(Collection::class)
+       ->and($collect->get('b'))
+       ->toBeInstanceOf(Collection::class)
+   ;
+});
+
+it('can filter columns', function () {
+   $row = collect([
+       'user_name' => 'Jigoro',
+       'grade_name' => '12dan'
+   ]);
+
+   [$user, $grade] = $row->filterColumns(collect(['user_', 'grade_']));
+   expect($user)
+       ->toHaveKey('name')
+       ->and($user->get('name'))
+       ->toBe('Jigoro')
+   ;
+    expect($grade)
+        ->toHaveKey('name')
+        ->and($grade->get('name'))
+        ->toBe('12dan')
+    ;
+});
