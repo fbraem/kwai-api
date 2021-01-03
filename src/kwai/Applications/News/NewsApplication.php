0f33313900245116bbc91ee68efd4be26631ba3c
fbraem <franky.braem@gmail.com>
2021-01-03 22:06:07 +0100

    Use symfony router

--- src/kwai/Applications/News/NewsApplication.php
+++ src/kwai/Applications/News/NewsApplication.php
@@ -7,28 +7,21 @@ declare(strict_types=1);
 
 namespace Kwai\Applications\News;
 
-use Kwai\Applications\Application;
+use Kwai\Applications\KwaiApplication;
 use Kwai\Applications\News\Actions\BrowseStoriesAction;
 use Kwai\Applications\News\Actions\GetArchiveAction;
 use Kwai\Applications\News\Actions\GetStoryAction;
 use Kwai\Core\Infrastructure\Dependencies\ConvertDependency;
 use Kwai\Core\Infrastructure\Dependencies\FileSystemDependency;
 use Kwai\Core\Infrastructure\Presentation\PreflightAction;
-use Slim\Routing\RouteCollectorProxy;
+use Kwai\Core\Infrastructure\Presentation\Router;
+use Psr\Container\ContainerInterface;
 
 /**
  * Class NewsApplication
  */
-class NewsApplication extends Application
+class NewsApplication extends KwaiApplication
 {
-    /**
-     * NewsApplication constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct('news');
-    }
-
     public function addDependencies(): void
     {
         parent::addDependencies();
@@ -40,45 +33,45 @@ class NewsApplication extends Application
     /**
      * @inheritDoc
      */
-    public function createRoutes(RouteCollectorProxy $group): void
+    public function createRouter(): Router
     {
-        $group->group(
-            '/stories',
-            function (RouteCollectorProxy $storiesGroup) {
-                $storiesGroup
-                    ->options('', PreflightAction::class)
-                ;
-                $storiesGroup
-                    ->get('', BrowseStoriesAction::class)
-                    ->setName('news.browse')
-                ;
-            }
-        );
-
-        $group->group(
-            '/stories/{id:[0-9]+}',
-            function (RouteCollectorProxy $storyGroup) {
-                $storyGroup
-                    ->options('', PreflightAction::class)
-                ;
-                $storyGroup
-                    ->get('', GetStoryAction::class)
-                    ->setName('news.read')
-                ;
-            }
-        );
-
-        $group->group(
-            '/archive',
-            function (RouteCollectorProxy $archiveGroup) {
-                $archiveGroup
-                    ->options('', PreflightAction::class)
-                ;
-                $archiveGroup
-                    ->get('', GetArchiveAction::class)
-                    ->setName('news.archive')
-                ;
-            }
-        );
+        return Router::create()
+            ->options(
+                'news.browse.options',
+                '/news/stories',
+            fn() => new PreflightAction()
+            )
+            ->get(
+                'news.browse',
+                '/news/stories',
+                fn(ContainerInterface $container) => new BrowseStoriesAction($container)
+            )
+            ->options(
+                'news.get.options',
+                '/news/stories/{id}',
+                fn() => new PreflightAction(),
+                requirements: [
+                    'id' => '\d+'
+                ]
+            )
+            ->get(
+                'news.get',
+                '/news/stories/{id}',
+                fn(ContainerInterface $container) => new GetStoryAction($container),
+                requirements: [
+                    'id' => '\d+'
+                ]
+            )
+            ->options(
+                'news.archive.options',
+                '/news/archive',
+                fn() => new PreflightAction()
+            )
+            ->get(
+                'news.archive',
+                '/news/archive',
+                fn(ContainerInterface $container) => new GetArchiveAction($container)
+            )
+        ;
     }
 }
