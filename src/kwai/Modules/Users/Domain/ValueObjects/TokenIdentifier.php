5453f9cba0a99813e83d9d116cd184e445f816a0
fbraem <franky.braem@gmail.com>
2021-01-04 21:15:45 +0100

    Use InvalidArgumentException

--- src/kwai/Modules/Users/Domain/ValueObjects/TokenIdentifier.php
+++ src/kwai/Modules/Users/Domain/ValueObjects/TokenIdentifier.php
@@ -9,6 +9,8 @@ declare(strict_types = 1);
 
 namespace Kwai\Modules\Users\Domain\ValueObjects;
 
+use Exception;
+use InvalidArgumentException;
 use function bin2hex;
 use function random_bytes;
 
@@ -21,11 +23,15 @@ final class TokenIdentifier
      * TokenIdentifier constructor.
      *
      * @param string|null $bytes A unique byte string in hex format.
-     * @throws \Exception
      */
     public function __construct(private ?string $bytes = null)
     {
-        $this->bytes ??= bin2hex(random_bytes(40));
+        try {
+            $this->bytes ??= bin2hex(random_bytes(40));
+        } catch (Exception $e)
+        {
+            throw new InvalidArgumentException(message: 'Cannot create TokenIdentifier', previous: $e);
+        }
     }
 
     public function __toString(): string
