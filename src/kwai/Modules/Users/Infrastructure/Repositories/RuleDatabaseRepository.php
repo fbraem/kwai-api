ae52de2a527d458f8aeba59e9ec1de0631215f79
fbraem <franky.braem@gmail.com>
2021-01-08 21:57:17 +0100

    Add RuleQuery

--- src/kwai/Modules/Users/Infrastructure/Repositories/RuleDatabaseRepository.php
+++ src/kwai/Modules/Users/Infrastructure/Repositories/RuleDatabaseRepository.php
@@ -1,27 +1,18 @@
 <?php
 /**
- * @package
- * @subpackage
+ * @package Modules
+ * @subpackage Users
  */
 declare(strict_types=1);
 
 namespace Kwai\Modules\Users\Infrastructure\Repositories;
 
+use Illuminate\Support\Collection;
 use Kwai\Core\Domain\Entity;
-use Kwai\Core\Infrastructure\Database\DatabaseException;
 use Kwai\Core\Infrastructure\Database\DatabaseRepository;
-use Kwai\Core\Infrastructure\Database\QueryException;
-use Kwai\Core\Infrastructure\Repositories\RepositoryException;
-use Kwai\Modules\Users\Domain\Rule;
 use Kwai\Modules\Users\Infrastructure\Mappers\RuleMapper;
-use Kwai\Modules\Users\Infrastructure\Tables;
+use Kwai\Modules\Users\Repositories\RuleQuery;
 use Kwai\Modules\Users\Repositories\RuleRepository;
-use Latitude\QueryBuilder\Query;
-use Latitude\QueryBuilder\Query\AbstractQuery;
-use Latitude\QueryBuilder\Query\SelectQuery;
-use function Latitude\QueryBuilder\alias;
-use function Latitude\QueryBuilder\field;
-use function Latitude\QueryBuilder\on;
 
 /**
  * RuleDatabaseRepository
@@ -31,98 +22,38 @@ class RuleDatabaseRepository extends DatabaseRepository implements RuleRepositor
     /**
      * @inheritDoc
      */
-    public function getAll(?string $subject = null): array
+    public function getAll(
+        ?RuleQuery $query = null,
+        ?int $limit = null,
+        ?int $offset = null
+    ): Collection
     {
-        $select = $this->createBaseQuery();
-        if ($subject) {
-            $select->where(
-                field(Tables::RULE_SUBJECTS()->getColumn('name'))->eq($subject)
-            );
-        }
-        return $this->execute($select);
-    }
+        $query ??= $this->createQuery();
 
-    /**
-     * Create the base query for SELECT
-     *
-     * @return SelectQuery
-     */
-    private function createBaseQuery(): SelectQuery
-    {
-        $aliasRulesFn = Tables::RULES()->getAliasFn();
-
-        /** @noinspection PhpUndefinedFieldInspection */
-        return $this->db->createQueryFactory()
-            ->select(
-                $aliasRulesFn('id'),
-                $aliasRulesFn('name'),
-                $aliasRulesFn('remark'),
-                $aliasRulesFn('created_at'),
-                $aliasRulesFn('updated_at'),
-                // Trick the mapper with the 'rules_' prefix ...
-                alias(
-                    Tables::RULE_ACTIONS()->name,
-                    Tables::RULES()->getAlias('action')
-                ),
-                // Trick the mapper with the 'rules_' prefix ...
-                alias(
-                    Tables::RULE_SUBJECTS()->name,
-                    Tables::RULES()->getAlias('subject')
-                )
-            )
-            ->from((string) Tables::RULES())
-            ->join(
-                (string) Tables::RULE_ACTIONS(),
-                on(
-                    Tables::RULES()->action_id,
-                    Tables::RULE_ACTIONS()->id
-                )
-            )
-            ->join(
-                (string) Tables::RULE_SUBJECTS(),
-                on(
-                    Tables::RULES()->subject_id,
-                    Tables::RULE_SUBJECTS()->id
-                )
-            )
-        ;
+        /* @var Collection $rules */
+        $rules = $query->execute($limit, $offset);
+        return $rules->mapWithKeys(
+            fn($item, $key) => [
+                $key => new Entity((int) $key, RuleMapper::toDomain($item))
+            ]
+        );
     }
 
     /**
      * @inheritDoc
      */
-    public function getByIds(array $ids): array
+    public function getByIds(array $ids): Collection
     {
-        /** @noinspection PhpUndefinedFieldInspection */
-        $query = $this->createBaseQuery()
-            ->where(field(Tables::RULES()->id)->in(...$ids))
-        ;
-        return $this->execute($query);
+        $query = $this->createQuery();
+        $query->filterById(...$ids);
+        return $this->getAll($query);
     }
 
     /**
-     * Execute the query and return an array of Rule entities.
-     *
-     * @param AbstractQuery $query
-     * @return Entity<Rule>[]
-     * @throws RepositoryException
+     * @inheritDoc
      */
-    private function execute(AbstractQuery $query): array
+    public function createQuery(): RuleQuery
     {
-        try {
-            $stmt = $this->db->execute($query);
-        } catch (QueryException $e) {
-            throw new RepositoryException(__METHOD__, $e);
-        }
-        $rows = $stmt->fetchAll();
-
-        $columnFilter = Tables::RULES()->createColumnFilter();
-        $result = [];
-        foreach ($rows as $row) {
-            $rule = RuleMapper::toDomain($columnFilter->filter($row));
-            $result[$rule->id()] = $rule;
-        }
-
-        return $result;
+        return new RuleDatabaseQuery($this->db);
     }
 }
--- src/kwai/Modules/Users/Repositories/RuleRepository.php
+++ src/kwai/Modules/Users/Repositories/RuleRepository.php
@@ -8,28 +8,41 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Users\Repositories;
 
-use Kwai\Core\Domain\Entity;
-use Kwai\Core\Infrastructure\Repositories\RepositoryException;
-use Kwai\Modules\Users\Domain\Rule;
+use Illuminate\Support\Collection;
+use Kwai\Core\Infrastructure\Database\QueryException;
 
 interface RuleRepository
 {
-    /**
-     * Get all rules
-     *
-     * @param string|null $subject
-     * @throws RepositoryException
-     * @return Entity<Rule>[]
-     */
-    public function getAll(?string $subject = null): array;
-
     /**
      * Get all rules with the given ids. The id is used
      * as key of the returned array.
      *
      * @param int[] $ids
-     * @throws RepositoryException
-     * @return Entity<Rule>[]
+     * @return Collection
+     * @throws QueryException
      */
-    public function getByIds(array $ids): array;
+    public function getByIds(array $ids): Collection;
+
+    /**
+     * Factory method to create the related query.
+     *
+     * @return RuleQuery
+     */
+    public function createQuery(): RuleQuery;
+
+    /**
+     * Get all rules with the given query. If no query is passed, all rules
+     * will be returned.
+     *
+     * @param RuleQuery|null $query
+     * @param int|null       $limit
+     * @param int|null       $offset
+     * @return Collection
+     * @throws QueryException
+     */
+    public function getAll(
+        ?RuleQuery $query = null,
+        ?int $limit = null,
+        ?int $offset = 0
+    ): Collection;
 }
