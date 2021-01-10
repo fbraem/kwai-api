6f550c333bde9032a7fc89228dc0618d22864a40
fbraem <franky.braem@gmail.com>
2021-01-10 19:19:38 +0100

    Remove transformWithKeys

--- src/kwai/macros.php
+++ src/kwai/macros.php
@@ -6,46 +6,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Collection;
 
-/**
- * transformWithKeys will create a new collection by iterating over each element
- * and pass the value and key to the callback. The result of the callback will
- * define what will happen.
- * If the returned value is a boolean true, the original key and item will be
- * added. When this value is a boolean false, the key and item will be ignored.
- * When the returned value is an associated array, the first key and value
- * will be added to the collection.
- *
- * ```
- * $col = new Collection(
- *      'name' => 'Jigoro Kano',
- *      'grade' => '12de DAN'
- * );
- * $newCol = $col->transformWithKeys(fn($item, $key) => match($key) {
- *      'name' => ['NAME' => strtoupper($item)],
- *      default => false
- * });
- * ```
- *
- * This code will give the following result:
- * [ 'NAME' => 'JIGORO KANO' ]
- */
-Collection::macro('transformWithKeys', function(callable $callback) {
-    $newCollection = new Collection();
-    $this->each(function($item, $key) use ($callback, $newCollection) {
-        $result = $callback($item, $key);
-        if (is_bool($result)) {
-            if ($result) {
-                $newCollection->put($key, $item);
-            }
-        }
-        else if (is_array($result)) {
-            $newKey = array_key_first($result);
-            $newCollection->put($newKey, $result[$newKey]);
-        }
-    });
-    return $newCollection;
-});
-
 Collection::macro('filterColumns', function(Collection $prefixes) {
     return $this->keys()->reduce(
         function ($result, $column) use ($prefixes) {
--- src/tests/Core/MacrosTest.php
+++ src/tests/Core/MacrosTest.php
@@ -4,26 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Collection;
 
-it('can transform a collection', function () {
-    $col = new Collection([
-        'name' => 'Jigoro Kano',
-        'grade' => '12de DAN',
-        'country' => 'Japan'
-    ]);
-    $newCol = $col->transformWithKeys(fn($item, $key) => match($key) {
-        'name' => ['NAME' => strtoupper($item)],
-        'country' => true,
-        default => false
-    });
-
-    expect($newCol->toArray())
-        ->toMatchArray([
-            'NAME' => 'JIGORO KANO',
-            'country' => 'Japan'
-        ])
-    ;
- });
-
 it('can create a collection recursively', function () {
    $arr = [
        'a' => 1,
