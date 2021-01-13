cb81bbb8d38d021f9a485b38608a49a92a848b2a
fbraem <franky.braem@gmail.com>
2021-01-13 22:25:20 +0100

    Use changed repository methods

--- src/kwai/Modules/Users/UseCases/AttachAbilityToUser.php
+++ src/kwai/Modules/Users/UseCases/AttachAbilityToUser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package Kwai
+ * @package Modules
  * @subpackage Users
  */
 declare(strict_types=1);
@@ -8,9 +8,12 @@ declare(strict_types=1);
 namespace Kwai\Modules\Users\UseCases;
 
 use Kwai\Core\Domain\Entity;
-use Kwai\Core\Domain\Exceptions\NotFoundException;
 use Kwai\Core\Domain\ValueObjects\UniqueId;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
+use Kwai\Modules\Users\Domain\Exceptions\AbilityNotFoundException;
+use Kwai\Modules\Users\Domain\Exceptions\UserNotFoundException;
+use Kwai\Modules\Users\Infrastructure\Repositories\AbilityDatabaseRepository;
+use Kwai\Modules\Users\Infrastructure\Repositories\UserDatabaseRepository;
 use Kwai\Modules\Users\Repositories\AbilityRepository;
 use Kwai\Modules\Users\Repositories\UserRepository;
 
@@ -18,50 +21,52 @@ use Kwai\Modules\Users\Repositories\UserRepository;
  * Class AttachAbilityToUser
  *
  * Use case to attach an ability to a user.
- * - Step 1 - Get the user
- * - Step 2 - Get the abilities of the user
- * - Step 3 - Get the ability
- * - Step 4 - Add the ability
  */
 class AttachAbilityToUser
 {
-    private UserRepository $userRepo;
-
-    private AbilityRepository $abilityRepo;
-
     /**
      * AttachAbilityToUser constructor.
      *
      * @param UserRepository    $userRepo
      * @param AbilityRepository $abilityRepo
      */
-    public function __construct(UserRepository $userRepo, AbilityRepository $abilityRepo)
-    {
+    public function __construct(
+        private UserRepository $userRepo,
+        private AbilityRepository $abilityRepo
+    ) {
         $this->userRepo = $userRepo;
         $this->abilityRepo = $abilityRepo;
     }
 
     /**
+     * Factory method
+     *
+     * @param UserDatabaseRepository    $userRepo
+     * @param AbilityDatabaseRepository $abilityRepo
+     * @return static
+     */
+    public static function create(
+        UserDatabaseRepository $userRepo,
+        AbilityDatabaseRepository $abilityRepo
+    ): self {
+        return new self($userRepo, $abilityRepo);
+    }
+
+    /**
      * @param AttachAbilityToUserCommand $command
      * @return Entity
-     * @throws NotFoundException
      * @throws RepositoryException
+     * @throws UserNotFoundException
+     * @throws AbilityNotFoundException
      */
     public function __invoke(AttachAbilityToUserCommand $command): Entity
     {
-        $user = $this->userRepo->getByUUID(new UniqueId($command->uuid));
-        $abilities = $this->abilityRepo->getByUser($user);
-        /** @noinspection PhpUndefinedMethodInspection */
-        $user->setAbilities($abilities);
-
-        // Already added
-        if (isset($abilities[$command->abilityId])) {
-            return $user;
-        }
-
+        $user = $this->userRepo->getByUniqueId(new UniqueId($command->uuid));
         // Add it
         $ability = $this->abilityRepo->getById($command->abilityId);
-        $this->userRepo->addAbility($user, $ability);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $user->addAbility($ability);
+        $this->userRepo->update($user);
 
         return $user;
     }
