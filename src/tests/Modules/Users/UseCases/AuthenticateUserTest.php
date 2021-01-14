7334e75c9e319eb0ba262658e86f72793b7c9e6b
fbraem <franky.braem@gmail.com>
2021-01-14 20:14:07 +0100

    Update for changed code

--- src/tests/Modules/Users/UseCases/AuthenticateUserTest.php
+++ src/tests/Modules/Users/UseCases/AuthenticateUserTest.php
@@ -6,11 +6,12 @@ declare(strict_types=1);
 
 namespace Tests\Modules\Users\UseCases;
 
+use Exception;
 use Kwai\Core\Domain\Entity;
 
 use Kwai\Core\Domain\Exceptions\NotFoundException;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
-use Kwai\Modules\Users\Infrastructure\Repositories\UserDatabaseRepository;
+use Kwai\Modules\Users\Infrastructure\Repositories\UserAccountDatabaseRepository;
 use Kwai\Modules\Users\Infrastructure\Repositories\AccessTokenDatabaseRepository;
 use Kwai\Modules\Users\Infrastructure\Repositories\RefreshTokenDatabaseRepository;
 use Kwai\Modules\Users\UseCases\AuthenticateUser;
@@ -27,23 +28,19 @@ it('can authenticate a user', function () use ($context) {
     $command->password = $_ENV['password'];
 
     try {
-        $refreshToken = (new AuthenticateUser(
-            new UserDatabaseRepository($context->db),
+        $refreshToken = AuthenticateUser::create(
+            new UserAccountDatabaseRepository($context->db),
             new AccessTokenDatabaseRepository($context->db),
             new RefreshTokenDatabaseRepository($context->db)
-        ))($command);
+        )($command);
         expect($refreshToken)
             ->toBeInstanceOf(Entity::class)
         ;
         expect($refreshToken->domain())
             ->toBeInstanceOf(RefreshToken::class)
         ;
-    } catch (NotFoundException $e) {
-        $this->assertTrue(true, (string) $e);
-    } catch (AuthenticationException $e) {
-        $this->assertTrue(true, (string) $e);
-    } catch (RepositoryException $e) {
-        $this->assertTrue(true, (string) $e);
+    } catch (Exception $e) {
+        $this->fail((string) $e);
     }
 })
     ->skip(!Context::hasDatabase(), 'No database available')
@@ -56,15 +53,15 @@ it('can handle a wrong authentication', function () use ($context) {
 
     try {
         /** @noinspection PhpUnhandledExceptionInspection */
-        (new AuthenticateUser(
-            new UserDatabaseRepository($context->db),
+        AuthenticateUser::create(
+            new UserAccountDatabaseRepository($context->db),
             new AccessTokenDatabaseRepository($context->db),
             new RefreshTokenDatabaseRepository($context->db)
-        ))($command);
+        )($command);
     } catch (NotFoundException $e) {
-        $this->assertTrue(true, (string) $e);
+        $this->fail((string) $e);
     } catch (RepositoryException $e) {
-        $this->assertTrue(true, (string) $e);
+        $this->fail((string) $e);
     }
 })
     ->throws(AuthenticationException::class)
