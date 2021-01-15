a1e9448aebeaa46426423ded5da171b4dd5a8803
fbraem <franky.braem@gmail.com>
2021-01-15 22:36:36 +0100

    Add command variables

--- src/kwai/Modules/Users/UseCases/BrowseUserInvitationsCommand.php
+++ src/kwai/Modules/Users/UseCases/BrowseUserInvitationsCommand.php
@@ -14,4 +14,29 @@ namespace Kwai\Modules\Users\UseCases;
  */
 class BrowseUserInvitationsCommand
 {
+    /**
+     * Only show invitations that are still valid until the given time.
+     * @var string|null
+     */
+    public ?string $active_time = null;
+
+    /**
+     * The timezone of $active_time
+     * @var string|null
+     */
+    public ?string $active_timezone = null;
+
+    /**
+     * Limit of returned records
+     *
+     * @var int|null
+     */
+    public ?int $limit = null;
+
+    /**
+     * Offset to start returning records
+     *
+     * @var int|null
+     */
+    public ?int $offset = null;
 }
