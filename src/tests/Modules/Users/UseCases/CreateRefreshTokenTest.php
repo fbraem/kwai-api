2d316cd81175111bbede5a16ef1c0df2b7fab040
fbraem <franky.braem@gmail.com>
2021-01-16 21:41:33 +0100

    Update for new code and PHP8

--- src/tests/Modules/Users/UseCases/CreateRefreshTokenTest.php
+++ src/tests/Modules/Users/UseCases/CreateRefreshTokenTest.php
@@ -3,14 +3,12 @@ declare(strict_types=1);
 
 namespace Tests\Modules\Users\UseCases;
 
+use Exception;
 use Kwai\Core\Domain\Entity;
-use Kwai\Core\Domain\Exceptions\NotFoundException;
-use Kwai\Core\Infrastructure\Repositories\RepositoryException;
-use Kwai\Modules\Users\Domain\Exceptions\AuthenticationException;
 use Kwai\Modules\Users\Domain\RefreshToken;
 use Kwai\Modules\Users\Infrastructure\Repositories\AccessTokenDatabaseRepository;
 use Kwai\Modules\Users\Infrastructure\Repositories\RefreshTokenDatabaseRepository;
-use Kwai\Modules\Users\Infrastructure\Repositories\UserDatabaseRepository;
+use Kwai\Modules\Users\Infrastructure\Repositories\UserAccountDatabaseRepository;
 use Kwai\Modules\Users\UseCases\AuthenticateUser;
 use Kwai\Modules\Users\UseCases\AuthenticateUserCommand;
 use Kwai\Modules\Users\UseCases\CreateRefreshToken;
@@ -29,7 +27,7 @@ it('can create a refreshtoken', function () use ($context) {
 
     try {
         $refreshToken = (new AuthenticateUser(
-            new UserDatabaseRepository($context->db),
+            new UserAccountDatabaseRepository($context->db),
             $accessTokenRepo,
             $refreshTokenRepo
         ))($command);
@@ -49,12 +47,8 @@ it('can create a refreshtoken', function () use ($context) {
         expect($refreshToken->domain())
             ->toBeInstanceOf(RefreshToken::class)
         ;
-    } catch (NotFoundException $e) {
-        $this->assertTrue(false, (string) $e);
-    } catch (AuthenticationException $e) {
-        $this->assertTrue(false, (string) $e);
-    } catch (RepositoryException $e) {
-        $this->assertTrue(false, (string) $e);
+    } catch (Exception $e) {
+        $this->fail((string) $e);
     }
 })
     ->skip(!Context::hasDatabase(), 'No database available')
