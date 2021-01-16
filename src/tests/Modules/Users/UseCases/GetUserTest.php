53008efd1d9282f4e61a8591f6f803e311774eb2
fbraem <franky.braem@gmail.com>
2021-01-16 23:03:11 +0100

    Add GetUserTest

---
+++ src/tests/Modules/Users/UseCases/GetUserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Kwai\Core\Domain\Entity;
+use Kwai\Core\Domain\ValueObjects\EmailAddress;
+use Kwai\Modules\Users\Infrastructure\Repositories\UserAccountDatabaseRepository;
+use Kwai\Modules\Users\Infrastructure\Repositories\UserDatabaseRepository;
+use Kwai\Modules\Users\UseCases\GetUser;
+use Kwai\Modules\Users\UseCases\GetUserCommand;
+use Tests\Context;
+
+$context = Context::createContext();
+
+it('can get an ability', function () use ($context) {
+    $accountRepo = new UserAccountDatabaseRepository($context->db);
+    try {
+        $account = $accountRepo->get(new EmailAddress('jigoro.kano@kwai.com'));
+    } catch (Exception $e) {
+        $this->fail((string) $e);
+    }
+
+    $command = new GetUserCommand();
+    $command->uuid = (string) $account->getUser()->getUuid();
+
+    try {
+        $user = GetUser::create(new UserDatabaseRepository($context->db))($command);
+        expect($user)
+            ->toBeInstanceOf(Entity::class)
+            ->and((string) $user->getEmailAddress())
+            ->toBe('jigoro.kano@kwai.com')
+        ;
+    } catch (Exception $e) {
+        $this->fail((string) $e);
+    }
+})
+    ->skip(!Context::hasDatabase(), 'No database available')
+;
