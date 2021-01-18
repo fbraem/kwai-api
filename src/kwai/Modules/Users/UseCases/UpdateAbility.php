0db05b3d5115f617d28af76307e3fc7c43da20f0
fbraem <franky.braem@gmail.com>
2021-01-18 19:43:54 +0100

    PHP8 and create

--- src/kwai/Modules/Users/UseCases/UpdateAbility.php
+++ src/kwai/Modules/Users/UseCases/UpdateAbility.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package Kwai
+ * @package Modules
  * @subpackage Users
  */
 declare(strict_types=1);
@@ -8,9 +8,9 @@ declare(strict_types=1);
 namespace Kwai\Modules\Users\UseCases;
 
 use Kwai\Core\Domain\Entity;
-use Kwai\Core\Domain\Exceptions\NotFoundException;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Modules\Users\Domain\Ability;
+use Kwai\Modules\Users\Domain\Exceptions\AbilityNotFoundException;
 use Kwai\Modules\Users\Repositories\AbilityRepository;
 use Kwai\Modules\Users\Repositories\RuleRepository;
 
@@ -22,73 +22,53 @@ use Kwai\Modules\Users\Repositories\RuleRepository;
 class UpdateAbility
 {
     /**
-     * The ability repository
-     */
-    private AbilityRepository $abilityRepo;
-
-    /**
-     * The rule repository
-     */
-    private RuleRepository $ruleRepo;
-
-    /**
      * UpdateAbility constructor.
      *
      * @param AbilityRepository $abilityRepo
      * @param RuleRepository $ruleRepo
      */
-    public function __construct(AbilityRepository $abilityRepo, RuleRepository $ruleRepo)
-    {
-        $this->abilityRepo = $abilityRepo;
-        $this->ruleRepo = $ruleRepo;
+    public function __construct(
+        private AbilityRepository $abilityRepo,
+        private RuleRepository $ruleRepo
+    ) {
+    }
+
+    /**
+     * Factory method
+     *
+     * @param AbilityRepository $abilityRepo
+     * @param RuleRepository    $ruleRepo
+     * @return UpdateAbility
+     */
+    public static function create(
+        AbilityRepository $abilityRepo,
+        RuleRepository $ruleRepo
+    ): self {
+        return new self($abilityRepo, $ruleRepo);
     }
 
     /**
      * @param UpdateAbilityCommand $command
      * @return Entity<Ability>
      * @throws RepositoryException
-     * @throws NotFoundException
+     * @throws AbilityNotFoundException
      */
     public function __invoke(UpdateAbilityCommand $command): Entity
     {
         $ability = $this->abilityRepo->getById($command->id);
         $newRules = $this->ruleRepo->getByIds($command->rules);
-        $currentRules = $ability->getRules();
-
-        // All rules that are not passed, must be deleted
-        $rulesToDelete = array_udiff(
-            $currentRules,
-            $newRules,
-            fn($a, $b) => $a->id() == $b->id() ? 0 : -1
-        );
-        $this->abilityRepo->deleteRules($ability, $rulesToDelete);
-        // Remove the deleted rules from the list
-        $currentRules = array_udiff(
-            $currentRules,
-            $rulesToDelete,
-            fn($a, $b) => $a->id() == $b->id() ? 0 : -1
-        );
-
-        $rulesToAdd = array_udiff(
-            $newRules,
-            $currentRules,
-            fn($a, $b) => $a->id() == $b->id() ? 0 : -1
-        );
-        $this->abilityRepo->addRules($ability, $rulesToAdd);
-        foreach ($rulesToAdd as $rule) {
-            $currentRules[$rule->id()] = $rule;
-        }
 
         $traceableTime = $ability->getTraceableTime();
         $traceableTime->markUpdated();
+
         $ability = new Entity(
             $ability->id(),
-            new Ability((object) [
-                'name' => $command->name,
-                'remark' => $command->remark,
-                'traceableTime' => $traceableTime,
-                'rules' => $currentRules
-            ])
+            new Ability(
+                name: $command->name,
+                remark: $command->remark,
+                traceableTime: $traceableTime,
+                rules: $newRules
+            )
         );
         $this->abilityRepo->update($ability);
 
