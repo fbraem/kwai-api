0dd9cc8328b295ae843c048af6c59c68e9bf71eb
fbraem <franky.braem@gmail.com>
2021-01-20 20:51:55 +0100

    Add getAll

--- src/kwai/Core/Infrastructure/Database/DatabaseRepository.php
+++ src/kwai/Core/Infrastructure/Database/DatabaseRepository.php
@@ -7,17 +7,62 @@ declare(strict_types=1);
 
 namespace Kwai\Core\Infrastructure\Database;
 
+use Closure;
+use Illuminate\Support\Collection;
+use Kwai\Core\Domain\Entity;
+use Kwai\Core\Infrastructure\Repositories\Query;
+use Kwai\Core\Infrastructure\Repositories\RepositoryException;
+
 /**
  * Class DatabaseRepository
  *
  * Base class for database repositories
  */
-class DatabaseRepository
+abstract class DatabaseRepository
 {
-    protected Connection $db;
+    /**
+     * DatabaseRepository constructor.
+     *
+     * @todo When all repositories have a mapper function, remove nullable
+     * @param Connection   $db
+     * @param Closure|null $mapFunction
+     */
+    public function __construct(
+        protected Connection $db,
+        private ?Closure $mapFunction = null
+    ) {
+    }
 
-    public function __construct(Connection $db)
+    /**
+     * @todo Make it abstract
+     * @return Query|null
+     */
+    public function createQuery(): ?Query
     {
-        $this->db = $db;
+        return null;
+    }
+
+    /**
+     * @param Query|null $query
+     * @param int|null   $limit
+     * @param int|null   $offset
+     * @return Collection
+     * @throws RepositoryException
+     */
+    public function getAll(?Query $query = null, ?int $limit = null, ?int $offset = null): Collection
+    {
+        $query ??= $this->createQuery();
+
+        try {
+            $pages = $query->execute($limit, $offset);
+        } catch (QueryException $e) {
+            throw new RepositoryException(__METHOD__, $e);
+        }
+
+        return $pages->mapWithKeys(
+            fn($item, $key) => [
+                $key => new Entity((int) $key, ($this->mapFunction)($item))
+            ]
+        );
     }
 }
