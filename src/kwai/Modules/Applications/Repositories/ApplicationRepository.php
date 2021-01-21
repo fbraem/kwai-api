6f992b1a7e4e5f7ae101ef342405a804c9663732
fbraem <franky.braem@gmail.com>
2021-01-21 19:25:13 +0100

    $query argument can be null

--- src/kwai/Modules/Applications/Repositories/ApplicationRepository.php
+++ src/kwai/Modules/Applications/Repositories/ApplicationRepository.php
@@ -7,7 +7,9 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Applications\Repositories;
 
+use Illuminate\Support\Collection;
 use Kwai\Core\Domain\Entity;
+use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Modules\Applications\Domain\Application;
 use Kwai\Modules\Applications\Domain\Exceptions\ApplicationNotFoundException;
@@ -33,4 +35,20 @@ interface ApplicationRepository
      * @return ApplicationQuery
      */
     public function createQuery(): ApplicationQuery;
+
+    /**
+     * Executes the query and returns a collection with entities.
+     * For each record, the mapper will be called to create an entity.
+     *
+     * @param ApplicationQuery|null $query
+     * @param int|null              $limit
+     * @param int|null              $offset
+     * @return Collection
+     * @throws QueryException
+     */
+    public function getAll(
+        ?ApplicationQuery $query = null,
+        ?int $limit = null,
+        ?int $offset = null
+    ): Collection;
 }
