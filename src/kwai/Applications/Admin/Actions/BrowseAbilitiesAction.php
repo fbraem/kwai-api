5586b235f60f32f70ff78d3b0c81037d95bf23ea
fbraem <franky.braem@gmail.com>
2021-01-22 20:54:23 +0100

    Use create and log exceptions

--- src/kwai/Applications/Admin/Actions/BrowseAbilitiesAction.php
+++ src/kwai/Applications/Admin/Actions/BrowseAbilitiesAction.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Kwai\Applications\Admin\Actions;
 
+use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Core\Infrastructure\Presentation\Responses\ResourceResponse;
 use Kwai\Core\Infrastructure\Presentation\Responses\SimpleResponse;
 use Kwai\Core\Infrastructure\Presentation\Action;
@@ -32,13 +33,19 @@ class BrowseAbilitiesAction extends Action
     {
         $repo = new AbilityDatabaseRepository($this->getContainerEntry('pdo_db'));
         try {
-            $users = (new BrowseAbilities($repo))(new BrowseAbilitiesCommand());
+            $users = BrowseAbilities::create($repo)(new BrowseAbilitiesCommand());
             return (new ResourceResponse(
                 AbilityTransformer::createForCollection($users)
             ))($response);
         } catch (RepositoryException $e) {
+            $this->logException($e);
             return (
                 new SimpleResponse(500, 'A repository exception occurred.')
+            )($response);
+        } catch (QueryException $e) {
+            $this->logException($e);
+            return (
+                new SimpleResponse(500, 'A query exception occurred.')
             )($response);
         }
     }
