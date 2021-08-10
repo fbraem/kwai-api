31b0204a54b33d9d816a8e3933a56318e91a1783
fbraem <franky.braem@gmail.com>
2021-08-10 20:45:30 +0200

    Use required state for id

--- src/kwai/Modules/News/Presentation/REST/StorySchema.php
+++ src/kwai/Modules/News/Presentation/REST/StorySchema.php
@@ -7,9 +7,6 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\News\Presentation\REST;
 
-use Kwai\Core\UseCases\Content;
-use Kwai\Modules\News\UseCases\CreateStoryCommand;
-use Kwai\Modules\News\UseCases\UpdateStoryCommand;
 use Nette\Schema\Elements\Structure;
 use Nette\Schema\Expect;
 use Nette\Schema\Processor;
@@ -25,38 +22,35 @@ class StorySchema
 
     public function __construct(bool $create = false)
     {
-        $dataSchema = [
-            'type' => Expect::anyOf('stories'),
-            'attributes' => Expect::Structure([
-                'enabled' => Expect::bool(false),
-                'publish_date' => Expect::string()->required()->pattern(self::DATETIME_PATTERN),
-                'timezone' => Expect::string()->required(),
-                'end_date' => Expect::string()->nullable()->pattern(self::DATETIME_PATTERN),
-                'promotion' => Expect::int(0),
-                'promotion_end_date' => Expect::string()->nullable()->pattern(self::DATETIME_PATTERN),
-                'remark' => Expect::string()->nullable(),
-                'contents' => Expect::arrayOf(Expect::structure([
-                    'title' => Expect::string()->required(),
-                    'locale' => Expect::string('nl'),
-                    'format' => Expect::string('md'),
-                    'summary' => Expect::string()->required(),
-                    'content' => Expect::string()
-                ]))->required()
-            ]),
-            'relationships' => Expect::structure([
-                'application' => Expect::structure([
-                    'data' => Expect::structure([
-                        'type' => Expect::string(),
-                        'id' => Expect::string()
-                    ])
-                ])->required()
-            ])
-        ];
-        if (!$create) {
-            $dataSchema['id'] = Expect::string()->required();
-        }
         $this->schema = Expect::structure([
-            'data' => Expect::structure($dataSchema),
+            'data' => Expect::structure([
+                'type' => Expect::anyOf('stories'),
+                'id' => Expect::string()->required(!$create),
+                'attributes' => Expect::Structure([
+                    'enabled' => Expect::bool(false),
+                    'publish_date' => Expect::string()->required()->pattern(self::DATETIME_PATTERN),
+                    'timezone' => Expect::string()->required(),
+                    'end_date' => Expect::string()->nullable()->pattern(self::DATETIME_PATTERN),
+                    'promotion' => Expect::int(0),
+                    'promotion_end_date' => Expect::string()->nullable()->pattern(self::DATETIME_PATTERN),
+                    'remark' => Expect::string()->nullable(),
+                    'contents' => Expect::arrayOf(Expect::structure([
+                        'title' => Expect::string()->required(),
+                        'locale' => Expect::string('nl'),
+                        'format' => Expect::string('md'),
+                        'summary' => Expect::string()->required(),
+                        'content' => Expect::string()
+                    ]))->required()
+                ]),
+                'relationships' => Expect::structure([
+                    'application' => Expect::structure([
+                        'data' => Expect::structure([
+                            'type' => Expect::string()->required(),
+                            'id' => Expect::string()->required()
+                        ])
+                    ])
+                ])
+            ])
         ]);
     }
 
