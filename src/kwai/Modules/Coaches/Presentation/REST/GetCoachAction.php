470bc93c4aa81006d087044027bf9fc34d1bab94
fbraem <franky.braem@gmail.com>
2021-08-29 19:36:27 +0200

    Add GetCoach

--- src/kwai/Modules/Coaches/Presentation/REST/GetCoachAction.php
+++ src/kwai/Modules/Coaches/Presentation/REST/GetCoachAction.php
@@ -7,7 +7,18 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Coaches\Presentation\REST;
 
+use Kwai\Core\Infrastructure\Database\Connection;
+use Kwai\Core\Infrastructure\Dependencies\DatabaseDependency;
 use Kwai\Core\Infrastructure\Presentation\Action;
+use Kwai\Core\Infrastructure\Presentation\Responses\NotFoundResponse;
+use Kwai\Core\Infrastructure\Presentation\Responses\ResourceResponse;
+use Kwai\Core\Infrastructure\Presentation\Responses\SimpleResponse;
+use Kwai\Core\Infrastructure\Repositories\RepositoryException;
+use Kwai\Modules\Coaches\Domain\Exceptions\CoachNotFoundException;
+use Kwai\Modules\Coaches\Infrastructure\Repositories\CoachDatabaseRepository;
+use Kwai\Modules\Coaches\Presentation\Transformers\CoachTransformer;
+use Kwai\Modules\Coaches\UseCases\GetCoach;
+use Kwai\Modules\Coaches\UseCases\GetCoachCommand;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -16,10 +27,36 @@ use Psr\Http\Message\ServerRequestInterface as Request;
  */
 class GetCoachAction extends Action
 {
+    public function __construct(
+        private ?Connection $database = null
+    ) {
+        parent::__construct();
+        $this->database ??= depends('kwai.database', DatabaseDependency::class);
+    }
+
     /**
      * @inheritDoc
      */
     public function __invoke(Request $request, Response $response, array $args)
     {
+        $command = new GetCoachCommand();
+        $command->id = (int) $args['id'];
+
+        try {
+            $coach = GetCoach::create(new CoachDatabaseRepository($this->database))($command);
+        } catch (RepositoryException $e) {
+            $this->logException($e);
+            return (
+                new SimpleResponse(500, 'A repository exception occurred.')
+            )($response);
+
+        } catch (CoachNotFoundException) {
+            return (new NotFoundResponse('Coach not found'))($response);
+        }
+
+        $user = $request->getAttribute('kwai.user');
+        $resource = CoachTransformer::createForItem($coach, $user);
+
+        return (new ResourceResponse($resource))($response);
     }
 }
