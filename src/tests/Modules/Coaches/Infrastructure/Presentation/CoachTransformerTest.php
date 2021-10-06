07d819d625bc81dd846bf4bcace28195896cedfa
fbraem <franky.braem@gmail.com>
2021-10-06 20:39:24 +0200

    Use toHaveKey and dataset

--- src/tests/Modules/Coaches/Infrastructure/Presentation/CoachTransformerTest.php
+++ src/tests/Modules/Coaches/Infrastructure/Presentation/CoachTransformerTest.php
@@ -3,44 +3,39 @@
 declare(strict_types=1);
 
 use Kwai\Core\Domain\Entity;
-use Kwai\Core\Domain\ValueObjects\Creator;
 use Kwai\Core\Domain\ValueObjects\Name;
 use Kwai\Modules\Coaches\Domain\Coach;
 use Kwai\Modules\Coaches\Domain\Member;
 use Kwai\Modules\Coaches\Presentation\Transformers\CoachTransformer;
 use League\Fractal\Manager;
-use League\Fractal\Serializer\DataArraySerializer;
+use League\Fractal\Serializer\JsonApiSerializer;
 
-it('can transform a coach', function () {
-    $coach = new Coach(
-        bio: 'The founder of Judo',
-        member: new Entity(1, new Member(
-            name: new Name('Jigoro', 'Kano')
-        )),
-        active: true,
-        creator: new Creator(
-            1,
-            new Name()
-        ),
-        diploma: 'Founder'
-    );
-
+it('can transform a coach', function ($coach) {
     $fractal = new Manager();
-    $fractal->setSerializer(new DataArraySerializer());
+    $fractal->setSerializer(new JsonApiSerializer());
     $data = $fractal
-        ->createData(CoachTransformer::createForItem(new Entity(1, $coach)))
+        ->createData(CoachTransformer::createForItem($coach))
         ->toArray()
     ;
 
     expect($data)
-        ->toBeArray()
-        ->toHaveKey('data')
-        ->and($data['data'])
-        ->toBeArray()
-        ->toMatchArray([
-            'id' => '1',
-            'updated_at' => null,
-            'bio' => 'The founder of Judo'
-        ])
+        ->toHaveKey('data.type', 'coaches')
+        ->toHaveKey('data.id', '1')
+        ->toHaveKey('data.attributes.updated_at', null)
+        ->toHaveKey('data.attributes.bio', 'The founder of Judo')
+        ->toHaveKey('data.attributes.diploma', 'Founder')
     ;
-});
+})
+->with([
+    new Entity(
+        1,
+        new Coach(
+            bio: 'The founder of Judo',
+            member: new Entity(1, new Member(
+                name: new Name('Jigoro', 'Kano')
+            )),
+            active: true,
+            diploma: 'Founder'
+        )
+    )
+]);
