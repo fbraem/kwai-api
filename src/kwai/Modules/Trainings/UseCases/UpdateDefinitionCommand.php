ca9928b497bd0c1f628e8b3885caa1ee72cb2f30
fbraem <franky.braem@gmail.com>
2021-10-11 15:57:03 +0200

    Add DefinitionTrait

--- src/kwai/Modules/Trainings/UseCases/UpdateDefinitionCommand.php
+++ src/kwai/Modules/Trainings/UseCases/UpdateDefinitionCommand.php
@@ -19,58 +19,5 @@ class UpdateDefinitionCommand
      */
     public int $id;
 
-    /**
-     * The name of the definition
-     */
-    public string $name;
-
-    /**
-     * The description of the definition
-     */
-    public string $description;
-
-    /**
-     * The id of a season
-     */
-    public ?int $season_id;
-
-    /**
-     * The id of a team
-     */
-    public ?int $team_id;
-
-    /**
-     * The weekday (1 = monday, ...)
-     */
-    public int $weekday;
-
-    /**
-     * The start time (HH:MM)
-     */
-    public string $start_time;
-
-    /**
-     * The end time (HH:MM)
-     */
-    public string $end_time;
-
-    /**
-     * The timezone for the start/end time
-     */
-    public string $time_zone;
-
-    /**
-     * Is this definition active?
-     */
-    public bool $active;
-
-    /**
-     * The location
-     */
-    public string $location;
-
-    /**
-     * A remark
-     */
-    public string $remark;
+    use DefinitionTrait;
 }
