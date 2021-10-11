122feb51941cc092a7f71e79aba9105af6dbc822
fbraem <franky.braem@gmail.com>
2021-10-11 20:33:56 +0200

    Add time_zone

--- src/kwai/Modules/Trainings/Presentation/REST/DefinitionSchema.php
+++ src/kwai/Modules/Trainings/Presentation/REST/DefinitionSchema.php
@@ -37,6 +37,7 @@ class DefinitionSchema implements \Kwai\Core\Infrastructure\Presentation\InputSc
                     'weekday' => Expect::int(),
                     'start_time' => Expect::string(),
                     'end_time' => Expect::string(),
+                    'time_zone' => Expect::string(),
                     'active' => Expect::bool(false),
                     'location' => Expect::string()->nullable(),
                     'remark' => Expect::string()->nullable(),
@@ -70,6 +71,7 @@ class DefinitionSchema implements \Kwai\Core\Infrastructure\Presentation\InputSc
         $command->weekday = $normalized->data->attributes->weekday;
         $command->start_time = $normalized->data->attributes->start_time;
         $command->end_time = $normalized->data->attributes->end_time;
+        $command->time_zone = $normalized->data->attributes->time_zone;
         $command->active = $normalized->data->attributes->active;
         $command->location = $normalized->data->attributes->location;
         $command->remark = $normalized->data->attributes->remark;
