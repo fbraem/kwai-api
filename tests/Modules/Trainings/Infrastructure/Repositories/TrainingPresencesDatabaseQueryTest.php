b25f694a14ffcedc93725cc70f5a503da9d8e2a9
fbraem <franky.braem@gmail.com>
2021-11-20 21:38:19 +0100

    Handle QueryException

--- tests/Modules/Trainings/Infrastructure/Repositories/TrainingPresencesDatabaseQueryTest.php
+++ tests/Modules/Trainings/Infrastructure/Repositories/TrainingPresencesDatabaseQueryTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Collection;
+use Kwai\Core\Infrastructure\Database\QueryException;
 use Kwai\Modules\Trainings\Infrastructure\Repositories\TrainingPresencesDatabaseQuery;
 use Tests\Context;
 
@@ -10,7 +11,11 @@ $context = Context::createContext();
 
 it('can query training presences', function () use ($context) {
     $query = new TrainingPresencesDatabaseQuery($context->db);
-    $presences = $query->execute();
+    try {
+        $presences = $query->execute();
+    } catch (QueryException $e) {
+        $this->fail((string) $e);
+    }
     expect($presences)
         ->toBeInstanceOf(Collection::class)
     ;
