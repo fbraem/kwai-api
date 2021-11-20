9b07b8173b18fa95654583604361e2c8e3d05d12
fbraem <franky.braem@gmail.com>
2021-11-20 21:26:08 +0100

    Code cleanup

--- tests/Modules/Trainings/Infrastructure/Mappers/CreatorMapperTest.php
+++ tests/Modules/Trainings/Infrastructure/Mappers/CreatorMapperTest.php
@@ -7,19 +7,19 @@ use Kwai\Core\Domain\ValueObjects\Name;
 use Kwai\Core\Infrastructure\Mappers\CreatorMapper;
 
 it('can map a record to a Creator', function () {
-   $record = collect([
+    $record = collect([
        'id' => '1',
        'first_name' => 'Jigoro',
        'last_name' => 'Kano'
-   ]);
+    ]);
 
-   $creator = CreatorMapper::toDomain($record);
+    $creator = CreatorMapper::toDomain($record);
 
-   expect($creator)
+    expect($creator)
        ->toBeInstanceOf(Creator::class)
        ->and($creator->getName())
        ->toBeInstanceOf(Name::class)
        ->and($creator->getName()->getFirstName())
        ->toBe('Jigoro')
-   ;
+    ;
 });
