d9f9c9c1dba433793b8d98c6ec2283eb9bc481ef
fbraem <franky.braem@gmail.com>
2021-12-03 19:13:33 +0100

    Use database from context

--- tests/Modules/Club/Presentation/REST/BrowseTeamsActionTest.php
+++ tests/Modules/Club/Presentation/REST/BrowseTeamsActionTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 use Kwai\Modules\Club\Presentation\REST\BrowseTeamsAction;
 use Nyholm\Psr7\Response;
 use Nyholm\Psr7\ServerRequest;
+use Tests\Context;
 
-it('execute BrowseTeamsAction', function () {
-    $action = new BrowseTeamsAction();
+$context = Context::createContext();
+
+it('execute BrowseTeamsAction', function ($context) {
+    $action = new BrowseTeamsAction($context->db);
 
     $request = new ServerRequest('GET', '/club/teams');
     $response = new Response();
@@ -15,4 +18,6 @@ it('execute BrowseTeamsAction', function () {
     $response = $action($request, $response, []);
 
     expect($response->getStatusCode())->toBe(200);
-});
+})
+    ->skip(!Context::hasDatabase(), 'No database available')
+;
