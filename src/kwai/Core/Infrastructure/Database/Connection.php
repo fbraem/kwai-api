02cfad2d487bf0f843000fd6a78e57a0481f720f
fbraem <franky.braem@gmail.com>
2021-12-20 22:03:35 +0100

    Set ATTR_EMULATE_PREPARES to false

--- src/kwai/Core/Infrastructure/Database/Connection.php
+++ src/kwai/Core/Infrastructure/Database/Connection.php
@@ -89,7 +89,8 @@ final class Connection
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_PERSISTENT => true, // BEST OPTION
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false
                 ]
             );
         } catch (PDOException $e) {
