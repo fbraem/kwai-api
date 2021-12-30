98a80287db7ef0b03aedae86c17237f5079fdd9d
fbraem <franky.braem@gmail.com>
2021-12-30 20:10:45 +0100

    Update UUID_REGEX

--- src/kwai/Applications/Application.php
+++ src/kwai/Applications/Application.php
@@ -35,7 +35,7 @@ abstract class Application
 {
     private array $middlewareQueue = [];
 
-    public const UUID_REGEX = '{uuid:[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}+}';
+    public const UUID_REGEX = '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}+';
 
     /**
      * KwaiApplication Constructor
