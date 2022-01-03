75a750fd529049df47115f94830b3686abdc1ff3
fbraem <franky.braem@gmail.com>
2022-01-03 16:14:53 +0100

    WIP

---
+++ src/kwai/Modules/Users/Presentation/REST/UpdateUserAction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package
+ * @subpackage
+ */
+declare(strict_types=1);
+
+namespace Kwai\Modules\Users\Presentation\REST;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+/**
+ * Class UpdateUserAction
+ */
+class UpdateUserAction extends \Kwai\Core\Infrastructure\Presentation\Action
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
