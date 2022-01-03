c72724d49a81a8c4d8822bb1936a06cc99b2cd27
fbraem <franky.braem@gmail.com>
2022-01-03 19:41:51 +0100

    Add tests for new methods aliases/createFromRow

--- tests/Core/Infrastructure/Database/TableSchemaTest.php
+++ tests/Core/Infrastructure/Database/TableSchemaTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Collection;
 use Kwai\Core\Infrastructure\Database\TableAttribute;
 use Kwai\Core\Infrastructure\Database\TableSchema;
 
@@ -39,4 +40,36 @@ it('can handle an alias', function () {
     expect($result->id)->toBeInt()->toEqual(1);
     expect($result->title)->toBeString()->toEqual('test');
     expect($result->content_text)->toBeNull();
+});
+
+it('can map rows', function () {
+    $result = StoryTableSchema::createFromRow(collect([
+        'news_stories_id' => 1,
+        'news_stories_title' => 'test'
+    ]));
+    expect($result->id)->toBeInt()->toEqual(1);
+    expect($result->title)->toBeString()->toEqual('test');
+    expect($result->content_text)->toBeNull();
+});
+
+it('can map a row with alias', function () {
+    $result = StoryTableSchema::createFromRow(
+        collect([
+            'n_id' => 1,
+            'n_title' => 'test'
+        ]),
+        'n'
+    );
+
+    expect($result->id)->toBeInt()->toEqual(1);
+    expect($result->title)->toBeString()->toEqual('test');
+    expect($result->content_text)->toBeNull();
+});
+
+it('can create aliases', function () {
+    $aliases = StoryTableSchema::aliases();
+    expect($aliases)
+       ->toBeInstanceOf(Collection::class)
+       ->and($aliases->toArray())
+    ;
 });
