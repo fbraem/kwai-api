bb5272adc0e347c4e8cfb480ea09085365f168fc
fbraem <franky.braem@gmail.com>
2022-01-15 23:22:56 +0100

    Don't initialize with null, uninitialized properties will be skipped when persisted

--- src/kwai/Modules/Users/Infrastructure/UsersTable.php
+++ src/kwai/Modules/Users/Infrastructure/UsersTable.php
@@ -16,17 +16,17 @@ use Kwai\Core\Infrastructure\Database\TableSchema;
 #[TableAttribute(name: 'users')]
 class UsersTable extends TableSchema
 {
-    public ?int $id = null;
+    public ?int $id;
     public string $email;
     public string $password;
-    public ?string $last_login = null;
-    public ?string $first_name = null;
-    public ?string $last_name = null;
-    public ?string $remark = null;
-    public ?int $member_id = null;
+    public ?string $last_login;
+    public ?string $first_name;
+    public ?string $last_name;
+    public ?string $remark;
+    public ?int $member_id;
     public string $uuid;
     public string $created_at;
-    public ?string $updated_at = null;
-    public ?int $revoked = 0;
-    public ?string $last_unsuccessful_login = null;
+    public ?string $updated_at;
+    public ?int $revoked;
+    public ?string $last_unsuccessful_login;
 }
