a10f1554295c211121187397db35b807c8b6db37
fbraem <franky.braem@gmail.com>
2022-01-31 22:00:57 +0100

    Add Policy.php

---
+++ src/kwai/Core/Domain/Policy.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package Core
+ * @subpackage Domain
+ */
+declare(strict_types=1);
+
+namespace Kwai\Core\Domain;
+
+/**
+ * Interface Policy
+ *
+ * An interface for implementing policies (Checking permissions)
+ */
+interface Policy
+{
+    /**
+     * Return true, when a user is allowed to create an entity.
+     *
+     * @return bool
+     */
+    public function canCreate(): bool;
+
+    /**
+     * Return true, when a user is allowed to remove an entity.
+     *
+     * @return bool
+     */
+    public function canRemove(): bool;
+
+    /**
+     * Return true, when a user is allowed to view an entity.
+     *
+     * @return bool
+     */
+    public function canView(): bool;
+
+    /**
+     * Return true, when a user is allowed to update an entity.
+     *
+     * @return bool
+     */
+    public function canUpdate(): bool;
+}
