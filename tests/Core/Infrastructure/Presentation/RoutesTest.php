ec9a29cdbb37f62dc4f98ee88124da205bfb37ec
fbraem <franky.braem@gmail.com>
2022-02-06 17:41:52 +0100

    Add test for loading routs with attributes

---
+++ tests/Core/Infrastructure/Presentation/RoutesTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use Kwai\Applications\UsersApplication;
+use Nyholm\Psr7\ServerRequest;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+
+it('can load routes from attributes', function () {
+    $app = new UsersApplication();
+    $routes = $app->createRoutes();
+
+    $request = new ServerRequest(
+       'GET',
+       '/users'
+    );
+    $symfonyRequest = (new HttpFoundationFactory())->createRequest($request);
+    $context = new RequestContext();
+    $context->fromRequest($symfonyRequest);
+    $matcher = new UrlMatcher($routes, $context);
+    $parameters = $matcher->matchRequest($symfonyRequest);
+    expect($routes)->toBeObject();
+    expect($parameters)
+        ->toBeArray()
+        ->toHaveKeys([
+            'auth',
+            '_action',
+            '_route',
+        ])
+        ->and($parameters['_route'])
+        ->toEqual('users.get')
+    ;
+});
