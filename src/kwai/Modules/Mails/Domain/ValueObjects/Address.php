d23ed2ed85e46f47112c978642f8f343893935b9
fbraem <franky.braem@gmail.com>
2022-02-16 20:55:01 +0100

    Add create

--- src/kwai/Modules/Mails/Domain/ValueObjects/Address.php
+++ src/kwai/Modules/Mails/Domain/ValueObjects/Address.php
@@ -40,4 +40,27 @@ final class Address
     {
         return $this->name;
     }
+
+    /**
+     * Factory method for creating an address.
+     *
+     * $address can be a string with an email address, a list array
+     * with the first element an email address and an optional second element
+     * with the name. Or it can be an associative array with the key as email
+     * and the value as name.
+     *
+     * @param string|array $address
+     * @return static
+     */
+    public static function create(string|array $address): self
+    {
+        if (is_array($address)) {
+            if (array_is_list($address)) {
+                return new self(new EmailAddress($address[0]), $address[1] ?? '');
+            }
+            $first_key = array_key_first($address);
+            return new self(new EmailAddress($first_key), $address[$first_key]);
+        }
+        return new self(new EmailAddress($address));
+    }
 }
