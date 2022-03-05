22d4b3f06d9cc112132750edd62d44328533bbf1
fbraem <franky.braem@gmail.com>
2022-03-05 21:43:16 +0100

    Add DsnDatabaseConfiguration

---
+++ src/kwai/Core/Infrastructure/Configuration/DsnDatabaseConfiguration.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package Core
+ * @subpackage Infrastructure
+ */
+declare(strict_types=1);
+
+namespace Kwai\Core\Infrastructure\Configuration;
+
+/**
+ * Class DsnDatabaseConfiguration
+ */
+class DsnDatabaseConfiguration
+{
+    private string $driver;
+
+    private array $parameters = [];
+
+    public function __construct(
+        private string $dsn
+    ) {
+        $parts = explode(':', $dsn, 2);
+        if (count($parts) != 2) {
+            throw new \InvalidArgumentException('Invalid DSN specified');
+        }
+        $this->driver = $parts[0];
+        $parameter_strings = explode(';', $parts[1]);
+        foreach ($parameter_strings as $parameter) {
+            $parameter_parts = explode('=', $parameter, 2);
+            if (count($parameter_parts) != 2) {
+                throw new \InvalidArgumentException('Invalid DSN specified');
+            }
+            $this->parameters[$parameter_parts[0]] = $parameter_parts[1];
+        }
+    }
+
+    public function getDriver(): string
+    {
+        return $this->driver;
+    }
+
+    public function getDsn(): string
+    {
+        return $this->dsn;
+    }
+
+    public function getDatabaseName(): string
+    {
+        return $this->parameters['dbname'];
+    }
+
+    public function getHost(): string
+    {
+        return $this->parameters['host'];
+    }
+
+    public function getCharset(): string
+    {
+        return $this->parameters['charset'];
+    }
+}
---
+++ tests/Core/Infrastructure/Configuration/DsnDatabaseConfigurationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Kwai\Core\Infrastructure\Configuration\DsnDatabaseConfiguration;
+
+it('can read a database DSN', function() {
+    $dsn = 'mysql:host=localhost;dbname=test;port=3306;charset=utf8mb4';
+    $config = new DsnDatabaseConfiguration(
+        'mysql:host=localhost;dbname=test;port=3306;charset=utf8mb4'
+    );
+    expect($config->getDriver())->toEqual('mysql');
+    expect($config->getDsn())->toEqual($dsn);
+    expect($config->getHost())->toEqual('localhost');
+    expect($config->getDatabaseName())->toEqual('test');
+});
