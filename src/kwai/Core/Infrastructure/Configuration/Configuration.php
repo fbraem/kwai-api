c30087aa2578223a9b8b6fb4f5d510a02457984d
fbraem <franky.braem@gmail.com>
2022-03-18 21:49:44 +0100

    Add createFromFile, createFromString

--- src/kwai/Core/Infrastructure/Configuration/Configuration.php
+++ src/kwai/Core/Infrastructure/Configuration/Configuration.php
@@ -8,74 +8,82 @@ declare(strict_types=1);
 namespace Kwai\Core\Infrastructure\Configuration;
 
 use Dotenv\Dotenv;
+use Dotenv\Loader\Loader;
+use Dotenv\Parser\Parser;
+use Dotenv\Repository\RepositoryBuilder;
+use Dotenv\Store\StringStore;
 
 /**
  * Class Configuration
  */
 class Configuration
 {
-    private ?array $vars = null;
-
     public function __construct(
-        private ?Dotenv $env = null
+        private Dotenv $env
     ) {
-        $this->env ??= Dotenv::createImmutable(
-            __DIR__ . '/../../../../../config',
-            '.kwai'
-        );
+    }
+
+    /**
+     * Load configuration from a file.
+     *
+     * @param string $path
+     * @param string $file
+     * @return static
+     */
+    public static function createFromFile(string $path, string $file): self
+    {
+        $env = Dotenv::createImmutable($path, $file);
+        $env->load();
+        return new self($env);
+    }
+
+    /**
+     * Load configuration from a string.
+     *
+     * @param string $content
+     * @return static
+     */
+    public static function createFromString(string $content): self
+    {
+        $repository = RepositoryBuilder::createWithDefaultAdapters()->immutable()->make();
+        $env = new Dotenv(new StringStore($content), new Parser(), new Loader(), $repository);
+        $env->load();
+        return new self($env);
     }
 
     public function getDatabaseConfiguration(): DatabaseConfiguration
     {
-        if ($this->vars === null) {
-            $this->vars = $this->env->load();
-        }
         DatabaseConfiguration::validate($this->env);
-        return DatabaseConfiguration::createFromVariables($this->vars);
+        return DatabaseConfiguration::createFromVariables($_SERVER);
     }
 
     public function getCorsConfiguration(): CorsConfiguration
     {
-        if ($this->vars === null) {
-            $this->vars = $this->env->load();
-        }
         CorsConfiguration::validate($this->env);
-        return CorsConfiguration::createFromVariables($this->vars);
+        return CorsConfiguration::createFromVariables($_SERVER);
     }
 
     public function getLoggerConfiguration(): LoggerConfiguration
     {
-        if ($this->vars === null) {
-            $this->vars = $this->env->load();
-        }
         LoggerConfiguration::validate($this->env);
-        return LoggerConfiguration::createFromVariables($this->vars);
+        return LoggerConfiguration::createFromVariables($_SERVER);
     }
 
     public function getSecurityConfiguration(): SecurityConfiguration
     {
-        if ($this->vars === null) {
-            $this->vars = $this->env->load();
-        }
         SecurityConfiguration::validate($this->env);
-        return SecurityConfiguration::createFromVariables($this->vars);
+        return SecurityConfiguration::createFromVariables($_SERVER);
     }
 
     public function getMailerConfiguration(): MailerConfiguration
     {
-        if ($this->vars === null) {
-            $this->vars = $this->env->load();
-        }
         MailerConfiguration::validate($this->env);
-        return MailerConfiguration::createFromVariables($this->vars);
+        return MailerConfiguration::createFromVariables($_SERVER);
     }
 
     public function getWebsiteConfiguration(): WebsiteConfiguration
     {
-        if ($this->vars === null) {
-            $this->vars = $this->env->load();
-        }
         WebsiteConfiguration::validate($this->env);
-        return WebsiteConfiguration::createFromVariables($this->vars);
+        return WebsiteConfiguration::createFromVariables($_SERVER);
     }
 }
