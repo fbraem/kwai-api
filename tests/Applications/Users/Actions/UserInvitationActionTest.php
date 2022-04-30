3e55bed0b3c87a58e466cf3407d7aae8c5188040
fbraem <franky.braem@gmail.com>
2022-04-30 19:32:01 +0200

    Use HttpClient

--- tests/Applications/Users/Actions/UserInvitationActionTest.php
+++ tests/Applications/Users/Actions/UserInvitationActionTest.php
@@ -1,101 +1,46 @@
 <?php
 declare(strict_types=1);
 
-use Kwai\Applications\Users\Actions\BrowseUserInvitationsAction;
-use Kwai\Applications\Users\Actions\ConfirmInvitationAction;
-use Kwai\Applications\Users\Actions\CreateUserInvitationAction;
-use Kwai\Applications\Users\Actions\GetUserInvitationAction;
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\ServerRequest;
-use Tests\Context;
-
-$context = Context::createContext();
-
 $data = [
     'data' => [
         'type' => 'user_invitations',
         'attributes' => [
-            'email' => "jigor.kano" . rand(1, 100) . "@gmail.com",
+            'email' => "jigoro.kano" . rand(1, 100) . "@gmail.com",
             'name' => 'Jigoro Kano',
             'remark' => 'A user invitation created with a unittest'
         ]
     ]
 ];
 
-it('can create an user invitation', function () use ($context, $data) {
-    $action = new CreateUserInvitationAction($context->db);
-
-    $request = new ServerRequest(
-        'POST',
-        '/users/invitations/',
-        []
-    );
-    $request = $request
-        ->withParsedBody($data)
-        ->withAttribute(
-            'kwai.user',
-            $context->user
-        )
-    ;
-    $response = new Response();
-    $response = $action($request, $response, []);
+it('can create an user invitation', function () use ($data) {
+    $response = $this->post('/users/invitations', $data);
     expect($response->getStatusCode())->toBe(200);
 
-    $result = json_decode((string) $response->getBody(), true);
+    $result = json_decode($response->getContent(), true);
     return $result['data']['id'];
-})
-    ->skip(!Context::hasDatabase(), 'No database available')
-;
+});
 
-it('can get an user invitation', function ($uuid) use ($context) {
-    $action = new GetUserInvitationAction($context->db);
-
-    $request = new ServerRequest(
-        'GET',
-        '/users/invitations/' . $uuid
-    );
-    $response = new Response();
-
-    $response = $action($request, $response, ['uuid' => $uuid]);
+it('can get an user invitation', function ($uuid) {
+    $response = $this->get('/users/invitations/' . $uuid);
     expect($response->getStatusCode())->toBe(200);
 })
-    ->skip(!Context::hasDatabase(), 'No database available')
     ->depends('it can create an user invitation')
 ;
 
-it('can confirm a user invitation', function ($uuid) use ($context, $data) {
-    $action = new ConfirmInvitationAction($context->db);
+it('can confirm a user invitation', function ($uuid) use ($data) {
     unset($data['data']['attributes']['name']);
+    $data['data']['id'] = $uuid;
     $data['data']['attributes']['first_name'] = 'Jigoro';
     $data['data']['attributes']['last_name'] = 'Kano';
     $data['data']['attributes']['password'] = 'Test1234';
 
-    $request = new ServerRequest(
-        'PATCH',
-        '/users/invitations/' . $uuid
-    );
-    $request = $request->withParsedBody($data);
-
-    $response = new Response();
-
-    $response = $action($request, $response, ['uuid' => $uuid]);
+    $response = $this->post('/users/invitations/' . $uuid, $data);
     expect($response->getStatusCode())->toBe(200);
 })
-    ->skip(!Context::hasDatabase(), 'No database available')
     ->depends('it can create an user invitation')
 ;
 
-it('can browse user invitations', function () use ($context) {
-    $action = new BrowseUserInvitationsAction($context->db);
-
-    $request = new ServerRequest(
-        'GET',
-        '/users/invitations/'
-    );
-    $response = new Response();
-
-    $response = $action($request, $response, []);
+it('can browse user invitations', function () {
+    $response = $this->get('/users/invitations');
     expect($response->getStatusCode())->toBe(200);
-})
-    ->skip(!Context::hasDatabase(), 'No database available')
-;
+});
