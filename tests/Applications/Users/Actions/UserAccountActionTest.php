ef7acfb8d3d7fd1375d62d03f09fd6fc05259477
fbraem <franky.braem@gmail.com>
2022-04-30 19:12:06 +0200

    Use HttpClient

--- tests/Applications/Users/Actions/UserAccountActionTest.php
+++ tests/Applications/Users/Actions/UserAccountActionTest.php
@@ -1,27 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use Kwai\Applications\Users\Actions\BrowseUserAccountsAction;
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\ServerRequest;
-use Tests\Context;
-
-$context = Context::createContext();
-
-it('can browse users', function () use ($context) {
-    $action = new BrowseUserAccountsAction($context->db);
-
-    $request = new ServerRequest(
-        'GET',
-        '/users/accounts'
-    );
-    $response = new Response();
-
-    $response = $action($request, $response, []);
+it('can browse user accounts', function () {
+    $response = $this->get('/users/accounts');
     expect($response->getStatusCode())->toBe(200);
-
-    $result = json_decode((string) $response->getBody(), true);
-    return $result['data'][0]['id'];
-})
-    ->skip(!Context::hasDatabase(), 'No database available')
-;
+});
