8f71252e25798f8ee0dac274b7e6046ed3d92302
fbraem <franky.braem@gmail.com>
2022-05-13 19:31:02 +0200

    Add expectations

--- tests/Applications/Users/Actions/BrowsePermissionsActionTest.php
+++ tests/Applications/Users/Actions/BrowsePermissionsActionTest.php
@@ -5,6 +5,20 @@ it('can browse user permissions', function () {
     $response = $this->get('/users/permissions');
     expect($response->getStatusCode())->toBe(200);
 
-    $result = json_decode($response->getContent(false), true);
-    dd($result);
+    $data = $response->toArray();
+    expect($data)
+        ->toHaveKey('data')
+    ;
+    expect($data['data'])
+        ->toBeArray()
+        ->toHaveKey('type')
+        ->toHaveKey('id')
+        ->toHaveKey('attributes')
+    ;
+    expect($data['data']['type'])
+        ->toBe('rules')
+    ;
+    expect($data['data']['attributes'])
+        ->toBeArray()
+    ;
 });
