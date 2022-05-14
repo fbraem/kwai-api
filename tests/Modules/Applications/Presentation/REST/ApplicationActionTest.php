414985c259d434d4ddef593ce4a40cd39eec965b
fbraem <franky.braem@gmail.com>
2022-05-14 22:49:44 +0200

    Use HttpClientTrait

--- tests/Modules/Applications/Presentation/REST/ApplicationActionTest.php
+++ tests/Modules/Applications/Presentation/REST/ApplicationActionTest.php
@@ -2,26 +2,11 @@
 
 declare(strict_types=1);
 
-use Kwai\Modules\Applications\Presentation\REST\GetApplicationAction;
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\ServerRequest;
-use Tests\Context;
+use Tests\HttpClientTrait;
 
-$context = Context::createContext();
+uses(HttpClientTrait::class);
 
-// TODO: add create/update application test
-
-it('can get an application', function () use ($context) {
-    $action = new GetApplicationAction($context->db);
-
-    $request = new ServerRequest(
-        'GET',
-        '/applications/1'
-    );
-
-    $response = new Response();
-    $response = $action($request, $response, ['id' => '1']);
+it('can get an application', function () {
+    $response = $this->get('/portal/applications/1');
     expect($response->getStatusCode())->toBe(200);
-})
-    ->skip(!Context::hasDatabase(), 'No database available')
-;
+});
