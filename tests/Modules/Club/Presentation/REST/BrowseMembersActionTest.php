86092c5fdc0c78a8a972f4cb1e625b79ae141abe
fbraem <franky.braem@gmail.com>
2022-05-20 20:09:57 +0200

    Use HttpClient and tobeJSONAPIArray

--- tests/Modules/Club/Presentation/REST/BrowseMembersActionTest.php
+++ tests/Modules/Club/Presentation/REST/BrowseMembersActionTest.php
@@ -2,22 +2,12 @@
 
 declare(strict_types=1);
 
-use Kwai\Modules\Club\Presentation\REST\BrowseMembersAction;
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\ServerRequest;
-use Tests\Context;
-
-$context = Context::createContext();
-
-it('execute BrowseMembersAction', function () use ($context) {
-    $action = new BrowseMembersAction($context->db);
-
-    $request = new ServerRequest('GET', '/club/members');
-    $response = new Response();
-
-    $response = $action($request, $response, []);
-
+it('execute BrowseMembersAction', function ()  {
+    $response = $this->get('/club/members');
     expect($response->getStatusCode())->toBe(200);
-})
-    ->skip(!Context::hasDatabase(), 'No database available')
-;
+
+    $data = $response->toArray();
+    expect($data)
+        ->tobeJSONAPIArray('members', true)
+    ;
+});
--- tests/Modules/Club/Presentation/REST/BrowseTeamsActionTest.php
+++ tests/Modules/Club/Presentation/REST/BrowseTeamsActionTest.php
@@ -1,23 +1,12 @@
 <?php
-
 declare(strict_types=1);
 
-use Kwai\Modules\Club\Presentation\REST\BrowseTeamsAction;
-use Nyholm\Psr7\Response;
-use Nyholm\Psr7\ServerRequest;
-use Tests\Context;
-
-$context = Context::createContext();
-
-it('execute BrowseTeamsAction', function () use ($context) {
-    $action = new BrowseTeamsAction($context->db);
-
-    $request = new ServerRequest('GET', '/club/teams');
-    $response = new Response();
-
-    $response = $action($request, $response, []);
-
+it('can browse teams', function () {
+    $response = $this->get('/club/teams');
     expect($response->getStatusCode())->toBe(200);
-})
-    ->skip(!Context::hasDatabase(), 'No database available')
-;
+
+    $data = $response->toArray();
+    expect($data)
+        ->tobeJSONAPIArray('teams')
+    ;
+});
