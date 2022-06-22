30e37ad0adc7784fa112959be3817e105effa214
fbraem <franky.braem@gmail.com>
2022-06-22 21:27:00 +0200

    Remove alias

--- src/kwai/Modules/Trainings/Infrastructure/PersonsTable.php
+++ src/kwai/Modules/Trainings/Infrastructure/PersonsTable.php
@@ -8,13 +8,13 @@ declare(strict_types=1);
 namespace Kwai\Modules\Trainings\Infrastructure;
 
 use Kwai\Core\Infrastructure\Database\TableAttribute;
-use Kwai\Core\Infrastructure\Database\TableSchema as TableSchemaAlias;
+use Kwai\Core\Infrastructure\Database\TableSchema;
 
 /**
  * Class PersonsTable
  */
 #[TableAttribute(name: 'persons')]
-class PersonsTable extends TableSchemaAlias
+class PersonsTable extends TableSchema
 {
     public ?int $id = null;
     public string $firstname;
