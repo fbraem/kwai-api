a73942e19ade5d935d0552f068a7e2962b70534a
fbraem <franky.braem@gmail.com>
2022-06-22 21:41:18 +0200

    Add TrainingCoachesTable

---
+++ src/kwai/Modules/Trainings/Infrastructure/TrainingCoachesTable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package Modules
+ * @subpackage Trainings
+ */
+declare(strict_types=1);
+
+namespace Kwai\Modules\Trainings\Infrastructure;
+
+use Kwai\Core\Infrastructure\Database\TableAttribute;
+use Kwai\Core\Infrastructure\Database\TableSchema;
+
+/**
+ * Class TrainingCoachesTable
+ */
+#[TableAttribute(name: 'training_coaches')]
+class TrainingCoachesTable extends TableSchema
+{
+    public int $training_id;
+    public int $coach_id;
+    public int $coach_type;
+    public int $payed;
+    public int $present;
+    public string $remark;
+    public int $user_id;
+    public ?string $updated_at = null;
+    public string $created_at;
+}
