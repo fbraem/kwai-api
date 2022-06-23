c32bc0880f987b4a6cd062f47f1a758c542478e0
fbraem <franky.braem@gmail.com>
2022-06-23 20:36:06 +0200

    Add TeamsTable

---
+++ src/kwai/Modules/Trainings/Infrastructure/TeamsTable.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package Modules
+ * @subpackage Trainings
+ */
+declare(strict_types=1);
+
+namespace Kwai\Modules\Trainings\Infrastructure;
+
+use Kwai\Core\Infrastructure\Database\TableAttribute;
+use Kwai\Core\Infrastructure\Database\TableSchema;
+
+/**
+ * Class TeamsTable
+ */
+#[TableAttribute(name: 'teams')]
+final class TeamsTable extends TableSchema
+{
+    public ?int $id = null;
+    public string $name;
+}
