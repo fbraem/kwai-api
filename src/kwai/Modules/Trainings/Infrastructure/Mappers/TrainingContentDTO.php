db747869f7add67a5dea2271a8ef06b801e791aa
fbraem <franky.braem@gmail.com>
2022-06-25 19:57:34 +0200

    Implement create

--- src/kwai/Modules/Trainings/Infrastructure/Mappers/TrainingContentDTO.php
+++ src/kwai/Modules/Trainings/Infrastructure/Mappers/TrainingContentDTO.php
@@ -7,6 +7,10 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Trainings\Infrastructure\Mappers;
 
+use Kwai\Core\Domain\ValueObjects\Creator;
+use Kwai\Core\Domain\ValueObjects\DocumentFormat;
+use Kwai\Core\Domain\ValueObjects\Locale;
+use Kwai\Core\Domain\ValueObjects\Name;
 use Kwai\Core\Domain\ValueObjects\Text;
 use Kwai\Modules\Trainings\Infrastructure\TrainingContentsTable;
 use Kwai\Modules\Trainings\Infrastructure\UsersTable;
@@ -20,5 +24,23 @@ class TrainingContentDTO
         public TrainingContentsTable $content,
         public UsersTable $user
     ) {
+    }
+
+    public function create(): Text
+    {
+        return new Text(
+            locale: Locale::from($this->content->locale),
+            format: DocumentFormat::from($this->content->format),
+            title: $this->content->title,
+            author: new Creator(
+                id: $this->user->id,
+                name: new Name(
+                    $this->user->first_name,
+                    $this->user->last_name
+                )
+            ),
+            summary: $this->content->summary,
+            content: $this->content->content
+        );
     }
 }
