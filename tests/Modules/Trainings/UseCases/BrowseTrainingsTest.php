d3724dc9463be66c19cf56eeed33d1cb1b212729
fbraem <franky.braem@gmail.com>
2022-07-25 20:27:44 +0200

    Use a mock for CoachDatabaseRepository and use TrainingEntity

--- tests/Modules/Trainings/UseCases/BrowseTrainingsTest.php
+++ tests/Modules/Trainings/UseCases/BrowseTrainingsTest.php
@@ -2,17 +2,44 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Collection;
-use Kwai\Core\Domain\Entity;
+use Kwai\Core\Domain\ValueObjects\Name;
+use Kwai\Modules\Trainings\Domain\Coach;
+use Kwai\Modules\Trainings\Domain\CoachEntity;
 use Kwai\Modules\Trainings\Domain\Exceptions\CoachNotFoundException;
+use Kwai\Modules\Trainings\Domain\TrainingEntity;
 use Kwai\Modules\Trainings\Infrastructure\Repositories\CoachDatabaseRepository;
 use Kwai\Modules\Trainings\Infrastructure\Repositories\DefinitionDatabaseRepository;
 use Kwai\Modules\Trainings\Infrastructure\Repositories\TrainingDatabaseRepository;
+use Kwai\Modules\Trainings\Repositories\CoachRepository;
 use Kwai\Modules\Trainings\UseCases\BrowseTrainings;
 use Kwai\Modules\Trainings\UseCases\BrowseTrainingsCommand;
 use Tests\DatabaseTrait;
 
 uses(DatabaseTrait::class);
-beforeEach(fn() => $this->withDatabase());
+beforeEach(function() {
+    $this->withDatabase();
+    $this->coachRepo = new class($this->db) extends CoachDatabaseRepository implements CoachRepository {
+        public function getById(int ...$ids): Collection
+        {
+            $coaches = new Collection();
+            foreach($ids as $id) {
+                $coaches->put(
+                    $id,
+                    new CoachEntity(
+                        $id,
+                        new Coach(
+                            name: new Name(
+                                firstname: 'Jigoro',
+                                lastname: 'Kano'
+                            )
+                        )
+                    )
+                );
+            }
+            return $coaches;
+        }
+    };
+});
 
 it('can browse trainings', function () {
     $repo = new TrainingDatabaseRepository($this->db);
@@ -34,7 +61,7 @@ it('can browse trainings', function () {
             ->toBeGreaterThan(0)
         ;
         expect($trainings->first())
-            ->toBeInstanceOf(Entity::class)
+            ->toBeInstanceOf(TrainingEntity::class)
         ;
     } catch (Exception $e) {
         $this->fail((string) $e);
@@ -50,7 +77,7 @@ it('can browse trainings for a given year and month', function () {
 
     $command = new BrowseTrainingsCommand();
     $command->year = 2020;
-    $command->month = 9;
+    $command->month = 12;
     try {
         [$count, $trainings] = BrowseTrainings::create(
             $repo,
@@ -66,7 +93,7 @@ it('can browse trainings for a given year and month', function () {
             ->toBeGreaterThan(0)
         ;
         expect($trainings->first())
-            ->toBeInstanceOf(Entity::class)
+            ->toBeInstanceOf(TrainingEntity::class)
         ;
     } catch (Exception $e) {
         $this->fail((string) $e);
@@ -77,15 +104,14 @@ it('can browse trainings for a given year and month', function () {
 
 it('can browse trainings for a coach', function () {
     $repo = new TrainingDatabaseRepository($this->db);
-    $coachRepo = new CoachDatabaseRepository($this->db);
     $defRepo = new DefinitionDatabaseRepository($this->db);
 
     $command = new BrowseTrainingsCommand();
-    $command->coach = 1;
+    $command->coach = 2;
     try {
         [$count, $trainings] = BrowseTrainings::create(
             $repo,
-            $coachRepo,
+            $this->coachRepo,
             $defRepo
         )($command);
         expect($count)
@@ -97,7 +123,7 @@ it('can browse trainings for a coach', function () {
             ->toBeGreaterThan(0)
         ;
         expect($trainings->first())
-            ->toBeInstanceOf(Entity::class)
+            ->toBeInstanceOf(TrainingEntity::class)
         ;
     } catch (Exception $e) {
         $this->fail((string) $e);
