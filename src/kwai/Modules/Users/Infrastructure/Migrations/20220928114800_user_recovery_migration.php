26d44f96ea4de99c7b9f4bbe8f2f6dda4bd14984
fbraem <franky.braem@gmail.com>
2022-09-26 12:09:02 +0200

    Add UserRecoveryMigration

---
+++ src/kwai/Modules/Users/Infrastructure/Migrations/20220928114800_user_recovery_migration.php
@@ -0,0 +1,24 @@
+<?php
+
+use Kwai\Core\Infrastructure\Dependencies\Settings;
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Base User Migration
+ */
+class UserRecoveryMigration extends AbstractMigration
+{
+    public function up()
+    {
+        $this->table('user_recoveries', ['signed' => false])
+            ->addColumn('user_id', 'integer')
+            ->addColumn('uuid', 'string')
+            ->addColumn('expired_at', 'datetime')
+            ->addColumn('expired_at_timezone', 'string')
+            ->addColumn('confirmed_at', 'datetime')
+            ->addColumn('remark', 'string', ['null' => true])
+            ->addTimestamps()
+            ->create()
+        ;
+    }
+}
