969f269fd4f166506fe479da3b07e1c5e3378cd6
fbraem <franky.braem@gmail.com>
2022-09-27 20:17:11 +0200

    Remove DatabaseMailerService

--- src/kwai/Core/Infrastructure/Dependencies/MailerDependency.php
+++ src/kwai/Core/Infrastructure/Dependencies/MailerDependency.php
@@ -8,11 +8,8 @@ declare(strict_types=1);
 namespace Kwai\Core\Infrastructure\Dependencies;
 
 use Kwai\Core\Infrastructure\Configuration\Configuration;
-use Kwai\Core\Infrastructure\Database\Connection;
-use Kwai\Core\Infrastructure\Mailer\DatabaseMailerService;
 use Kwai\Core\Infrastructure\Mailer\MailerService;
 use Kwai\Core\Infrastructure\Mailer\MailerServiceFactory;
-use Kwai\Modules\Mails\Infrastructure\Repositories\MailDatabaseRepository;
 
 /**
  * Class MailerDependency
@@ -20,19 +17,13 @@ use Kwai\Modules\Mails\Infrastructure\Repositories\MailDatabaseRepository;
 class MailerDependency implements Dependency
 {
     public function __construct(
-        private ?Configuration $settings = null,
-        private ?Connection $db = null
+        private ?Configuration $settings = null
     ) {
         $this->settings ??= depends('kwai.settings', Settings::class);
-        $this->db ??= depends('kwai.db', DatabaseDependency::class);
     }
 
     public function create(): MailerService
     {
-        return new DatabaseMailerService(
-            new MailDatabaseRepository($this->db),
-            (new MailerServiceFactory($this->settings->getMailerConfiguration()))
-                ->create()
-        );
+        return (new MailerServiceFactory($this->settings->getMailerConfiguration()))->create();
     }
 }
