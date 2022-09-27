6ec7737ba53b2ccca8bb34afd72c35cc28152fe0
fbraem <franky.braem@gmail.com>
2022-09-27 20:21:59 +0200

    Remove sendMail, move arguments to constructor

--- src/kwai/Core/Infrastructure/Mailer/DatabaseMailerService.php
+++ src/kwai/Core/Infrastructure/Mailer/DatabaseMailerService.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package
- * @subpackage
+ * @package Core
+ * @subpackage Infrastructure
  */
 declare(strict_types=1);
 
@@ -12,10 +12,7 @@ use Kwai\Core\Domain\ValueObjects\UniqueId;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
 use Kwai\Modules\Mails\Domain\Mail;
 use Kwai\Modules\Mails\Domain\ValueObjects\Address;
-use Kwai\Modules\Mails\Domain\ValueObjects\MailContent;
-use Kwai\Modules\Mails\Domain\ValueObjects\RecipientType;
 use Kwai\Modules\Mails\Repositories\MailRepository;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class DatabaseMailerService
@@ -26,7 +23,10 @@ class DatabaseMailerService implements MailerService
 {
     public function __construct(
         private readonly MailRepository $mailRepo,
-        private readonly MailerService $service
+        private readonly MailerService $service,
+        private readonly UniqueId $uuid,
+        private readonly string $tag,
+        private readonly ?Creator $creator = null
     ) {
     }
 
@@ -41,31 +41,17 @@ class DatabaseMailerService implements MailerService
     /**
      * @throws RepositoryException
      */
-    public function sendMail(
-        UniqueId $uuid,
-        string $tag,
-        TemplatedMessage $mail,
-        array $recipients,
-        ?Creator $creator = null,
-    ): void {
+    public function send(Message $message): void {
         $this->mailRepo->create(
             new Mail(
-                uuid: $uuid,
-                sender: $mail->getFrom() ?? $this->service->getFrom(),
-                content: $mail->createMailContent(),
-                creator: $creator,
-                tag: $tag,
-                recipients: collect($recipients)
+                uuid: $this->uuid,
+                sender: $message->getFrom() ?? $this->service->getFrom(),
+                content: $message->createMailContent(),
+                creator: $this->creator,
+                tag: $this->tag,
+                recipients: collect($message->getRecipients())
             )
         );
-        $this->send($mail);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function send(Message $message): void
-    {
         $this->service->send($message);
     }
 }
