7d006f91bc0ac98affae35e4fe07715f761b51ee
fbraem <franky.braem@gmail.com>
2022-10-14 22:10:31 +0200

    Implement Mailer

--- src/kwai/Modules/Users/Mailers/UserRecoveryMailer.php
+++ src/kwai/Modules/Users/Mailers/UserRecoveryMailer.php
@@ -7,13 +7,11 @@ declare(strict_types=1);
 
 namespace Kwai\Modules\Users\Mailers;
 
+use Kwai\Core\Domain\Mailer;
 use Kwai\Core\Infrastructure\Mailer\MailerService;
-use Kwai\Core\Infrastructure\Mailer\MessageTemplateFactory;
+use Kwai\Core\Infrastructure\Mailer\Message;
 use Kwai\Core\Infrastructure\Mailer\Recipient;
-use Kwai\Core\Infrastructure\Mailer\Recipients;
 use Kwai\Core\Infrastructure\Template\MailTemplate;
-use Kwai\Modules\Mails\Repositories\MailRepository;
-use Kwai\Modules\Users\Domain\UserAccountEntity;
 use Kwai\Modules\Users\Domain\UserRecoveryEntity;
 
 /**
@@ -21,44 +19,33 @@ use Kwai\Modules\Users\Domain\UserRecoveryEntity;
  *
  * Mails a recovery code to the user
  */
-class UserRecoveryMailer
+class UserRecoveryMailer implements Mailer
 {
     public function __construct(
         private readonly MailerService $mailer,
         private readonly MailTemplate $template,
-        private readonly MailRepository $mailRepo,
-        private readonly UserRecoveryEntity $recovery,
-        private readonly UserAccountEntity $account
+        private readonly UserRecoveryEntity $recovery
     ) {
     }
 
-    public function send(Recipients $recipients): void
+    public function send(): Message
     {
+        $recipients = $this->mailer->createRecipients();
         $message = $this->template->createMessage(
             $recipients->withTo(
                 new Recipient(
-                    (string) $this->recovery->getReceiver(),
-                    (string) $this->account->getUser()->getUsername()
+                    (string) $this->recovery->getUser()->getEmailAddress(),
+                    (string) $this->recovery->getUser()->getUsername()
                 )
             ),
             'Recover User Password',
             [
                 'uuid' => (string) $this->recovery->getUuid(),
-                'name' => (string) $this->account->getUser()->getUsername(),
-                'expires' => '',
+                'name' => (string) $this->recovery->getUser()->getUsername(),
+                'expires' => 2
             ]
         );
-        /*
-        $this->mailRepo->create(
-            new Mail(
-                uuid: $this->$this->recovery->getUuid(),
-                content: $message->createMailContent(),
-                tag: 'Users.recover',
-                recipients: collect($recipients)
-            )
-        );
-        */
-
         $this->mailer->send($message);
+        return $message;
     }
 }
