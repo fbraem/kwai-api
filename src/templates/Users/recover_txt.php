293eb40f7910ac0c0d86baaf53941e605e6860d7
fbraem <franky.braem@gmail.com>
2022-10-15 16:28:18 +0200

    Enhance mail text

--- src/templates/Users/recover_txt.php
+++ src/templates/Users/recover_txt.php
@@ -1,3 +1,10 @@
-Dear <?= $name ?>,
+Dear <?= $this->e($name) ?>,
 
-Please click on this link <?= $uuid ?> to reset your password.
+We have received your request to reset your password.
+
+Use this code <?= $this->e($uuid) ?> on our reset password page to reset your password.
+
+If you need assistance, or you did not make this request, please contact our webmaster
+<?= $this->e($website['email']) ?>.
+
+Note: this code will expire within <?= $this->e($expires) ?>
