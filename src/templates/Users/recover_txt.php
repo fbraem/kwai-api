cff2d48e4799da2dde949229ebd0d90df73f99a1
fbraem <franky.braem@gmail.com>
2022-10-15 16:29:12 +0200

    Remove a newline

--- src/templates/Users/recover_txt.php
+++ src/templates/Users/recover_txt.php
@@ -4,7 +4,6 @@ We have received your request to reset your password.
 
 Use this code <?= $this->e($uuid) ?> on our reset password page to reset your password.
 
-If you need assistance, or you did not make this request, please contact our webmaster
-<?= $this->e($website['email']) ?>.
+If you need assistance, or you did not make this request, please contact our webmaster <?= $this->e($website['email']) ?>.
 
 Note: this code will expire within <?= $this->e($expires) ?>
