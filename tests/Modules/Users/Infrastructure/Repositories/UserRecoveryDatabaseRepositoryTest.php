68b8fe916f1fc1ad27d3feff87ecdf1d00443a74
fbraem <franky.braem@gmail.com>
2022-10-17 17:51:11 +0200

    Update test

--- tests/Modules/Users/Infrastructure/Repositories/UserRecoveryDatabaseRepositoryTest.php
+++ tests/Modules/Users/Infrastructure/Repositories/UserRecoveryDatabaseRepositoryTest.php
@@ -2,8 +2,16 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Collection;
+use Kwai\Core\Domain\ValueObjects\EmailAddress;
+use Kwai\Core\Domain\ValueObjects\LocalTimestamp;
+use Kwai\Core\Domain\ValueObjects\Name;
+use Kwai\Core\Domain\ValueObjects\Timestamp;
+use Kwai\Core\Domain\ValueObjects\UniqueId;
 use Kwai\Core\Infrastructure\Repositories\RepositoryException;
+use Kwai\Modules\Users\Domain\User;
+use Kwai\Modules\Users\Domain\UserEntity;
 use Kwai\Modules\Users\Domain\UserRecovery;
+use Kwai\Modules\Users\Domain\UserRecoveryEntity;
 use Kwai\Modules\Users\Infrastructure\Repositories\UserRecoveryDatabaseRepository;
 use Tests\DatabaseTrait;
 
@@ -12,8 +20,24 @@ beforeEach(fn() => $this->withDatabase());
 
 it('can recover a user', function () {
     $repo = new UserRecoveryDatabaseRepository($this->db);
-    $recovery = new UserRecovery();
-    $repo->create($recovery);
+    $recovery = new UserRecovery(
+        new UniqueId(),
+        new LocalTimestamp(Timestamp::createNow(), 'UTC'),
+        new UserEntity(
+            1,
+            new User(
+                new UniqueId(),
+                new EmailAddress('jigoro.kano@kwai.com'),
+                new Name('Jigoro', 'Kano')
+            )
+        )
+    );
+    try {
+        $recovery = $repo->create($recovery);
+        expect($recovery)->toBeInstanceOf(UserRecoveryEntity::class);
+    } catch (RepositoryException $e) {
+        $this->fail((string) $e);
+    }
 })
     ->skip(fn() => !$this->hasDatabase(), 'No database available')
 ;
